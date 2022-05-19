cd6e8a0d22ebcd1c3458081661ca6911276f42bf
LetMeFly666 <814114971@qq.com>
2022-05-19 21:25:32 +0800

    Debug For 丢失的File

--- debug.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-GITHUBLEETCODEDEPLOYKEY = os.environ["GITHUBLEETCODEDEPLOYKEY"]
-FORPYTHONDEBUG = os.environ["FORPYTHONDEBUG"]
-response = requests.post("https://www.letmefly.xyz/URLdebug/", data={"GITHUBLEETCODEDEPLOYKEY": GITHUBLEETCODEDEPLOYKEY, "FORPYTHONDEBUG": FORPYTHONDEBUG})
-print(response)
-print(response.text)
