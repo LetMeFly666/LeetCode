51b8928e4aaf3a99d70edeab44adc0e92f7010c9
LetMeFly666 <814114971@qq.com>
2022-05-19 21:12:06 +0800

    for python debug

--- debug.py
+++ debug.py
@@ -2,6 +2,7 @@ import os
 import requests
 
 GITHUBLEETCODEDEPLOYKEY = os.environ["GITHUBLEETCODEDEPLOYKEY"]
-response = requests.post("https://www.letmefly.xyz/URLdebug/", data={"GITHUBLEETCODEDEPLOYKEY": GITHUBLEETCODEDEPLOYKEY})
+FORPYTHONDEBUG = os.environ["FORPYTHONDEBUG"]
+response = requests.post("https://www.letmefly.xyz/URLdebug/", data={"GITHUBLEETCODEDEPLOYKEY": GITHUBLEETCODEDEPLOYKEY, "FORPYTHONDEBUG": FORPYTHONDEBUG})
 print(response)
 print(response.text)
