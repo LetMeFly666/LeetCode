349f101f72e01d5a511f1c45e9714ad0107f24c3
LetMeFly666 <814114971@qq.com>
2022-07-09 21:45:09 +0800

    去掉了VsCode自动import的包

--- newSolution.py
+++ newSolution.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-07-03 11:21:14
 LastEditors: LetMeFly
-LastEditTime: 2022-07-04 23:17:44
+LastEditTime: 2022-07-09 21:34:52
 Command: python newSolution.py 102. 二叉树的层序遍历
 What's more: 当前仅支持数字开头的题目
 '''
@@ -10,7 +10,6 @@ import sys
 import time
 from urllib.parse import quote
 
-from numpy import linspace
 
 argv = sys.argv
 print(argv)
