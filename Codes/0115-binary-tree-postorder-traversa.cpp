cb4d8825f18a292b60a8b444ee3779ceb99d7362
LetMeFly666 <814114971@qq.com>
2022-07-29 15:56:29 +0800

    添加了问题“145. 二叉树的后序遍历”的代码和题解

---
+++ Codes/0115-binary-tree-postorder-traversa.cpp
@@ -0,0 +1,38 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2022-07-29 15:49:26
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2022-07-29 15:49:26
+ */
+#ifdef _WIN32
+#include "_[1,2]toVector.h"
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> ans;
+
+    void dfs(TreeNode* root) {
+        if (!root)
+            return;
+        dfs(root->left);
+        dfs(root->right);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
