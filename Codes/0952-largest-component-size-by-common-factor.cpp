9568ca08b7093c767901b2992c066ad7058a749b
LetMeFly666 <814114971@qq.com>
2022-07-30 10:17:53 +0800

    Changes & BugFix: 1. 修改静态数组为动态数组(Vector)，否则VsCode的类的函数中无法开辟数个1e5的空间 2. 分解质因数的时候，t=6时k=2，i<k需要修改为i<=k

--- Codes/0952-largest-component-size-by-common-factor.cpp
+++ Codes/0952-largest-component-size-by-common-factor.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2022-07-30 09:54:10
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-07-30 10:11:33
+ * @LastEditTime: 2022-07-30 10:16:04
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -12,29 +12,27 @@ class Solution {
 public:
     int largestComponentSize(vector<int>& nums) {
         // 分解因数到hasThisFactor中
-        vector<int> hasThisFactor[100010];
-        vector<int> num4Factor[100010];
+        vector<vector<int>> hasThisFactor(100010);
+        vector<vector<int>> num4Factor(100010);
         for (int t : nums) {
             int k = sqrt(t);
-            for (int i = 2; i < k; i++) {
+            for (int i = 2; i <= k; i++) {
                 if (t % i == 0) {
                     hasThisFactor[i].push_back(t);
                     num4Factor[t].push_back(i);
-                    hasThisFactor[t / i].push_back(t);
-                    num4Factor[t].push_back(t / i);
+                    if (t / i != i) {
+                        hasThisFactor[t / i].push_back(t);
+                        num4Factor[t].push_back(t / i);
+                    }
                 }
-            }
-            if (k * k == t) {
-                hasThisFactor[k].push_back(t);
-                num4Factor[t].push_back(k);
             }
             // 自己是自己的因数
             hasThisFactor[t].push_back(t);
             num4Factor[t].push_back(t);
         }
         int ans = 0;
-        bool visitedFactor[100010] = {false};
-        bool visitedNum[100010] = {false};
+        vector<bool> visitedFactor(100010, false);
+        vector<bool> visitedNum(100010, false);
         for (int i = 2; i <= 100000; i++) {
             if (hasThisFactor[i].size() && !visitedFactor[i]) {
                 visitedFactor[i] = true;
