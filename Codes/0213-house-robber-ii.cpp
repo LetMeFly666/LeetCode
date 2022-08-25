d6916f5524f705ae9425301daffe59bfc9528414
LetMeFly666 <814114971@qq.com>
2022-08-25 09:14:42 +0800

    0213's code

--- Codes/0213-house-robber-ii.cpp
+++ Codes/0213-house-robber-ii.cpp
@@ -2,11 +2,12 @@
  * @Author: LetMeFly
  * @Date: 2022-08-24 10:56:04
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-08-24 11:00:31
+ * @LastEditTime: 2022-08-25 09:14:23
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
 #endif
+#ifdef FirstTry
 // Half，先不写了
 class Solution {
 public:
@@ -21,3 +22,28 @@ public:
 
     }
 };
+#else  // FirstTry
+// Copy
+class Solution {
+public:
+    int robRange(vector<int>& nums, int start, int end) {
+        int first = nums[start], second = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    int rob(vector<int>& nums) {
+        int length = nums.size();
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return max(nums[0], nums[1]);
+        }
+        return max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+};
+#endif  // FirstTry
