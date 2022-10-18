db39a0531414af384a305130b142f193050d8b9a
LetMeFly666 <814114971@qq.com>
2022-10-18 10:29:41 +0800

    Error Version

---
+++ Codes/0902-numbers-at-most-n-given-digit-set.cpp
@@ -0,0 +1,129 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2022-10-18 09:18:38
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2022-10-18 10:26:23
+ */
+#ifdef _WIN32
+#include "_[1,2]toVector.h"
+#endif
+
+#ifdef FirstTry  // Error  // 每个数字可以选择任意次，不是一次
+class Solution {
+private:
+    int ans;
+
+    int A(int n, int m) {
+        if (n < m)
+            return 0;
+        int ans = 1;
+        for (int i = 0; i < m; i++) {
+            ans *= n - i;
+        }
+        // for (int i = 0; i < m; i++) {
+        //     ans /= i + 1;
+        // }
+        return ans;
+    }
+
+    void dfs(unordered_set<char>& unused, string nowString, int n) {
+        if (nowString.size() == n) {
+            if (atoll(nowString.c_str()) <= n)
+                ans++;
+        }
+        else {
+            for (char c : unused) {
+                unordered_set<char> newSet = unused;
+                newSet.erase(c);
+                nowString += c;
+                dfs(newSet, nowString, n);
+                nowString.pop_back();
+            }
+        }
+    }
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        ans = 0;
+        int len = to_string(n).size();
+        for (int i = 1; i < len; i++) {
+            printf("A(%d, %d) = %d\n", int(digits.size()), i, A(digits.size(), i));
+            ans += A(digits.size(), i);
+        }
+        if (digits.size() >= len) {
+            unordered_set<char> unused;
+            for (string& d : digits) {
+                unused.insert(d[0]);
+            }
+            dfs(unused, "", n);
+        }
+        return ans;
+    }
+};
+#else  // FirstTry
+// SecondTry
+class Solution {
+private:
+    int ans;
+
+    void dfs(vector<char>& chars, string nowString, int n) {
+        // dbg(nowString);
+        if (nowString.size() == to_string(n).size()) {
+            if (atoll(nowString.c_str()) <= n)
+                ans++;
+        }
+        else {
+            for (char c : chars) {
+                nowString += c;
+                dfs(chars, nowString, n);
+                nowString.pop_back();
+            }
+        }
+    }
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        ans = 0;
+        int len = to_string(n).size();
+        for (int i = 1; i < len; i++) {
+            int cnt = digits.size();
+            for (int j = 1; j < i; j++) {
+                cnt *= digits.size();
+            }
+            ans += cnt;
+        }
+        vector<char> chars;
+        for (string& d : digits) {
+            chars.push_back(d[0]);
+        }
+        dfs(chars, "", n);
+        return ans;
+    }
+};
+#endif  // FirstTry
+
+
+#ifdef _WIN32
+/*
+["1","3","5","7"]
+100
+
+*/
+
+/*
+["1"]
+834
+
+*/
+int main() {
+    string s;
+    int n;
+    while (cin >> s >> n) {
+        vector<string> v = stringToVectorString(s);
+        for (string& s : v) {
+            s = s.substr(1, 1);
+        }
+        Solution sol;
+        cout << sol.atMostNGivenDigitSet(v, n) << endl;
+    }
+    return 0;
+}
+#endif
