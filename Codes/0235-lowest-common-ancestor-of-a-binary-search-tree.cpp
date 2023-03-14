5d20f52964b4254b5759add1a95d9ac5d23e551e
LetMeFly666 <814114971@qq.com>
2023-03-14 21:40:19 +0800

    AC，原来是忘记加else了

--- Codes/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ Codes/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -2,7 +2,8 @@
  * @Author: LetMeFly
  * @Date: 2023-03-14 21:37:37
  * @LastEditors: LetMeFly
- * @LastEditTime: 2023-03-14 21:37:38
+ * @LastEditTime: 2023-03-14 21:39:59
+ * @More: From郅非，帮郅非调BUG
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -17,6 +18,15 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+void debug(queue<TreeNode*> a, string name) {
+    cout << name << ":";
+    while (a.size()) {
+        cout << " " << a.front()->val;
+        a.pop();
+    }
+    cout << endl;
+}
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -24,20 +34,24 @@ public:
         queue<TreeNode*> b;
         TreeNode* t=root;
         while(t!=p&&t){
+            printf("t->val = %d\n", t->val);  //******
             a.push(t);
             if(p->val>t->val&&t->right) t=t->right;
-            if(p->val<t->val&&t->left) t=t->left;
+            else if(p->val<t->val&&t->left) t=t->left;
         }
         a.push(t);
         t=root;
         while(t!=q&&t){
             b.push(t);
             if(q->val>t->val&&t->right) t=t->right;
-            if(q->val<t->val&&t->left) t=t->left;
+            else if(q->val<t->val&&t->left) t=t->left;
         }
         b.push(t);
+        debug(a, "a");
+        debug(b, "b");
         TreeNode* ans;
         while(!a.empty()&&!b.empty()){
+            printf("a.front()->val = %d, b.front()->val = %d\n", a.front()->val, b.front()->val);
             if(a.front()==b.front()){
                 ans=a.front();
                 a.pop();
