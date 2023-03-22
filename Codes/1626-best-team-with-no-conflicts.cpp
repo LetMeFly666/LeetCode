0986bd3cc694f70e61f6e29fcefb2de1afe9651b
LetMeFly666 <814114971@qq.com>
2023-03-22 12:13:51 +0800

    Copy了1626的代码

--- Codes/1626-best-team-with-no-conflicts.cpp
+++ Codes/1626-best-team-with-no-conflicts.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2023-03-22 10:07:18
  * @LastEditors: LetMeFly
- * @LastEditTime: 2023-03-22 11:01:56
+ * @LastEditTime: 2023-03-22 12:13:32
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -86,10 +86,24 @@ public:
 // SecondTry
 class Solution {
 public:
-    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+    int bestTeamScore(vector<int> &scores, vector<int> &ages) {
+        int n = scores.size();
+        pair<int, int> a[n];
+        for (int i = 0; i < n; ++i)
+            a[i] = {scores[i], ages[i]};
+        sort(a, a + n);
 
+        int f[n]; memset(f, 0, sizeof(f));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j)
+                if (a[j].second <= a[i].second)
+                    f[i] = max(f[i], f[j]);
+            f[i] += a[i].first;
+        }
+        return *max_element(f, f + n);
     }
 };
+// CopyFrom: https://leetcode.cn/problems/best-team-with-no-conflicts/solutions/2183029/zui-chang-di-zeng-zi-xu-lie-cong-on2-dao-ojqu/
 #endif  // FirstTry
 
 #ifdef _WIN32
