05a686645f539aa61fcb9da76067a6f4407b9c4d
LetMeFly666 <814114971@qq.com>
2023-03-22 10:43:14 +0800

    ChangeBack

--- Codes/_[1,2]toVector.h
+++ Codes/_[1,2]toVector.h
@@ -8,30 +8,13 @@ using namespace std;
 typedef long long ll;
 
 template<class Type>
-void debug(Type a, string end="\n") {
-    try {
-        cout << a << end;
+void debug(vector<Type>v)
+{
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<' ';
     }
-    catch (...) {
-        try {
-            cout << "{" << a.first << ", " << a.second << "}" << end;
-        }
-        catch (...) {
-            cout << "{";
-            bool first = true;
-            for (auto i : a) {
-                if (first) {
-                    first = false;
-                }
-                else {
-                    cout << ", ";
-                }
-                cout << i;
-            }
-            cout << "}"
-            cout << end;
-        }
-    }
+    puts("");
 }
 
 template<class Type>
