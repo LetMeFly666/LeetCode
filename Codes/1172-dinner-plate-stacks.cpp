1bcc0baf4dfb1307f1a835524cf9bbc9c32e1118
LetMeFly666 <814114971@qq.com>
2023-04-28 10:32:47 +0800

    Copy了1172餐盘栈的代码

--- Codes/1172-dinner-plate-stacks.cpp
+++ Codes/1172-dinner-plate-stacks.cpp
@@ -16,6 +16,7 @@
  * int param_2 = obj->pop();
  * int param_3 = obj->popAtStack(index);
  */
+#ifdef FirstTry  // Error
 class DinnerPlates {
 private:
     int capacity;
@@ -78,3 +79,77 @@ public:
         return v[pos];
     }
 };
+#else  // FirstTry
+// SecondTry  // Copy
+class DinnerPlates {
+public:
+    DinnerPlates(int capacity) {
+        this->capacity = capacity;
+    }
+
+    void push(int val) {
+        if (poppedPos.empty()) {
+            int pos = stk.size();
+            stk.emplace_back(val);
+            if (pos % capacity == 0) {
+                top.emplace_back(0);
+            } else {
+                top.back()++;
+            }
+        } else {
+            int pos = *poppedPos.begin();
+            poppedPos.erase(pos);
+            stk[pos] = val;
+            int index = pos / capacity;
+            top[index]++;
+        }
+    }
+    
+    int pop() {
+        while (!stk.empty() && poppedPos.count(stk.size() - 1)) {
+            stk.pop_back();
+            int pos = *poppedPos.rbegin();
+            poppedPos.erase(pos);
+            if (pos % capacity == 0) {
+                top.pop_back();
+            }
+        }
+        if (stk.empty()) {
+            return -1;
+        } else {
+            int pos = stk.size() - 1;
+            int val = stk.back();
+            stk.pop_back();
+            if (pos % capacity == 0) {
+                top.pop_back();
+            } else {
+                top.back() = top.size() - 2;
+            }
+            return val;
+        }
+    }
+    
+    int popAtStack(int index) {
+        if (index >= top.size()) {
+            return -1;
+        }
+        int stackTop = top[index];
+        if (stackTop < 0) {
+            return -1;
+        }
+        top[index]--;
+        int pos = index * capacity + stackTop;
+        poppedPos.emplace(pos);
+        return stk[pos];
+    }
+private:
+    int capacity;
+    vector<int> stk;
+    vector<int> top;
+    set<int> poppedPos;
+};
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/dinner-plate-stacks/solutions/2247703/can-pan-zhan-by-leetcode-solution-afsw/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+#endif  // FirstTry
