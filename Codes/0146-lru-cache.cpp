49010840643da6f4e7c1f12b0086e4a8fba6a7a5
LetMeFly666 <814114971@qq.com>
2023-09-24 13:46:42 +0800

    0146-v2-error: get也会刷新最近使用时间

--- Codes/0146-lru-cache.cpp
+++ Codes/0146-lru-cache.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2023-09-24 13:04:40
  * @LastEditors: LetMeFly
- * @LastEditTime: 2023-09-24 13:14:23
+ * @LastEditTime: 2023-09-24 13:43:03
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -10,24 +10,26 @@
 
 class Node {
 public:
-    Node* next, *previous;
-    int value;
-    Node(Node* next, Node* previous, int value) {
-        this->next = next;
+    Node* previous, *next;
+    int key, value;
+    Node(Node* previous, Node* next, int key, int value) {
         this->previous = previous;
+        this->next = next;
+        this->key = key;
         this->value = value;
     }
 };
 
 class LRUCache {
 private:
-    Node* head;
-    Node* tail;
+    Node* head, *tail;
     int capacity;
     unordered_map<int, Node*> ma;
 public:
     LRUCache(int capacity) {
-        tail = head = new Node(nullptr, nullptr, 0);
+        head = new Node(nullptr, nullptr, 0, 0);
+        tail = new Node(head, nullptr, 0, 0);
+        head->next = tail;
         this->capacity = capacity;
     }
     
@@ -42,29 +44,33 @@ public:
         if (ma.count(key)) {
             Node* thisNode = ma[key];
             thisNode->value = value;
-            Node* next = thisNode->next;
-            Node* previous = thisNode->previous;
-            previous->next = next;
-            if (next) {
-                next->previous = previous;
-            }
+            Node* previous = thisNode->previous, *next = thisNode->next;
+            previous->next = next, next->previous = previous;
             thisNode->next = head->next;
-            thisNode->previous = head;
             head->next = thisNode;
+            thisNode->next->previous = thisNode;
         }
         else {
-            Node* thisNode = new Node(head->next, head, value);
-            if (head->next) {
-                head->next->previous = thisNode;
-            }
-            head->next = thisNode;
+            Node* thisNode = new Node(head, head->next, key, value);
             ma[key] = thisNode;
+            head->next = thisNode, thisNode->next->previous = thisNode;
             if (ma.size() > capacity) {
-                
+                Node* toRemove = tail->previous;
+                ma.erase(toRemove->key);
+                toRemove->previous->next = tail;
+                tail->previous = toRemove->previous;
+                delete toRemove;
             }
         }
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
 
 /**
  * Your LRUCache object will be instantiated and called as such:
