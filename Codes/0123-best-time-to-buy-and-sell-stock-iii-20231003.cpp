3c9f625d0e817f857fe6df432ad015e6a251459c
LetMeFly666 <814114971@qq.com>
2023-10-03 17:43:21 +0800

    更新了0123的C++代码

--- Codes/0123-best-time-to-buy-and-sell-stock-iii-20231003.cpp
+++ Codes/0123-best-time-to-buy-and-sell-stock-iii-20231003.cpp
@@ -14,11 +14,11 @@ public:
         int buy1 = -prices[0], sell1 = 0, buy2 = -prices[0], sell2 = 0;
         for (int i = 1; i < prices.size(); i++) {
             int buy1new = max(buy1, -prices[i]);
-            int sell1new = max(sell1,  sell1 + prices[i]);
+            int sell1new = max(sell1,  buy1 + prices[i]);
             int buy2new = max(buy2, sell1 - prices[i]);
             int sell2new = max(sell2, buy2 + prices[i]);
             buy1 = buy1new, sell1 = sell1new, buy2 = buy2new, sell2 = sell2new;
         }
-        return max(buy1, buy2);
+        return sell2;
     }
 };
