c8bafe455fbc6224d1df903833b9b7d633227dbc
LetMeFly666 <814114971@qq.com>
2023-10-13 14:30:01 +0800

    添加了 问题“1488. 避免洪水泛滥”的代码和题解

--- Codes/1488-avoid-flood-in-the-city.cpp
+++ Codes/1488-avoid-flood-in-the-city.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2023-10-13 09:27:50
  * @LastEditors: LetMeFly
- * @LastEditTime: 2023-10-13 09:36:44
+ * @LastEditTime: 2023-10-13 13:55:12
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -11,39 +11,25 @@
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
-        unordered_set<int> whichRained;
-        vector<int> todolist;
-        int cnt0 = 0;
-        for (int t : rains) {
-            if (!t) {
-                cnt0++;
+        set<int> whenNotRain;
+        unordered_map<int, int> whichAndWhen;
+        vector<int> ans(rains.size(), 1);  // 没有0号湖
+        for (int i = 0; i < rains.size(); i++) {
+            if (!rains[i]) {
+                whenNotRain.insert(i);
                 continue;
             }
-            if (whichRained.count(t)) {
-                if (cnt0 <= 0) {
+            ans[i] = -1;
+            if (whichAndWhen.count(rains[i])) {
+                int lastRainDay = whichAndWhen[rains[i]];  // 找一个lastRainDay后的晴天
+                set<int>::iterator it = whenNotRain.upper_bound(lastRainDay);
+                if (it == whenNotRain.end()) {
                     return {};
                 }
-                todolist.push_back(t);
-                cnt0--;
+                ans[*it] = rains[i];
+                whenNotRain.erase(it);
             }
-            else {
-                whichRained.insert(t);
-            }
-        }
-        vector<int> ans(rains.size());
-        int dotowho = 0;
-        for (int i = 0; i < rains.size(); i++) {
-            if (rains[i]) {
-                ans[i] = -1;
-            }
-            else {
-                if (dotowho >= todolist.size()) {
-                    ans[i] = 1;  // 随便选一个抽水就行
-                }
-                else {
-                    ans[i] = todolist[dotowho++];
-                }
-            }
+            whichAndWhen[rains[i]] = i;
         }
         return ans;
     }
