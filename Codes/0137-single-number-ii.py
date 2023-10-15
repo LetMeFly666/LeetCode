f5f17a32f0fb582fc53a238dfe62a7ab370589be
LetMeFly666 <814114971@qq.com>
2023-10-15 22:01:12 +0800

    v2error

--- Codes/0137-single-number-ii.py
+++ Codes/0137-single-number-ii.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2023-10-15 19:37:29
 LastEditors: LetMeFly
-LastEditTime: 2023-10-15 19:39:33
+LastEditTime: 2023-10-15 22:00:21
 '''
 from typing import List
 
@@ -13,6 +13,7 @@ class Solution:
             for i in range(32):
                 cnt[i] += ((t >> i) & 1)
         ans = 0
-        for i in range(32):
+        for i in range(31):
             ans += (1 << i) * (cnt[i] % 3)
+        ans -= (1 << i) * (cnt[31] % 3)  
         return ans
