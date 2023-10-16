a0094f08e9e3f7c1576fadb5878c0498511ce6cb
LetMeFly666 <814114971@qq.com>
2023-10-16 22:45:52 +0800

    firstVersion: -temp overflow了

---
+++ Codes/0260-single-number-iii-20231016.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2023-10-16 22:38:39
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2023-10-16 22:43:27
+ */
+#ifdef _WIN32
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int temp = 0;
+        for (int t : nums) {
+            temp ^= t;
+        }
+        int mask = temp & (-temp);
+        vector<int> ans(2);
+        for (int t : nums) {
+            ans[(t & mask) != 0] ^= t;
+        }
+        return ans;
+    }
+};
