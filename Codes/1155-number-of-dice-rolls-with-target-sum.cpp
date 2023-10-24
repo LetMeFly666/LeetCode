b804c1de93acdc05ef166405ac8fc9064889e046
LetMeFly666 <814114971@qq.com>
2023-10-24 22:45:59 +0800

    ac-by phone

--- Codes/1155-number-of-dice-rolls-with-target-sum.cpp
+++ Codes/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2023-10-24 14:29:37
  * @LastEditors: LetMeFly
- * @LastEditTime: 2023-10-24 14:32:26
+ * @LastEditTime: 2023-10-24 20:22:54
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -14,16 +14,16 @@ class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
         vector<vector<ll>> dp(n + 1, vector<ll>(target + 1, 0));
-        for (int j = 1; j <= k; j++) {
+        for (int j = 1; j <= min(k, target); j++) {
             dp[1][j] = 1;
         }
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= target; j++) {
-                for (int _k = 1; k <= min(k, j); _k++) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+                for (int _k = 1; _k <= min(k, j); _k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - _k]) % MOD;
                 }
             }
         }
-        return dp[k][target];
+        return dp[n][target];
     }
 };
