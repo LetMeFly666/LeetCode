25f650124b3683cd37581a4d9dca8a5a95614d29
LetMeFly666 <814114971@qq.com>
2024-01-14 11:12:30 +0800

    v2: AC 但其实不会删头节点，因此无需新增临时头节点

--- Codes/0083-remove-duplicates-from-sorted-list_20240114.cpp
+++ Codes/0083-remove-duplicates-from-sorted-list_20240114.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2024-01-14 11:06:50
  * @LastEditors: LetMeFly
- * @LastEditTime: 2024-01-14 11:08:01
+ * @LastEditTime: 2024-01-14 11:11:24
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -17,7 +17,9 @@ public:
             if (head->val == head->next->val) {
                 head->next = head->next->next;  // haven't delete node
             }
-            head = head->next;
+            else {
+                head = head->next;
+            }
         }
         return ans->next;
     }
