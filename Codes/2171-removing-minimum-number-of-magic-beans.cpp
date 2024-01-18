571fcbb76a34e5a5efc119fa9005596234e1cf0d
LetMeFly666 <814114971@qq.com>
2024-01-18 19:42:28 +0800

    添加了问题“2171.拿出最少数目的魔法豆”的代码和题解

--- Codes/2171-removing-minimum-number-of-magic-beans.cpp
+++ Codes/2171-removing-minimum-number-of-magic-beans.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2024-01-18 19:20:51
  * @LastEditors: LetMeFly
- * @LastEditTime: 2024-01-18 19:30:51
+ * @LastEditTime: 2024-01-18 19:32:10
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -12,7 +12,7 @@ class Solution {
 public:
     long long minimumRemoval(vector<int>& beans) {
         sort(beans.begin(), beans.end());
-        long long all = accumulate(beans.begin(), beans.end(), 0);
+        long long all = accumulate(beans.begin(), beans.end(), 0LL);
         long long ans = all;
         for (int i = 0; i < beans.size(); i++) {
             ans = min(ans, all - ((long long)beans.size() - i) * beans[i]);
--- Codes/2171-removing-minimum-number-of-magic-beans.py
+++ Codes/2171-removing-minimum-number-of-magic-beans.py
@@ -2,12 +2,13 @@
 Author: LetMeFly
 Date: 2024-01-18 19:32:37
 LastEditors: LetMeFly
-LastEditTime: 2024-01-18 19:34:06
+LastEditTime: 2024-01-18 19:35:01
 '''
 from typing import List
 
 class Solution:
     def minimumRemoval(self, beans: List[int]) -> int:
+        beans.sort()
         all_ = sum(beans)
         ans = all_
         for i, thisBean in enumerate(beans):
