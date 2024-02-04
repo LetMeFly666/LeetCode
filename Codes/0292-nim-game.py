5f126b773ebdf4a01a6460adad2e06b95be3a12f
LetMeFly666 <814114971@qq.com>
2024-02-04 13:39:52 +0800

    v1: error 虽然是->bool，但return 的0不等于False

---
+++ Codes/0292-nim-game.py
@@ -0,0 +1,9 @@
+'''
+Author: LetMeFly
+Date: 2024-02-04 13:38:25
+LastEditors: LetMeFly
+LastEditTime: 2024-02-04 13:38:27
+'''
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return n % 4
