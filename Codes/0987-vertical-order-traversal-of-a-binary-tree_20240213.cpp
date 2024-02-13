6f33a8b17c1744e1df0949c9fa7a31a67ad22e77
LetMeFly666 <814114971@qq.com>
2024-02-13 11:27:01 +0800

    v2: AC, 这次和2021年7月的代码相差就很大了
    当时 大括号在另一行、没有手动空格、没有使用__[1,2]toVector.h、**还是竞赛思维**
    并且当时是：最多1000列，最多往左1000，最多往右1000。第0列存到v[1000]，v[y]存放所有第y列的节点。列处理上相当于是桶排序，对于每一列先按深度后按大小排序，一列一列地加入到答案数组中。

--- Codes/0987-vertical-order-traversal-of-a-binary-tree_20240213.cpp
+++ Codes/0987-vertical-order-traversal-of-a-binary-tree_20240213.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2024-02-13 11:03:59
  * @LastEditors: LetMeFly
- * @LastEditTime: 2024-02-13 11:10:24
+ * @LastEditTime: 2024-02-13 11:16:19
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -19,31 +19,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+typedef pair<TreeNode*, pair<int, int>> NodeInfo;
+
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode*, int>> q;  // [<node, col>, ...
-        q.push({root, 0});
-        vector<pair<TreeNode*, int>> v;
+        queue<NodeInfo> q;  // [<node, <col, height>>, ...
+        q.push({root, {0, 1}});
+        vector<NodeInfo> v;
         while (q.size()) {
-            pair<TreeNode*, int> thisNode = q.front();
+            NodeInfo thisNode = q.front();
             q.pop();
             v.push_back(thisNode);
             if (thisNode.first->left) {
-                q.push({thisNode.first->left, thisNode.second - 1});
+                q.push({thisNode.first->left, {thisNode.second.first - 1, thisNode.second.second + 1}});
             }
             if (thisNode.first->right) {
-                q.push({thisNode.first->right, thisNode.second + 1});
+                q.push({thisNode.first->right, {thisNode.second.first + 1, thisNode.second.second + 1}});
             }
         }
-        sort(v.begin(), v.end(), [&](const pair<TreeNode*, int>& a, const pair<TreeNode*, int>& b) {
+        sort(v.begin(), v.end(), [&](const NodeInfo& a, const NodeInfo& b) {
+            // if (a.second.first != b.second.first) {  // not same col
+            //     return a.second.first < b.second.first;
+            // }
+            // if (a.second.second != b.second.second) {  // same col, different row
+            //     return a.second.second < b.second.second;
+            // }
+            // return a.first->val < b.first->val;
             return a.second == b.second ? a.first->val < b.first->val : a.second < b.second;
         });
         vector<vector<int>> ans;
-        int lastLayer = 1000000;
-        for (pair<TreeNode*, int>& a : v) {
-            if (a.second != lastLayer) {
-                lastLayer = a.second;
+        int lastCol = 1000000;
+        for (NodeInfo& a : v) {
+            if (a.second.first != lastCol) {
+                lastCol = a.second.first;
                 ans.push_back({});
             }
             ans.back().push_back(a.first->val);
