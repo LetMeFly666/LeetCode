af2bcc003b7932063f5ac0150f7e6ffa19107e8e
LetMeFly666 <814114971@qq.com>
2024-05-13 16:09:44 +0800

    -s

---
+++ Codes/0994-sf_method2.c
@@ -0,0 +1,6 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2024-05-13 15:12:04
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2024-05-13 15:12:21
+ */
--- newSolution.py
+++ newSolution.py
@@ -2,13 +2,15 @@
 Author: LetMeFly
 Date: 2022-07-03 11:21:14
 LastEditors: LetMeFly
-LastEditTime: 2024-03-28 17:33:27
+LastEditTime: 2024-05-13 16:04:57
 Command: python newSolution.py 102. 二叉树的层序遍历
 What's more: 当前仅支持数字开头的题目
 '''
 import os
+import re
 import sys
 import time
+import datetime
 from urllib.parse import quote
 
 
@@ -34,7 +36,39 @@ print(solutionURLll)
 with open(nameProblem, "r", encoding="utf-8") as f:
     problem = f.read()
 
-solution = problem + """
+
+def genSolutionPart(num):
+    suffix2markdowncode = {
+        'cpp': ('cpp', 'C++'),
+        'py': ('python', 'Python'),
+        'c': ('c', 'C语言'),
+    }
+    today4code = []
+    for file in os.listdir('Codes'):
+        first = file.split('-')[0]
+        try:
+            first = int(first)
+        except:
+            pass
+        if first != num:
+            continue
+        print(file)
+        with open(os.path.join('Codes', file), 'r', encoding='utf-8') as f:
+            data = f.read()
+        time_pattern = re.compile(r"(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})")
+        match = time_pattern.search(data)
+        if not match:
+            print('time not found')
+            continue
+        year, month, day, hour, miunte, second = map(int, match.groups())
+        fileDay = f'{year:04d}-{day:02d}-{month:02d}'
+        today = datetime.date.today().strftime('%Y-%d-%m')
+        if fileDay != today:
+            print(f'{fileDay} != {today}')
+            continue
+        today4code.append(os.path.join('Codes', file))
+    # TODO: 一题两解的支持
+    result = """
 ## 解题方法：xx
 
 11111
@@ -43,24 +77,25 @@ solution = problem + """
 + 空间复杂度$O(N\log N)$
 
 ### AC代码
+"""
+    for file in today4code:
+        fileType = os.path.splitext(file)[-1]
+        if fileType.startswith('.'):
+            fileType = fileType[1:]
+        markdowncode = suffix2markdowncode[fileType]
+        with open(file, 'r', encoding='utf-8') as f:
+            data = f.read()
+        # data = removePrefix(data, fileType)  # TODO: 移除前面注释以及其他头部文件
+        result += f'\n#### {markdowncode[1]}\n\n```{markdowncode[0]}\n{data}\n```\n'
+    return result
 
-#### C++
 
-```cpp
-
-```
-
-#### Python
-
-```python
-
-```
-
+solution = problem + genSolutionPart(num) +"""
 > 同步发文于CSDN和我的[个人博客](https://blog.letmefly.xyz/)，原创不易，转载经作者同意后请附上[原文链接]({0})哦~
 > Tisfy：[https://letmefly.blog.csdn.net/article/details/--------------------------](https://letmefly.blog.csdn.net/article/details/--------------------------)
 """.format(solutionURLll)
 
-def refreshPublistTime(solution: str) -> solution:
+def refreshPublistTime(solution: str) -> str:
     splited = solution.split("\n")
     splited[2] = "date: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return "\n".join(splited)
@@ -72,6 +107,7 @@ solutionName = "Solutions/LeetCode {0:04d}.{1}.md".format(num, title)
 with open(solutionName, "x", encoding="utf-8") as f:
     f.write(solution)
 
+exit(0)
 # 认领issue
 os.system(f'git checkout -b {num}')
 issueCreateResult = os.popen(f'gh issue create -t "Who can add 1 more problem of {num}" -b "By newSolution.py using GH"').read()
