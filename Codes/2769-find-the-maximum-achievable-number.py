7a0bf686e2f670963a9f664cb0a9cc6ab8ea1479
LetMeFly666 <814114971@qq.com>
2024-05-21 20:40:47 +0800

    update: 添加问题“2769.找出最大的可达成数字”的代码和题解

---
+++ Codes/2769-find-the-maximum-achievable-number.py
@@ -0,0 +1,9 @@
+'''
+Author: LetMeFly
+Date: 2024-05-21 20:35:45
+LastEditors: LetMeFly
+LastEditTime: 2024-05-21 20:35:52
+'''
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + t * 2
