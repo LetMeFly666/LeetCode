0d0e38cfe90b54466eff72884ad400736d237b0b
LetMeFly666 <814114971@qq.com>
2025-03-05 23:41:14 +0800

    update: 添加问题“283.移动零”的代码和题解
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/0283-move-zeroes_20250305.cpp
+++ Codes/0283-move-zeroes_20250305.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-03-05 22:52:56
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-03-05 23:01:32
+ * @LastEditTime: 2025-03-05 23:13:24
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -11,21 +11,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (int l = 0, r = 0; r < nums.size(); l++) {
-            while (l < nums.size() && nums[l]) {  // 到第一个0
-                l++;
+        int loc0 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]) {
+                swap(nums[i], nums[loc0++]);
             }
-            if (l == nums.size()) {
-                break;
-            }
-            r = l + 1;
-            while (r < nums.size() && !nums[r]) {  // 到第一个非零
-                r++;
-            }
-            if (r == nums.size()) {
-                break;
-            }
-            swap(nums[l], nums[r]);
         }
     }
 };
