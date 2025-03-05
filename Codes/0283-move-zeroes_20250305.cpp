610f5852a7357548cc9b30b76e2c334f0f475ee9
LetMeFly666 <814114971@qq.com>
2025-03-05 23:05:48 +0800

    refactor.before: 写得一点都不优雅
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/0283-move-zeroes_20250305.cpp
+++ Codes/0283-move-zeroes_20250305.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-03-05 22:52:56
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-03-05 22:54:50
+ * @LastEditTime: 2025-03-05 23:01:32
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -11,14 +11,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (int l = 0, r = nums.size() - 1; l < r; l++) {
-            while (r > l && !nums[r]) {
-                r--;
+        for (int l = 0, r = 0; r < nums.size(); l++) {
+            while (l < nums.size() && nums[l]) {  // 到第一个0
+                l++;
             }
-            if (!nums[l]) {
-                nums[l] = nums[r];
-                nums[r--] = 0;
+            if (l == nums.size()) {
+                break;
             }
+            r = l + 1;
+            while (r < nums.size() && !nums[r]) {  // 到第一个非零
+                r++;
+            }
+            if (r == nums.size()) {
+                break;
+            }
+            swap(nums[l], nums[r]);
         }
     }
 };
