70cc4dbf6639cdddf462b1c4fe53b4b3e4c05ff2
LetMeFly666 <814114971@qq.com>
2025-03-23 10:34:51 +0800

    v3: WA
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,108 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-03-23 09:37:28
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-03-23 10:32:35
+ */
+#ifdef _WIN32
+#include "_[1,2]toVector.h"
+#endif
+
+/*
+*)*(**
+()(())
+
+
+(*)*
+(())
+
+
+(**(
+xxxx
+
+(*
+()
+
+
+
+*/
+// class Solution {
+// public:
+//     bool canBeValid(string s, string locked) {
+//         int custome = 0, needRight;
+//         for (int i = 0; i < locked.size(); i++) {
+//             if (locked[i] == '0') {
+//                 custome++;
+//             } else if (s[i] == ')') {
+//                 if (!custome && !needRight) {
+//                     return false;
+//                 }
+//                 if (needRight) {
+//                     needRight--;
+//                 } else {
+//                     custome--;
+//                 }
+//             } else {  // s[i] is '(' and locked
+//                 needRight++;
+//             }
+//         }
+//         return custome
+//     }
+// };
+
+/*
+custome: 当前有多少个没被预约的*
+needRight: 还需要多少个)
+如果遇到)就直接消耗一个needRight
+如果遇到*就先累积起来到reserved，看会不会碰到)
+*/
+// class Solution {
+// public:
+//     bool canBeValid(string s, string locked) {
+//         int custome = 0, needRight = 0, reserved = 0;
+//         for (int i = 0; i < locked.size(); i++) {
+//             if (locked[i] == '0') {
+//                 if (needRight) {
+//                     needRight--;
+//                     reserved++;
+//                 } else {
+//                     custome++;
+//                 }
+//             } else if (s[i] == ')') {
+//                 if (needRight) {
+//                     needRight--;
+//                 } else if ()
+//             } else {
+                
+//             }
+//         }
+//     }
+// };
+
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int l = 0, r = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (locked[i]) {
+                if (s[i] == '(') {
+                    l++, r++;
+                } else {
+                    l--, r--;
+                    if (r < 0) {
+                        return false;
+                    }
+                    if (l < 0) {
+                        l = r % 2 ? 1 : 0;
+                    }
+                }
+            } else {  // *
+                l--, r++;
+                if (l < 0) {
+                    l = r % 2 ? 1 : 0;
+                }
+            }
+        }
+        return !l;
+    }
+};
