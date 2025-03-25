5db07b9ce19b70f502027cd53b1f38ddd307d1b9
LetMeFly666 <814114971@qq.com>
2025-03-25 21:18:07 +0800

    2711: AC.cpp - AC,100.00%,100.00%
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2711-difference-of-number-of-distinct-values-on-diagonals_WeiYunSuan-O1KongJian.cpp
+++ Codes/2711-difference-of-number-of-distinct-values-on-diagonals_WeiYunSuan-O1KongJian.cpp
@@ -2,7 +2,8 @@
  * @Author: LetMeFly
  * @Date: 2025-03-25 21:06:53
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-03-25 21:14:52
+ * @LastEditTime: 2025-03-25 21:17:48
+ * @Description: AC,100.00%,100.00%
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -15,19 +16,19 @@ class Solution {
 public:
     vector<vector<int>> differenceOfDistinctValues(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 0));
+        vector<vector<int>> ans(n, vector<int>(m));
         for (int k = 0; k < m + n - 1; k++) {
             int i = k < m ? 0 : k - m + 1, j = k < m ? k : 0;
             ll se = 0;
             int d = 0;
             for (; i + d < n && j + d < m; d++) {
                 ans[i + d][j + d] = __builtin_popcountll(se);
-                se |= 1LL << ans[i + d][j + d];
+                se |= 1LL << grid[i + d][j + d];
             }
             se = 0;
             for (d--; d >= 0; d--) {
                 ans[i + d][j + d] = abs(ans[i + d][j + d] - __builtin_popcountll(se));
-                se |= 1LL << ans[i + d][j + d];
+                se |= 1LL << grid[i + d][j + d];
             }
         }
         return ans;
