c5e572da8b65666ad3518255bc4fefebdb2c21c2
LetMeFly666 <814114971@qq.com>
2025-03-25 21:28:42 +0800

    2711: WA.Java
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2711-difference-of-number-of-distinct-values-on-diagonals_WeiYunSuan-O1KongJian.java
+++ Codes/2711-difference-of-number-of-distinct-values-on-diagonals_WeiYunSuan-O1KongJian.java
@@ -2,14 +2,14 @@
  * @Author: LetMeFly
  * @Date: 2025-03-25 21:23:51
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-03-25 21:26:25
+ * @LastEditTime: 2025-03-25 21:28:30
  */
 class Solution {
     public int[][] differenceOfDistinctValues(int[][] grid) {
         int n = grid.length, m = grid[0].length;
         int[][] ans = new int[n][m];
         for (int k = 0; k < m + n - 1; k++) {
-            int i = k < m ? 0 : m - k + 1;
+            int i = k < m ? 0 : k - m + 1;
             int j = k < m ? m : 0;
             long se = 0;
             int d = 0;
