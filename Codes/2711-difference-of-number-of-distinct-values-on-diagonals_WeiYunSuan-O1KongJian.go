563e7e5901e28fc7e575a0996862aca6639a16ec
LetMeFly666 <814114971@qq.com>
2025-03-25 21:35:32 +0800

    2711: 编译错误.Golang
    Line 32: Char 33: undefined: abs (solution.go)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2711-difference-of-number-of-distinct-values-on-diagonals_WeiYunSuan-O1KongJian.go
@@ -0,0 +1,37 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-03-25 21:29:42
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-03-25 21:34:29
+ */
+package main
+
+import "math/bits"
+
+func differenceOfDistinctValues(grid [][]int) [][]int {
+    n, m := len(grid), len(grid[0])
+    ans := make([][]int, n)
+    for i := range ans {
+        ans[i] = make([]int, m)
+    }
+    for k := 0; k < m + n - 1; k++ {
+        var i, j int
+        if k < m {
+            i, j = 0, k
+        } else {
+            i, j = k - m + 1, 0
+        }
+        se := uint64(0)
+        d := 0
+        for ; i + d < n && j + d < m; d++ {
+            ans[i + d][j + d] = bits.OnesCount64(se)
+            se |= uint64(1) << ans[i + d][j + d]
+        }
+        se = 0
+        for d--; d >= 0; d-- {
+            ans[i + d][j + d] = abs(ans[i + d][j + d] - bits.OnesCount64(se))
+            se |= uint64(1) << ans[i + d][j + d]
+        }
+    }
+    return ans
+}
