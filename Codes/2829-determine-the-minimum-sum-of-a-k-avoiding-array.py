8f85dc3064aba43776febbe69dcf12d42dbfe0fd
LetMeFly666 <814114971@qq.com>
2025-03-26 13:48:42 +0800

    2829: AC.py - AC,64.10%,74.36%
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2829-determine-the-minimum-sum-of-a-k-avoiding-array.py
+++ Codes/2829-determine-the-minimum-sum-of-a-k-avoiding-array.py
@@ -6,6 +6,6 @@ LastEditTime: 2025-03-26 13:47:07
 '''
 class Solution:
     def minimumSum(self, n: int, k: int) -> int:
-        to = min(n, k / 2)
+        to = min(n, k // 2)
         n -= to
-        return to * (to + 1) / 2 + n * (k + k + n - 1) / 2
+        return to * (to + 1) // 2 + n * (k + k + n - 1) // 2
