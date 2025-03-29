2d89be8ff3fbca1fcdbd18af0db0d12379d9f8c0
LetMeFly666 <814114971@qq.com>
2025-03-29 11:33:19 +0800

    2360: WA.cpp
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2360-longest-cycle-in-a-graph.cpp
+++ Codes/2360-longest-cycle-in-a-graph.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-03-29 10:47:24
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-03-29 11:25:28
+ * @LastEditTime: 2025-03-29 11:32:15
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -33,13 +33,13 @@ public:
         int cnt = 1;
         vector<int> visited(edges.size());
         for (int i = 0; i < edges.size(); i++) {
-            // int begin = cnt;
+            int begin = cnt;
             while (edges[i] != -1 && !visited[i]) {
                 visited[i] = cnt++;
                 printf("visited[%d] = %d, next: %d\n", i, visited[i], edges[i]);  // ************
                 i = edges[i];
             }
-            if (edges[i] != -1) {
+            if (edges[i] != -1 && visited[i] > begin) {
                 printf("i = %d, edges[%d] = %d\n", i, i, edges[i]);  // ****
                 // printf("cnt - begin = %d - %d = %d\n", cnt, begin, cnt - begin);  // *******
                 printf("cnt - edges[i] = %d - %d = %d\n", cnt, edges[i], cnt - edges[i]);  // *******
