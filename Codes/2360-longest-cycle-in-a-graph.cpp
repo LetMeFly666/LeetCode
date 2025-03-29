e5427b08bb7d7f4dff9847a5dcc540d9e2f3837d
LetMeFly666 <814114971@qq.com>
2025-03-29 11:42:40 +0800

    2360: debug.cpp C++和Python不一样，for循环中改变i了是能作用到条件括号中的
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2360-longest-cycle-in-a-graph.cpp
+++ Codes/2360-longest-cycle-in-a-graph.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-03-29 10:47:24
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-03-29 11:34:42
+ * @LastEditTime: 2025-03-29 11:38:10
  */
 #ifdef _WIN32
 #include "_[1,2]toVector.h"
@@ -19,12 +19,25 @@
         ↓        |
         2 -------+
 */
+
 /*
  0  1 2 3
 [2,-1,3,1]
 
 0-->2-->3-->1
 
+*/
+
+/*
+ 0 1 2 3  4  5
+[5,2,3,1,-1,-1]
+
+
+0-->5      4
+
+   1---->2
+    ↖ ↙
+      3
 */
 class Solution {
 public:
@@ -34,11 +47,16 @@ public:
         vector<int> visited(edges.size());
         for (int i = 0; i < edges.size(); i++) {
             int begin = cnt;
+            printf("begin: %d\n", begin);  // *******
             while (edges[i] != -1 && !visited[i]) {
                 visited[i] = cnt++;
+                printf("visited[%d] = %d, next: %d\n", i, visited[i], edges[i]);  // ************
                 i = edges[i];
             }
             if (edges[i] != -1 && visited[i] >= begin) {
+                printf("i = %d, edges[%d] = %d\n", i, i, edges[i]);  // ****
+                // printf("cnt - begin = %d - %d = %d\n", cnt, begin, cnt - begin);  // *******
+                printf("cnt - edges[i] = %d - %d = %d\n", cnt, edges[i], cnt - edges[i]);  // *******
                 ans = max(ans, cnt - visited[i]);
             }
         }
