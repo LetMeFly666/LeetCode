e03d3be86a6be099b0c02da2d3148e16f9fdb4b3
LetMeFly666 <814114971@qq.com>
2025-04-12 08:30:37 +0800

    3272: 将错再错-字符串前半段的枚举包含奇数长度的中间值 (#867)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3272-find-the-count-of-good-integers.cpp
+++ Codes/3272-find-the-count-of-good-integers.cpp
@@ -2,21 +2,21 @@
  * @Author: LetMeFly
  * @Date: 2025-04-12 07:51:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-04-12 08:19:56
+ * @LastEditTime: 2025-04-12 08:29:37
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
 /*
-1 -> ''
+1 -> 1-9
 2 -> 1-9
-3 -> 1-9
+3 -> 10->99
 4 -> 10->99
-5 -> 10->99
+5 -> 100->999
 6 -> 100->999
 
-n -> [10^(n/2-1), 10^(n/2)-1)
+n -> [10^((n-1)/2-1), 10^((n-1)/2)-1)
 */
 typedef long long ll;
 
@@ -35,15 +35,9 @@ public:
         int half = n / 2;
         ll ans = 0;
         for (int start = pow(10, half - 1), end = start * 10; start < end; start++) {
-            string prefix = to_string(start), suffix = prefix;
+            string prefix = to_string(start), suffix = prefix.substr(0, prefix.size() - n % 2);
             reverse(suffix.begin(), suffix.end());
-            if (n % 2) {
-                for (int i = 0; i < 10; i++) {
-                    ans += isOk(prefix + char('0' + i) + suffix, k);
-                }
-            } else {
-                ans += isOk(prefix + suffix, k);
-            }
+            ans += isOk(prefix + suffix, k);
         }
         return ans;
     }
