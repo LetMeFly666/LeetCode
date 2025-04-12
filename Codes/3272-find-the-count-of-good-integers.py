c96f27f7dbdfcffa3b3f20276a64a0c068638130
LetMeFly666 <814114971@qq.com>
2025-04-12 10:53:26 +0800

    3272: AC.py - AC,32.00%,100.00%(#867)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3272-find-the-count-of-good-integers.py
+++ Codes/3272-find-the-count-of-good-integers.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-04-12 09:44:25
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-04-12 10:37:48
+LastEditTime: 2025-04-12 10:52:36
 Description: 中间终中断了下
 '''
 from collections import Counter
@@ -21,8 +21,8 @@ class Solution:
     def calc(self, s: str) -> int:
         times = Counter(s)
         ans = (len(s) - times['0']) * self.factor[len(s) - 1]
-        for i in range(10):
-            ans //= self.factor[times[chr(ord('0') + i)]]
+        for _, val in times.items():
+            ans //= self.factor[val]
         return ans
 
     def countGoodIntegers(self, n: int, k: int) -> int:
@@ -35,7 +35,7 @@ class Solution:
         ans = 0
         for i in range(base, base * 10):
             prefix = str(i)
-            s = prefix[::-1][n % 2:]
+            s = prefix + prefix[::-1][n % 2:]
             if self.isOk(s) and not self.ifVisited(s):
                 ans += self.calc(s)
         return ans
