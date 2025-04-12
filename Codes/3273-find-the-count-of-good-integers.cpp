76c7c78f5cf69125595b06dc81bef629c662de19
LetMeFly666 <814114971@qq.com>
2025-04-12 08:13:11 +0800

    3272: WA.cpp(#867)
    todo: rename
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3273-find-the-count-of-good-integers.cpp
+++ Codes/3273-find-the-count-of-good-integers.cpp
@@ -2,26 +2,39 @@
  * @Author: LetMeFly
  * @Date: 2025-04-12 07:51:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-04-12 08:00:58
+ * @LastEditTime: 2025-04-12 08:11:44
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
+/*
+1 -> ''
+2 -> 1-9
+3 -> 1-9
+4 -> 10->99
+5 -> 10->99
+6 -> 100->999
+
+n -> [10^(n/2-1), 10^(n/2)-1)
+*/
 typedef long long ll;
 
 class Solution {
 private:
     bool isOk(string s, int k) {
         ll val = stoll(s);
-        printf("%s: %d\n", s.c_str(), val % k == 0);  // *****
+        // printf("%s: %d\n", s.c_str(), val % k == 0);  // *****
         return val % k == 0;
     };
 public:
     ll countGoodIntegers(int n, int k) {
+        if (n == 1) {
+            return 1;
+        }
         int half = n / 2;
         ll ans = 0;
-        for (int start = pow(10, half), end = start * 10; start < end; start++) {
+        for (int start = pow(10, half - 1), end = start * 10; start < end; start++) {
             string prefix = to_string(start), suffix = prefix;
             reverse(suffix.begin(), suffix.end());
             if (n % 2) {
