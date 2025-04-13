c32f56eb771c3a66247426aaff0eaaddcd66f505
LetMeFly666 <814114971@qq.com>
2025-04-14 00:09:43 +0800

    3272: AC.java - AC,100.00%,87.23% (#871)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1922-count-good-numbers.java
+++ Codes/1922-count-good-numbers.java
@@ -2,15 +2,16 @@
  * @Author: LetMeFly
  * @Date: 2025-04-13 17:07:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-04-13 17:09:52
+ * @LastEditTime: 2025-04-14 00:07:21
+ * @Description: AC,100.00%,87.23%
  */
 class Solution {
-    private final long mod = 1e9 + 7;
+    private final long mod = 1000000007;
 
     private long pow(long a, long b) {
         long ans = 1;
         while (b > 0) {
-            if (b & 1 == 1) {
+            if ((b & 1) == 1) {
                 ans = ans * a % mod;
             }
             a = a * a % mod;
@@ -20,6 +21,6 @@ class Solution {
     }
 
     public int countGoodNumbers(long n) {
-        return pow(5, (n + 1) / 2) * pow(4, n / 2) % mod;
+        return (int)(pow(5, (n + 1) / 2) * pow(4, n / 2) % mod);
     }
 }
