c5ce3c8fc72a87452d3bc8a7f93ba69a71515feb
LetMeFly666 <814114971@qq.com>
2025-04-14 00:02:23 +0800

    1922: begin.go (#871)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/1922-count-good-numbers.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-04-13 17:54:42
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-04-13 17:54:58
+ * @Description: continue on subway，太晃了还是先算了
+ */
+package main
+
+func countGoodNumbers(n int64) int {
+    
+}
