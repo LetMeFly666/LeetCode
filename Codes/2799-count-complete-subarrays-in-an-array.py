48f2c0dcfc34cb3108cf081283e2d1e88895f306
LetMeFly666 <814114971@qq.com>
2025-04-24 23:06:31 +0800

    init: Py (#896)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2799-count-complete-subarrays-in-an-array.py
@@ -0,0 +1,9 @@
+'''
+Author: LetMeFly
+Date: 2025-04-24 22:47:44
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-04-24 23:06:18
+'''
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        
