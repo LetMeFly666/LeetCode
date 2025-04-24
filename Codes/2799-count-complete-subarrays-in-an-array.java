a83157b9aeb93f646e18cbfeda8c82ed0f610f59
LetMeFly666 <814114971@qq.com>
2025-04-24 23:10:45 +0800

    init: Java (#896)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2799-count-complete-subarrays-in-an-array.java
@@ -0,0 +1,11 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-04-24 22:47:48
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-04-24 23:10:34
+ */
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        
+    }
+}
