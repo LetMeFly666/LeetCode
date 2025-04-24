c21503128ed3e3aede6422f904bb7885e227f50a
LetMeFly666 <814114971@qq.com>
2025-04-24 23:15:34 +0800

    CE: Java (#896)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2799-count-complete-subarrays-in-an-array.java
+++ Codes/2799-count-complete-subarrays-in-an-array.java
@@ -2,10 +2,32 @@
  * @Author: LetMeFly
  * @Date: 2025-04-24 22:47:48
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-04-24 23:10:34
+ * @LastEditTime: 2025-04-24 23:14:56
  */
+import java.util.Set;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.HashMap;
+
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        
+        Set<Integer> se = new HashSet<>();
+        for (int t : nums) {
+            se.add(t);
+        }
+        int allType = se.size();
+        Map<Integer, Integer> times = new HashMap<>();
+        int ans = 0;
+        int l = 0;
+        for (int t : nums) {
+            times.merge(t, 1, Integer::sum);
+            if (cnt.size() == k && cnt.get(nums[l]) > 1) {
+                cnt.merge(nums[l++], -1, Integer::sum);
+            }
+            if (cnt.size() == k) {
+                ans += l + 1;
+            }
+        }
+        return ans;
     }
 }
