4a9af2ab7abdffd3e86510057306c94c2793e00c
LetMeFly666 <814114971@qq.com>
2025-04-24 23:22:57 +0800

    AC: Go (#896)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2799-count-complete-subarrays-in-an-array.go
+++ Codes/2799-count-complete-subarrays-in-an-array.go
@@ -2,10 +2,27 @@
  * @Author: LetMeFly
  * @Date: 2025-04-24 22:47:30
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-04-24 23:19:49
+ * @LastEditTime: 2025-04-24 23:22:04
  */
 package main
 
-func countCompleteSubarrays(nums []int) int {
-    
+func countCompleteSubarrays(nums []int) (ans int) {
+    visited := map[int]bool{}
+	for _, t := range nums {
+		visited[t] = true
+	}
+	allType := len(visited)
+	times := map[int]int{}
+	l := 0
+	for _, t := range nums {
+		times[t]++
+		for len(times) == allType && times[nums[l]] > 1 {
+			times[nums[l]]--
+			l++
+		}
+		if len(times) == allType {
+			ans += l + 1
+		}
+	}
+	return
 }
