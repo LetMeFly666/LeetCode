e0f7ecb9ff54d2a674b79de441b5f8de5656950e
LetMeFly666 <814114971@qq.com>
2025-04-24 23:20:02 +0800

    init: Go (#896)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2799-count-complete-subarrays-in-an-array.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-04-24 22:47:30
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-04-24 23:19:49
+ */
+package main
+
+func countCompleteSubarrays(nums []int) int {
+    
+}
