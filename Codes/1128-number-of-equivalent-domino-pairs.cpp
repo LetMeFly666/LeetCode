7b92ae9b6d3b2833667ce479b81421a572b0aef1
LetMeFly666 <814114971@qq.com>
2025-05-04 15:09:39 +0800

    AC: C++(#911)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1128-number-of-equivalent-domino-pairs.cpp
+++ Codes/1128-number-of-equivalent-domino-pairs.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-04 14:26:01
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-04 14:50:43
+ * @LastEditTime: 2025-05-04 15:02:06
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -11,12 +11,10 @@
 class Solution {
 private:
     inline int pair2int(vector<int>& v) {
+        if (v[0] > v[1]) {
+            return (v[1] - 1) * 9 + (v[0] - 1);
+        }
         return (v[0] - 1) * 9 + (v[1] - 1);
-    }
-
-    inline int reverseNum(int i) {
-        int a = i % 9, b = i / 9;
-        return a * 9 + b;
     }
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
@@ -26,12 +24,7 @@ public:
         }
         int ans = 0;
         for (int i = 0; i < 81; i++) {
-            int r = reverseNum(i);
-            int t = times[i] + (i == r ? 0 : times[r]);
-            if (times[i] || times[r]) {
-                printf("i: %d, r: %d, times[%d]: %d, times[%d]: %d, t = %d, %d * (%d - 1) = %d\n", i, r, i, times[i], r, times[r], t, t, t, t * (t - 1));
-            }
-            ans += t * (t - 1) / (i == r ? 4 : 2);
+            ans += times[i] * (times[i] - 1) / 2;
         }
         return ans;
     }
