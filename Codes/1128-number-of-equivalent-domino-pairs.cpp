e6f854e384abb0d187bb0b5b8891107d4fc44e43
LetMeFly666 <814114971@qq.com>
2025-05-04 14:43:49 +0800

    WA: C++  + debug (#911)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1128-number-of-equivalent-domino-pairs.cpp
+++ Codes/1128-number-of-equivalent-domino-pairs.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-04 14:26:01
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-04 14:42:24
+ * @LastEditTime: 2025-05-04 14:43:44
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -30,7 +30,7 @@ public:
             if (times[i] || times[r]) {
                 printf("i: %d, r: %d, times[%d]: %d, times[%d]: %d\n", i, r, i, times[i], r, times[r]);
             }
-            int t = times[i] + t == r ? 0 : times[r];
+            int t = times[i] + (t == r ? 0 : times[r]);
             ans += t * (t - 1);
         }
         return ans / 2;
