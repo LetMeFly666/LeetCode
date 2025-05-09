6c40a41504f4d12a05925e8cd04a5d717be5fed9
LetMeFly666 <814114971@qq.com>
2025-05-09 13:05:52 +0800

    feat: 移除空格结尾的文件-兼容Win系统
    其实还是那三个文件和文件夹(#924)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- filterSpacePoint.py
+++ filterSpacePoint.py
@@ -2,17 +2,32 @@
 Author: LetMeFly
 Date: 2025-05-09 12:46:04
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-05-09 12:57:20
+LastEditTime: 2025-05-09 13:03:25
 '''
 import os
 
 BASE_DIR = 'AllProblems'
 
+def check1(file: str, split_str: str):
+    splited = file.split(split_str)
+    notOk = False
+    for i, d in enumerate(splited):
+        if d.endswith(' '):
+            splited[i] = d.strip()
+            notOk = True
+    if notOk:
+        return split_str.join(splited)
+    return False
+
 def filter1file(file: str):
-    splited = file.split('.')
-    for i in splited:
-        if i.endswith(' '):
-            print(file)
+    split_char_list = ['.', '/']
+    now = file
+    for split_char in split_char_list:
+        result = check1(now, split_char)
+        if result:
+            now = result
+    if now != file:
+        print(f'{file} -> {now}')
 
 for dir in os.listdir(BASE_DIR):
     dir_path = os.path.join(BASE_DIR, dir)
