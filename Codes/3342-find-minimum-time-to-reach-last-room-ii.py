9bd20fb0dbc3952ed2c5b6461a456e705711ca28
LetMeFly666 <814114971@qq.com>
2025-05-09 13:39:34 +0800

    3342: AC.Py - AC,6.51%,34.92% (#924)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3342-find-minimum-time-to-reach-last-room-ii.py
+++ Codes/3342-find-minimum-time-to-reach-last-room-ii.py
@@ -2,11 +2,30 @@
 Author: LetMeFly
 Date: 2025-05-09 12:45:04
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-05-09 13:34:53
+LastEditTime: 2025-05-09 13:38:28
 '''
 from typing import List
 import heapq
 
+DIRECTIONS = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        
+        n, m = len(moveTime), len(moveTime[0])
+        ans = [[2000000001] * m for _ in range(n)]
+        ans[0][0] = 0
+        pq = [(0, 0, 0)]
+        while pq:
+            t, x, y = heapq.heappop(pq)
+            if t > ans[x][y]:
+                continue
+            for dx, dy in DIRECTIONS:
+                nx = x + dx
+                ny = y + dy
+                if not (0 <= nx < n and 0 <= ny < m):
+                    continue
+                nt = max(t, moveTime[nx][ny]) + (x + y) % 2 + 1
+                if nt < ans[nx][ny]:
+                    ans[nx][ny] = nt
+                    heapq.heappush(pq, (nt, nx, ny))
+        return ans[n - 1][m - 1]
