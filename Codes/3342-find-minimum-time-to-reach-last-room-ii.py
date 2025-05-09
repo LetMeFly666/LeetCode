f964d3692e82611b0deca760b194c4874c7f4d1c
LetMeFly666 <814114971@qq.com>
2025-05-09 13:35:08 +0800

    3342: Begin.Py (#924)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3342-find-minimum-time-to-reach-last-room-ii.py
+++ Codes/3342-find-minimum-time-to-reach-last-room-ii.py
@@ -2,9 +2,10 @@
 Author: LetMeFly
 Date: 2025-05-09 12:45:04
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-05-09 13:09:14
+LastEditTime: 2025-05-09 13:34:53
 '''
 from typing import List
+import heapq
 
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
