ee9be991567a72218df5ae6f6faebef4b381eb90
LetMeFly666 <814114971@qq.com>
2025-05-10 12:12:02 +0800

    2918: archive.Cpp (#974)
    某人穿好了
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ Codes/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -2,15 +2,18 @@
  * @Author: LetMeFly
  * @Date: 2025-05-10 12:07:54
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-10 12:09:35
+ * @LastEditTime: 2025-05-10 12:11:10
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
+typedef long long ll;
+
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        
+        ll s1 = 0, s2 = 0;
+        int c1 = 0, c2 = 0;
     }
 };
