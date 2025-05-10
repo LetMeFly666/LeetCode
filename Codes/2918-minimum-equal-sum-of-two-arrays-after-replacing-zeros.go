e96e4aa366de614ae25356ea0489b1d095372302
LetMeFly666 <814114971@qq.com>
2025-05-10 19:39:30 +0800

    2918: AC.go - AC,17.65%, 29.41% (#974)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.go
+++ Codes/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.go
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-10 12:07:54
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-10 19:37:14
+ * @LastEditTime: 2025-05-10 19:38:55
  */
 package main
 
@@ -20,7 +20,7 @@ func cal2918(a []int) (s int64, cnt int64) {
 func minSum(nums1 []int, nums2 []int) int64 {
     s1, c1 := cal2918(nums1)
     s2, c2 := cal2918(nums2)
-    if s1 > s2 + c2 && c1 == 0 || s1 + c1 < s2 && c2 == 0 {
+    if s1 < s2 + c2 && c1 == 0 || s1 + c1 > s2 && c2 == 0 {
         return -1
     }
     return max(s1 + c1, s2 + c2)
