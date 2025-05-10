15815f6c587714c32bddbea475d98a8c43128231
LetMeFly666 <814114971@qq.com>
2025-05-10 19:26:49 +0800

    2918: Init.Py (#974)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -0,0 +1,11 @@
+'''
+Author: LetMeFly
+Date: 2025-05-10 12:07:54
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-05-10 19:26:33
+'''
+from typing import List
+
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        
