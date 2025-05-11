35a0fafdd2f180a25dd829fd58f301b4b6fe3132
LetMeFly666 <814114971@qq.com>
2025-05-11 14:20:14 +0800

    1550: AC.go - AC,100.00%,25.00% (#929)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1550-three-consecutive-odds.go
+++ Codes/1550-three-consecutive-odds.go
@@ -2,15 +2,15 @@
  * @Author: LetMeFly
  * @Date: 2025-05-11 14:00:52
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-11 14:17:46
+ * @LastEditTime: 2025-05-11 14:19:31
  */
 package main
 
 func threeConsecutiveOdds(arr []int) bool {
     for i := 2; i < len(arr); i++ {
-		if arr[i] % 2 == 1 && arr[i - 1] % 2 == 1 && arr[i - 2] % 2 == 1 {
-			return true;
-		}
-		return false;
-	}
+        if arr[i] % 2 == 1 && arr[i - 1] % 2 == 1 && arr[i - 2] % 2 == 1 {
+            return true
+        }
+    }
+    return false;
 }
