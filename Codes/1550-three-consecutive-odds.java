907ed7af16dce5644419803a5e0e0c47454df462
LetMeFly666 <814114971@qq.com>
2025-05-11 14:19:12 +0800

    1550: CE.go (#929)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1550-three-consecutive-odds.java
+++ Codes/1550-three-consecutive-odds.java
@@ -2,7 +2,8 @@
  * @Author: LetMeFly
  * @Date: 2025-05-11 14:00:52
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-11 14:16:53
+ * @LastEditTime: 2025-05-11 14:17:39
+ * @Description: 1550: AC,100.00%,88.19%
  */
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
