b5561798b55a45a3dbff39ba0819b96c62fb1141
LetMeFly666 <814114971@qq.com>
2025-05-11 14:15:28 +0800

    1550: WA.Py (#929)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/1550-three-consecutive-odds.py
@@ -0,0 +1,14 @@
+'''
+Author: LetMeFly
+Date: 2025-05-11 14:00:52
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-05-11 14:14:39
+'''
+from typing import List
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(3, len(arr)):
+            if arr[i] % 2 and arr[i - 1] % 2 and arr[i - 2] % 2:
+                return True
+        return False
