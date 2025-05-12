d14a0db36f2909f9d99bc8dbbc0025299a8a5f8d
LetMeFly666 <814114971@qq.com>
2025-05-12 22:33:57 +0800

    2094: RE.java (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2094-finding-3-digit-even-numbers.java
+++ Codes/2094-finding-3-digit-even-numbers.java
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-12 10:20:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-12 13:19:27
+ * @LastEditTime: 2025-05-12 22:32:22
  */
 import java.util.Array;
 
@@ -14,7 +14,24 @@ class Solution {
         }
         Array<Integer> ans = new Array<>();
         for (int i = 100; i < 1000; i += 2) {
-            int tmp
+            int tmp = i;
+            int[] thisTimes = new int[10];
+            while (tmp > 0) {
+                thisTimes[tmp % 10]++;
+                tmp /= 10;
+            }
+            boolean ok = true;
+            for (int j = 0; j < 10; j++) {
+                if (thisTimes[j] > times[j]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                ans.add(i);
+            }
         }
+        Arrays.sort(ans);
+        return new int[]{ans};
     }
 }
