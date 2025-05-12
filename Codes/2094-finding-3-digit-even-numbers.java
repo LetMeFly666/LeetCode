432452edbd88542565fa617d4748c0fa5a70a7e3
LetMeFly666 <814114971@qq.com>
2025-05-12 13:20:35 +0800

    2094: archive.Java - sleep (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2094-finding-3-digit-even-numbers.java
+++ Codes/2094-finding-3-digit-even-numbers.java
@@ -1,5 +1,20 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-12 10:20:43
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-12 13:19:27
+ */
+import java.util.Array;
+
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        
+        int[] times = new int[10];
+        for (int d : digits) {
+            times[d]++;
+        }
+        Array<Integer> ans = new Array<>();
+        for (int i = 100; i < 1000; i += 2) {
+            int tmp
+        }
     }
 }
