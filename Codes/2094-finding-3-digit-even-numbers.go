15da78680ed1a33b1b2b5f24cd2818e4e43d7ff7
LetMeFly666 <814114971@qq.com>
2025-05-12 22:39:56 +0800

    2094: Init.Go (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2094-finding-3-digit-even-numbers.go
+++ Codes/2094-finding-3-digit-even-numbers.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-12 21:58:15
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-12 22:39:29
+ */
+package main
+
 func findEvenNumbers(digits []int) []int {
     
 }
