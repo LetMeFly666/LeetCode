48dce58b45b14178c1cfd0c304446930df7908a7
LetMeFly666 <814114971@qq.com>
2025-05-12 13:13:51 +0800

    2094: init.Py (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2094-finding-3-digit-even-numbers.py
+++ Codes/2094-finding-3-digit-even-numbers.py
@@ -1,3 +1,11 @@
+'''
+Author: LetMeFly
+Date: 2025-05-12 10:20:43
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-05-12 13:13:38
+'''
+from typing import List
+
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         
