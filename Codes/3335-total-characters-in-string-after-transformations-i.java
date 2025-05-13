bd38a1f3b5561471baa1ba1871ea02154dce370e
LetMeFly666 <814114971@qq.com>
2025-05-13 09:23:40 +0800

    3335: Ac.Java - AC,61.29%,67.74% (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3335-total-characters-in-string-after-transformations-i.java
+++ Codes/3335-total-characters-in-string-after-transformations-i.java
@@ -1,5 +1,28 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-13 09:02:15
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-13 09:19:43
+ */
+
 class Solution {
+    private final int MOD = 1000000007;
+
     public int lengthAfterTransformations(String s, int t) {
-        
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+        int ans = s.length();
+        while (t-- > 0) {
+            int z = cnt[25];
+            for (int i = 24; i >= 0; i--) {
+                cnt[i + 1] = cnt[i];
+            }
+            cnt[0] = z;
+            cnt[1] = (cnt[1] + z) % MOD;
+            ans = (ans + z) % MOD;
+        }
+        return ans;
     }
 }
