8bcc85f546d477a9983ef1c12afcab45fbf2e55d
LetMeFly <56995506+LetMeFly666@users.noreply.github.com>
2025-05-13 09:41:57 +0800

    update: 添加问题“3335.字符串转换后的长度I”的代码和题解(#934)
    * 3335: WA.Cpp (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: RE.Cpp (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: AC.Cpp - AC,92.65%,98.53% (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: Init.Py (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: Ac.Py - AC,64.71%,94.12% (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: Ac.Java - AC,61.29%,67.74% (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: WA.go (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3335: AC.go - AC,66.67%,66.67% (#931)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * update: 添加问题“3335.字符串转换后的长度I”的代码和题解(#934)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/3335-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,34 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-13 09:02:15
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-13 09:15:09
+ * @Description: 3335: AC,92.65%,98.53%
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+const int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        int cnt[26] = {0};
+        for (char c : s) {
+            cnt[c - 'a']++;
+        }
+        int ans = s.size();
+        while (t--) {
+            int z = cnt[25];
+            for (int i = 24; i >= 0; i--) {  // 这里必须从后往前
+                cnt[i + 1] = cnt[i];
+            }
+            cnt[0] = z;
+            cnt[1] = (cnt[1] + z) % MOD;
+            ans = (ans + z) % MOD;
+            // debug(cnt);
+        }
+        return ans;
+    }
+};
