9ae581abeee90c2c787933484b0cd0cf513bb7a4
LetMeFly666 <814114971@qq.com>
2025-05-13 09:12:48 +0800

    3335: RE.Cpp (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3335-total-characters-in-string-after-transformations-i.cpp
+++ Codes/3335-total-characters-in-string-after-transformations-i.cpp
@@ -2,13 +2,20 @@
  * @Author: LetMeFly
  * @Date: 2025-05-13 09:02:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-13 09:07:45
+ * @LastEditTime: 2025-05-13 09:10:46
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
 class Solution {
+// private:
+//     void debug(int a[26]) {
+//         for (int i = 0; i < 26; i++) {
+//             cout << a[i] << ' ';
+//         }
+//         puts("");
+//     }
 public:
     int lengthAfterTransformations(string s, int t) {
         int cnt[26] = {0};
@@ -18,12 +25,13 @@ public:
         int ans = s.size();
         while (t--) {
             int z = cnt[25];
-            for (int i = 0; i < 25; i++) {
+            for (int i = 24; i >= 0; i--) {  // 这里必须从后往前
                 cnt[i + 1] = cnt[i];
             }
             cnt[0] = z;
             cnt[1] += z;
             ans += z;
+            // debug(cnt);
         }
         return ans;
     }
