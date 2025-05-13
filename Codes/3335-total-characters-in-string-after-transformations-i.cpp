65dbe62dfc53f6e98d2aad553cfafc428b744bde
LetMeFly666 <814114971@qq.com>
2025-05-13 09:14:29 +0800

    3335: AC.Cpp - AC,92.65%,98.53% (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3335-total-characters-in-string-after-transformations-i.cpp
+++ Codes/3335-total-characters-in-string-after-transformations-i.cpp
@@ -2,20 +2,15 @@
  * @Author: LetMeFly
  * @Date: 2025-05-13 09:02:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-13 09:10:46
+ * @LastEditTime: 2025-05-13 09:13:30
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
+const int MOD = 1e9 + 7;
+
 class Solution {
-// private:
-//     void debug(int a[26]) {
-//         for (int i = 0; i < 26; i++) {
-//             cout << a[i] << ' ';
-//         }
-//         puts("");
-//     }
 public:
     int lengthAfterTransformations(string s, int t) {
         int cnt[26] = {0};
@@ -29,8 +24,8 @@ public:
                 cnt[i + 1] = cnt[i];
             }
             cnt[0] = z;
-            cnt[1] += z;
-            ans += z;
+            cnt[1] = (cnt[1] + z) % MOD;
+            ans = (ans + z) % MOD;
             // debug(cnt);
         }
         return ans;
