e795592d148f5b53bd6d2a3be2a61383214f0122
LetMeFly666 <814114971@qq.com>
2025-05-13 09:08:47 +0800

    3335: WA.Cpp (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/3335-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,30 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-13 09:02:15
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-13 09:07:45
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        int cnt[26] = {0};
+        for (char c : s) {
+            cnt[c - 'a']++;
+        }
+        int ans = s.size();
+        while (t--) {
+            int z = cnt[25];
+            for (int i = 0; i < 25; i++) {
+                cnt[i + 1] = cnt[i];
+            }
+            cnt[0] = z;
+            cnt[1] += z;
+            ans += z;
+        }
+        return ans;
+    }
+};
---
+++ Codes/3335-total-characters-in-string-after-transformations-i.py
@@ -0,0 +1,3 @@
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        
