4a61cce3a62bedc3ca4f89d4ecfdcc1873415fc2
LetMeFly666 <814114971@qq.com>
2025-05-13 09:28:26 +0800

    3335: WA.go (#931)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3335-total-characters-in-string-after-transformations-i.go
+++ Codes/3335-total-characters-in-string-after-transformations-i.go
@@ -1,3 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-13 09:02:15
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-13 09:27:17
+ */
+package main
+
+var MOD3351 = 1000000007
+
 func lengthAfterTransformations(s string, t int) int {
-    
+    cnt := make([]int, len(s))
+	for i := 0; i < len(s); i++ {
+		cnt[s[i] - 'a']++
+	}
+	ans := len(s)
+	for i := 0; i < t; i++ {
+		z := cnt[25]
+		for j := 24; j >= 0; j-- {
+			cnt[j + 1] = cnt[j]
+		}
+		cnt[0] = z
+		cnt[1] = (cnt[1] + z) % MOD3351
+		ans = (ans + z) % MOD3351
+	}
+	return ans
 }
