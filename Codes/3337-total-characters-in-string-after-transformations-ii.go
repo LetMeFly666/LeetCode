d21f83024e1f33ff13a37edc81c3c5b0f16d1f11
LetMeFly666 <814114971@qq.com>
2025-05-15 09:52:11 +0800

    3337: Init.go (#935)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3337-total-characters-in-string-after-transformations-ii.go
+++ Codes/3337-total-characters-in-string-after-transformations-ii.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-15 09:49:53
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-15 09:50:28
+ */
+package main
+
 func lengthAfterTransformations(s string, t int, nums []int) int {
     
 }
