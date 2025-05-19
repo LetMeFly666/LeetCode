ba5cbaebd1cb29c27986c7f1380571b5fb730d9b
LetMeFly666 <814114971@qq.com>
2025-05-19 23:13:27 +0800

    1931: WA.cpp (#941) - all0
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1931-painting-a-grid-with-three-different-colors.cpp
+++ Codes/1931-painting-a-grid-with-three-different-colors.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-19 21:52:57
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-19 22:51:47
+ * @LastEditTime: 2025-05-19 23:11:19
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -11,6 +11,7 @@
 class Solution {
 private:
     static constexpr int pow[6] = {1, 3, 9, 27, 81, 243};
+    static constexpr int MOD = 1e9 + 7;
 
     inline int getTh(int a, int n) {
         return a / pow[n] % 3;
@@ -30,25 +31,37 @@ private:
 public:
     int colorTheGrid(int m, int n) {
         int types = pow[m];
-        unordered_map<int, int> okList;
+        unordered_map<int, vector<int>> okList;
         for (int i = 0; i < types; i++) {
             for (int j = 0; j < types; j++) {
                 if (ok(i, j, m)) {
-                    okList[i]++;
+                    okList[i].push_back(j);
                 }
             }
         }
         int ok1type = okList.size();
         vector<int> firstCol(ok1type, 1), secondCol(ok1type);
         unordered_map<int, int> code2idx(ok1type);
+        unordered_map<int, int> idx2code(ok1type);
         int th = 0;
-        for (unordered_map<int, int>::iterator it = okList.begin(); it != okList.end(); it++, th++) {
+        for (unordered_map<int, vector<int>>::iterator it = okList.begin(); it != okList.end(); it++, th++) {
             code2idx[it->first] = th;
+            idx2code[th] = it->first;
         }
         for (int j = 2; j <= n; j++) {
             for (int t = 0; t < ok1type; t++) {
-                
+                int lastCode = idx2code[t];
+                for (int nextCode : okList[lastCode]) {
+                    secondCol[code2idx[nextCode]] = (secondCol[code2idx[nextCode]] + firstCol[t]) % MOD;
+                }
             }
+            firstCol = move(secondCol);
+            secondCol = vector<int>(ok1type);
         }
+        int ans = 0;
+        for (int t : secondCol) {
+            ans = (ans + t) % MOD;
+        }
+        return ans;
     }
 };
