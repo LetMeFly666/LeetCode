af771136e5d8b14e23b7fdc3a021a8bdd840471a
LetMeFly666 <814114971@qq.com>
2025-05-20 09:17:50 +0800

    3337: fix.cpp (#941)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1931-painting-a-grid-with-three-different-colors_JvZhenKuaiSuMi.cpp
+++ Codes/1931-painting-a-grid-with-three-different-colors_JvZhenKuaiSuMi.cpp
@@ -2,17 +2,17 @@
  * @Author: LetMeFly
  * @Date: 2025-05-20 08:59:53
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-20 09:09:16
+ * @LastEditTime: 2025-05-20 09:17:44
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
-typedef matrix vector<vector<int>>;
+typedef vector<vector<int>> matrix;
 
 class Solution {
 private:
-    static constexpr int pow[6] = {1, 3, 9, 81, 243};
+    static constexpr int pow[6] = {1, 3, 9, 27, 81, 243};
 
     inline int getTh(int a, int n) {
         return a / pow[n] % 3;
@@ -28,13 +28,14 @@ private:
     }
 public:
     int colorTheGrid(int m, int n) {
-        vector<int> singleOk();
+        vector<int> singleOk;
         for (int i = 0; i < pow[m]; i++) {
+            printf("isOk(%d, %d) = %d\n", i, m, isOk(i, m));
             if (isOk(i, m)) {
                 singleOk.push_back(i);
             }
         }
-        int singleNum = singleOk.size();
+        int singleNum = singleOk.size();  // 即使m=5也一共有48种
         return singleNum;
     }
 };
