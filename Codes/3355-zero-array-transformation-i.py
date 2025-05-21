38cb810326f370f9bd4d9face723b2b6dababaeb
LetMeFly666 <814114971@qq.com>
2025-05-21 23:23:44 +0800

    3355: AC.go  (#945)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3355-zero-array-transformation-i.py
+++ Codes/3355-zero-array-transformation-i.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-05-21 23:06:35
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-05-21 23:16:48
+LastEditTime: 2025-05-21 23:19:48
 '''
 from typing import List
 
@@ -11,7 +11,7 @@ class Solution:
         diff = [0] * (len(nums) + 1)
         for l, r in queries:
             diff[l] += 1
-            diff[r + 1] += 1
+            diff[r + 1] -= 1
         cnt = 0
         for i in range(len(nums)):
             cnt += diff[i]
