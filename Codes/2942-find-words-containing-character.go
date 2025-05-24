ded4edea25290e771f484307f40e300eb31066e8
LetMeFly666 <814114971@qq.com>
2025-05-24 21:41:31 +0800

    2941: AC.py+AC.java(AC,100.00%,88.61%)+AC.go(AC,100.00%,16.67%) (#952)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2942-find-words-containing-character.go
+++ Codes/2942-find-words-containing-character.go
@@ -1,3 +1,18 @@
-func findWordsContaining(words []string, x byte) []int {
-    
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-24 21:30:36
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-24 21:40:52
+ */
+package main
+
+import "strings"
+
+func findWordsContaining(words []string, x byte) (ans []int) {
+    for i, word := range words {
+        if strings.IndexByte(word, x) >= 0 {
+            ans = append(ans, i)
+        }
+    }
+    return
 }
--- Codes/2942-find-words-containing-character.py
+++ Codes/2942-find-words-containing-character.py
@@ -2,10 +2,10 @@
 Author: LetMeFly
 Date: 2025-05-24 21:30:36
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-05-24 21:35:58
+LastEditTime: 2025-05-24 21:37:05
 '''
 from typing import List
 
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
-        return [i for i in len(words) if x in words[i]]
+        return [i for i in range(len(words)) if x in words[i]]
