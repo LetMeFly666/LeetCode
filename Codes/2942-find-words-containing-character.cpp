02d94568a74528c656cb769fb2c10dcb108e575b
LetMeFly666 <814114971@qq.com>
2025-05-24 21:36:58 +0800

    2941: AC.cpp+RE.py (#952)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2942-find-words-containing-character.cpp
+++ Codes/2942-find-words-containing-character.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-24 21:30:36
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-24 21:32:53
+ * @LastEditTime: 2025-05-24 21:34:02
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -12,10 +12,11 @@ class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
-        for (string& word : words) {
-            if (word.find(x) != string::npos) {
-                
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i].find(x) != string::npos) {
+                ans.push_back(i);
             }
         }
+        return ans;
     }
 };
--- Codes/2942-find-words-containing-character.py
+++ Codes/2942-find-words-containing-character.py
@@ -1,3 +1,11 @@
+'''
+Author: LetMeFly
+Date: 2025-05-24 21:30:36
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-05-24 21:35:58
+'''
+from typing import List
+
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
-        
+        return [i for i in len(words) if x in words[i]]
