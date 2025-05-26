937dad5694a5ca0b52baa022840a6233b0e0835e
LetMeFly666 <814114971@qq.com>
2025-05-26 23:28:05 +0800

    1857: WA.cpp (#956)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1857-largest-color-value-in-a-directed-graph.cpp
+++ Codes/1857-largest-color-value-in-a-directed-graph.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-05-26 10:24:14
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-26 10:30:56
+ * @LastEditTime: 2025-05-26 23:26:40
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -11,42 +11,34 @@
 class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        vector<vector<int>> inNodes(edges.size() + 1);
-        vector<int> out(edges.size() + 1);
-        for (vector<int>& edge : edges) {
-            inNodes[edge[1]].push_back(edge[0]);
-            out[edge[1]]++;
+        vector<vector<int>> out(colors.size());
+        vector<int> indegree(colors.size());
+        for (vector<int>& edge: edges) {
+            out[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
         }
-        vector<array<int, 26>> dp(edges.size() + 1);
         queue<int> q;
-        for (int i = 0; i <= edges.size(); i++) {
-            if (!out[i]) {
+        for (int i = 0; i < colors.size(); i++) {
+            if (!indegree[i]) {
                 q.push(i);
             }
         }
+        vector<array<int, 26>> dp(colors.size());
+        int ans = 0;
         while (q.size()) {
             int thisNode = q.front();
-            dp[thisNode][colors[thisNode] - 'a']++;
             q.pop();
-            for (int inNode : inNodes[thisNode]) {
-                out[inNode]--;
-                if (!out[inNode]) {
-                    q.push(inNode);
+            ans = max(ans, ++dp[thisNode][colors[thisNode] - 'a']);
+            for (int nextNode : out[thisNode]) {
+                indegree[nextNode]--;
+                if (!indegree[nextNode]) {
+                    q.push(nextNode);
                 }
                 for (int i = 0; i < 26; i++) {
-                    dp[inNode][i] += dp[thisNode][i];
+                    dp[nextNode][i] = max(dp[nextNode][i], dp[thisNode][i]);
                 }
             }
         }
-        if (q.size()) {
-            return -1;
-        }
-        int ans = 0;
-        for (array<int, 26>& a : dp) {
-            for (int t : a) {
-                ans = max(ans, t);
-            }
-        }
-        return ans;
+        return q.size() ? -1 : ans;
     }
 };
