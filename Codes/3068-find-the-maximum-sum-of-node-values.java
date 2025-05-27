27915e40228b31267e359e9757446c2239354611
LetMeFly666 <814114971@qq.com>
2025-05-27 23:44:03 +0800

    3068: WA.java (#951)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3068-find-the-maximum-sum-of-node-values.java
+++ Codes/3068-find-the-maximum-sum-of-node-values.java
@@ -2,11 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2025-05-27 23:28:05
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-27 23:42:20
+ * @LastEditTime: 2025-05-27 23:43:37
  */
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        long odd = 0, even = -1000000000000000;
+        long odd = 0, even = -1000000000000000L;  // 记得带“L”
         for (int t : nums) {
             long newO = Math.max(odd + t, even + (t ^ k));
             long newE = Math.max(even + t, odd + (t ^ k));
