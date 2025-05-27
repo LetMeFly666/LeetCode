594749efe028092a5ec5a43587138ada4d2943a0
LetMeFly <56995506+LetMeFly666@users.noreply.github.com>
2025-05-27 22:15:45 +0800

    update: 添加问题“2894.分类求和并作差”的代码和题解(#959)
    * 2894: WA.cpp (#958)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 2894: AC.(cpp+py) + WA.java (#958)
    
    cpp - AC,100.00%,19.79%
    py - AC,100.00%,88.02%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 2894: AC.(java+go) (#958)
    
    java - AC,100.00%,80.77%
    go - AC,100.00%,68.18%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * update: 添加问题“2894.分类求和并作差”的代码和题解(#959)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * typo: nums2 -> num2
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2894-divisible-and-non-divisible-sums-difference.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-27 21:42:21
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-27 21:58:33
+ */
+package main
+
+func differenceOfSums(n int, m int) int {
+    return (1 + n) * n / 2 - (m + n / m * m) * (n / m)
+}
