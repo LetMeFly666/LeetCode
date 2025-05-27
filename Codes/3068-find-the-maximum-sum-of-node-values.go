0797da47fb221b2a7c042026b2b638e89fd72a6a
LetMeFly666 <814114971@qq.com>
2025-05-27 23:49:06 +0800

    3068: AC.java + RE.go (#951)
    java - AC,100.00%,42.45%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3068-find-the-maximum-sum-of-node-values.go
+++ Codes/3068-find-the-maximum-sum-of-node-values.go
@@ -1,3 +1,15 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-27 23:28:05
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-27 23:48:05
+ */
+package main
+
 func maximumValueSum(nums []int, k int, edges [][]int) int64 {
-    
+    odd, even := -10000000000000000, int64(0)
+    for _, t := range nums {
+        odd, even = max(odd + int64(t), even + int64(t ^ k)), max(even + int64(t), odd + int64(t ^ k))
+    }
+    return even
 }
--- Codes/3068-find-the-maximum-sum-of-node-values.java
+++ Codes/3068-find-the-maximum-sum-of-node-values.java
@@ -2,11 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2025-05-27 23:28:05
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-27 23:43:37
+ * @LastEditTime: 2025-05-27 23:45:06
  */
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        long odd = 0, even = -1000000000000000L;  // 记得带“L”
+        long even = 0, odd = -1000000000000000L;  // 记得带“L”
         for (int t : nums) {
             long newO = Math.max(odd + t, even + (t ^ k));
             long newE = Math.max(even + t, odd + (t ^ k));
