aef7932421e60eb7fab0198759af63136bc30d13
LetMeFly666 <814114971@qq.com>
2025-05-27 21:59:43 +0800

    2894: AC.(java+go) (#958)
    java - AC,100.00%,80.77%
    go - AC,100.00%,68.18%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2894-divisible-and-non-divisible-sums-difference.java
+++ Codes/2894-divisible-and-non-divisible-sums-difference.java
@@ -2,10 +2,10 @@
  * @Author: LetMeFly
  * @Date: 2025-05-27 21:42:21
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-05-27 21:55:39
+ * @LastEditTime: 2025-05-27 21:57:48
  */
 class Solution {
     public int differenceOfSums(int n, int m) {
-        return (1 + n) * n / 2 + (m + n / m * m) * (n / m);
+        return (1 + n) * n / 2 - (m + n / m * m) * (n / m);
     }
 }
