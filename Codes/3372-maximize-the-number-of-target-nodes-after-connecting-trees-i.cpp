83104568b30b621bdb8029ee37c8ec12c4fa15b3
LetMeFly <56995506+LetMeFly666@users.noreply.github.com>
2025-05-29 13:54:40 +0800

    update: 添加问题“3372.连接两棵树后最大目标节点数目I”的代码和题解(#962)
    * 3372: WA.cpp (#961)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: WA.cpp (#961)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: WA.cpp (#961)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: AC.(cpp+py)+CE.java (#961)
    
    cpp - AC,75.44%,71.93%
    py - AC,88.89%,66.67%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: CE.java (#961)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: AC.java (#961)
    
    java - AC,92.00%,96.00%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: WA.go (#961)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * 3372: AC.go (#961)
    
    AC,50.00%,87.50%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * update: 添加问题“3372.连接两棵树后最大目标节点数目I”的代码和题解(#962)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * fix: 空间复杂度(#962)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -0,0 +1,49 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-28 21:43:27
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-28 22:55:41
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+private:
+    vector<vector<int>> buildTree(vector<vector<int>>& edges) {
+        vector<vector<int>> graph(edges.size() + 1);
+        for (vector<int>& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        return graph;
+    }
+
+    int dfs(vector<vector<int>>& graph, int lastNode, int thisNode, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        int ans = 1;
+        for (int nextNode : graph[thisNode]) {
+            if (nextNode == lastNode) {
+                continue;
+            }
+            ans += dfs(graph, thisNode, nextNode, k - 1);
+        }
+        return ans;
+    }
+public:
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        vector<vector<int>> graph2 = buildTree(edges2);
+        int add = 0;
+        for (int i = 0; i <= edges2.size(); i++) {
+            add = max(add, dfs(graph2, -1, i, k - 1));
+        }
+        vector<vector<int>> graph1 = buildTree(edges1);
+        vector<int> ans(graph1.size());
+        for (int i = 0; i < ans.size(); i++) {
+            ans[i] = dfs(graph1, -1, i, k) + add;
+        }
+        return ans;
+    }
+};
---
+++ Codes/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.java
@@ -0,0 +1,49 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-05-28 21:43:27
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-05-28 23:21:21
+ */
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    private List<Integer>[] buildTree(int[][] edges) {
+        List<Integer>[] ans = new ArrayList[edges.length + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            ans[edge[0]].add(edge[1]);
+            ans[edge[1]].add(edge[0]);
+        }
+        return ans;
+    }
+
+    private int dfs(List<Integer>[] tree, int lastNode, int thisNode, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        int ans = 1;
+        for (int nextNode : tree[thisNode]) {
+            if (nextNode != lastNode) {
+                ans += dfs(tree, thisNode, nextNode, k - 1);
+            }
+        }
+        return ans;
+    }
+
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
+        List<Integer>[] tree2 = buildTree(edges2);
+        int add = 0;
+        for (int i = 0; i < tree2.length; i++) {
+            add = Math.max(add, dfs(tree2, -1, i, k - 1));
+        }
+        List<Integer>[] tree1 = buildTree(edges1);
+        int[] ans = new int[tree1.length];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = add + dfs(tree1, -1, i, k);
+        }
+        return ans;
+    }
+}
