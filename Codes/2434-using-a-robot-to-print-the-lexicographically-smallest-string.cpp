1a90978aa2ade24799210ae5eae0b63de330a38d
LetMeFly666 <814114971@qq.com>
2025-06-06 22:28:14 +0800

    2434: AC.cpp+Re.py (#972)
    cpp - AC,57.09%,38.32%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-06-06 21:49:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-06 22:17:57
+ * @LastEditTime: 2025-06-06 22:19:18
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -19,7 +19,7 @@ public:
         string ans;
         for (int i = 0; i < s.size(); i++) {
             st.push(s[i]);
-            while (st.size() && st.top() <= mini[i]) {
+            while (st.size() && st.top() <= mini[i + 1]) {
                 ans += st.top();
                 st.pop();
             }
--- Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,3 +1,18 @@
+'''
+Author: LetMeFly
+Date: 2025-06-06 21:49:43
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-06-06 22:27:47
+'''
 class Solution:
     def robotWithString(self, s: str) -> str:
-        
+        mini = ['z' * (len(s) + 1)]
+        for i in range(len(s) - 1, -1, -1):
+            mini[i] = min(mini[i + 1], s[i])
+        ans = []
+        st = []
+        for i, c in enumerate(s):
+            st.append(c)
+            while st and st[-1] <= mini[i + 1]:
+                ans.append(st.pop())
+        return ''.join(ans)
