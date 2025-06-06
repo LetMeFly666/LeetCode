da6e41f4ee80c0fed5ece5789058568f1d068bc4
LetMeFly666 <814114971@qq.com>
2025-06-06 22:37:44 +0800

    2434: AC.py+CE.java (#972)
    py - AC,72.97%,5.40%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -1,5 +1,28 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-06-06 21:49:43
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-06-06 22:37:10
+ */
+import java.util.StringBuilder;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 class Solution {
     public String robotWithString(String s) {
-        
+        char[] mini = new char[s.length() + 1];
+        mini[s.length()] = 'z';
+        for (int i = s.length() - 1; i >= 0; i--) {
+            mini[i] = Math.min(mini[i + 1], s.charAt(i));
+        }
+        StringBuilder ans = new StringBuilder(s.length());
+        Deque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            st.push(s.charAt(i));
+            while (!st.isEmpty() && st.peek() <= mini[i + 1]) {
+                ans.append(st.pop());
+            }
+        }
+        return ans.toString();
     }
 }
--- Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -2,11 +2,11 @@
 Author: LetMeFly
 Date: 2025-06-06 21:49:43
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-06-06 22:27:47
+LastEditTime: 2025-06-06 22:30:06
 '''
 class Solution:
     def robotWithString(self, s: str) -> str:
-        mini = ['z' * (len(s) + 1)]
+        mini = ['z'] * (len(s) + 1)
         for i in range(len(s) - 1, -1, -1):
             mini[i] = min(mini[i + 1], s[i])
         ans = []
