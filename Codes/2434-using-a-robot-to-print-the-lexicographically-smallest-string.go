bc6f00c59cfd035e110989708f4c908d1b00fcff
LetMeFly666 <814114971@qq.com>
2025-06-06 23:09:49 +0800

    2434: AC.go(#972)
    go - AC,80.00%,93.33%
    原来是用这种方式来解决的吗
    
    make的第三个参数是容量
    如make([]int, 0, n)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.go
+++ Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.go
@@ -2,12 +2,13 @@
  * @Author: LetMeFly
  * @Date: 2025-06-06 21:49:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-06 22:44:45
+ * @LastEditTime: 2025-06-06 23:06:01
  */
 package main
 
 func robotWithString(s string) string {
-    mini := make([]byte, len(s) + 1, 'z')
+    mini := make([]byte, len(s) + 1)
+    mini[len(s)] = 'z'
     for i := len(s) - 1; i >= 0; i-- {
         mini[i] = min(mini[i + 1], s[i])
     }
