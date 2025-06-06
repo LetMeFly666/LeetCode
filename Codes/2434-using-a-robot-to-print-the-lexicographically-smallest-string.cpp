8f96e477708ba2d0c1ecf26813ee0613e1323ff2
LetMeFly666 <814114971@qq.com>
2025-06-06 22:08:54 +0800

    2434: WA.cpp (#972)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ Codes/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-06-06 21:49:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-06 22:01:20
+ * @LastEditTime: 2025-06-06 22:08:25
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -14,7 +14,7 @@ public:
         string ans;
         stack<char> st;
         for (char c : s) {
-            while (st.size() && c > st.top()) {
+            while (st.size() && c < st.top()) {
                 ans += st.top();
                 st.pop();
             }
