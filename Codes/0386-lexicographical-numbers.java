2b3ab22d9e17fc59dc4c459f688dfe6f0ff68b6f
LetMeFly666 <814114971@qq.com>
2025-06-09 22:00:42 +0800

    386: CE.java (#974)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/0386-lexicographical-numbers.java
+++ Codes/0386-lexicographical-numbers.java
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-06-09 10:09:21
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-09 21:53:25
+ * @LastEditTime: 2025-06-09 21:59:20
  */
 import java.util.List;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> ans = new ArrayList<>();
         for (int now = 1, i = 0; i < n; i++) {
-            ans[i] = now;
+            ans.offer(now);
             if (now * 10 <= n) {
                 now *= 10;
             } else {
