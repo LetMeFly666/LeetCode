da96cee762a36cc2d950718610590cb560a417c5
LetMeFly666 <814114971@qq.com>
2025-06-10 23:41:36 +0800

    3442: AC.Cpp+Py+Go{Java (#978)
    int数组竟然也能 for(int t : a)
    
    cpp - AC,35.86%,85.52%
    py - AC,77.03%,77.03%
    go - AC,100.00%,62.50%
    java - AC,100.00%,57.67%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3442-maximum-difference-between-even-and-odd-frequency-i.java
+++ Codes/3442-maximum-difference-between-even-and-odd-frequency-i.java
@@ -1,5 +1,23 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-06-10 23:07:14
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-06-10 23:40:04
+ */
 class Solution {
     public int maxDifference(String s) {
-        
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        int a1 = 0, a2 = 100;
+        for (int t : cnt) {
+            if (t % 2 == 1) {
+                a1 = Math.max(a1, t);
+            } else if (t > 0) {
+                a2 = Math.min(a2, t);
+            }
+        }
+        return a1 - a2;
     }
 }
