c35c4b4d9a1c21fc185c7822420a3288ce3746a8
LetMeFly666 <814114971@qq.com>
2025-06-10 23:11:53 +0800

    3442: WA.cpp (#978)
    what? 这能WA
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-06-10 23:07:14
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-06-10 23:09:00
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int maxDifference(string s) {
+        int cnt[26] = {0};
+        for (char c : s) {
+            cnt[c - 'a']++;
+        }
+        int a1 = 0, a2 = 100;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] % 2) {
+                a1 = max(a1, cnt[i]);
+            } else {
+                a2 = min(a2, cnt[i]);
+            }
+        }
+        return a1 - a2;
+    }
+};
---
+++ Codes/3442-maximum-difference-between-even-and-odd-frequency-i.go
@@ -0,0 +1,3 @@
+func maxDifference(s string) int {
+    
+}
