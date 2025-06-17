e342947d7dc5a76b523e4804213edbe511aebc5f
LetMeFly666 <814114971@qq.com>
2025-06-17 23:46:46 +0800

    1432: WA.cpp (#985)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1432-max-difference-you-can-get-from-changing-an-integer.cpp
+++ Codes/1432-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-06-15 22:35:58
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-17 10:49:41
+ * @LastEditTime: 2025-06-17 23:42:32
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -32,11 +32,18 @@ public:
         int ans = 0;
         string s = to_string(num);
         for (int a = 0; a < 10; a++) {
-            
+            if (!isIn(s, '0' + a)) {
+                continue;
+            }
             for (int b = 0; b < 10; b++) {
+                string x = replace(s, '0' + a, '0' + b);
                 for (int c = 0; c < 10; c++) {
+                    if (!isIn(x, '0' + c)) {
+                        continue;
+                    }
                     for (int d = 0; d < 10; d++) {
-                        
+                        string y = replace(x, '0' + c, '0' + d);
+                        ans = max(ans, abs(atoi(x.c_str()) - atoi(y.c_str())));
                     }
                 }
             }
