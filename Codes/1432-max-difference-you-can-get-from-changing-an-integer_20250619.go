6d277564410f4ab9a1db217c35faae22b36163cb
LetMeFly666 <814114971@qq.com>
2025-06-19 13:49:32 +0800

    1432: WA (#985)
    解答错误
    1 / 211 个通过的测试用例
    
    F**  事儿真多
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/1432-max-difference-you-can-get-from-changing-an-integer_20250619.go
+++ Codes/1432-max-difference-you-can-get-from-changing-an-integer_20250619.go
@@ -2,20 +2,39 @@
  * @Author: LetMeFly
  * @Date: 2025-06-19 10:16:46
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-19 13:38:19
+ * @LastEditTime: 2025-06-19 13:47:56
  */
 package main
+
+import (
+	"strconv"
+	"strings"
+)
 
 // M: !9->9
 // m: first==1 ? !01->0 : first->1
 func maxDiff(num int) int {
     s := string(num)
-    var M, m int
+    M, m := num, num
     change := func(s string, a, b byte) int {
-        for i := range s {
-            if s[i] == a {
-                s[i] = b
-            }
+        ans, _ := strconv.Atoi(strings.ReplaceAll(s, string(a), string(b)))
+        return ans
+    }
+    for i := range s {
+        if s[i] != '9' {
+            M = change(s, s[i], '9')
+            break
         }
     }
+    if s[0] == '1' {
+        for i := range s {
+            if s[i] > '1' {
+                m = change(s, s[i], '0')
+                break
+            }
+        }
+    } else {
+        m = change(s, s[0], '1')
+    }
+    return M - m
 }
