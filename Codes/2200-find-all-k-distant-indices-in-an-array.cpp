d5ae8e92258df360284f590526c52c8f1581007f
LetMeFly666 <814114971@qq.com>
2025-06-25 22:29:09 +0800

    2200: WA.cpp (#997)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2200-find-all-k-distant-indices-in-an-array.cpp
+++ Codes/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -13,7 +13,7 @@ public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
         vector<int> ans;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = max(0, i - k + 1); j <= min(int(nums.size()) - 1, i + k - 1); j++) {
+            for (int j = max(0, i - k); j <= min(int(nums.size()) - 1, i + k); j++) {
                 if (nums[j] == key) {
                     ans.push_back(j);
                     break;
