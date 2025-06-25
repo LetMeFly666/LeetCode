073db235edb41c2bb406e7ea25497f3818bb33a7
LetMeFly666 <814114971@qq.com>
2025-06-25 22:31:02 +0800

    2200: AC.cpp - O(n^2) (#997)
    cpp - AC,46.29%,63.21%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2200-find-all-k-distant-indices-in-an-array.cpp
+++ Codes/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-06-25 22:24:22
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-25 22:28:06
+ * @LastEditTime: 2025-06-25 22:29:22
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -15,7 +15,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             for (int j = max(0, i - k); j <= min(int(nums.size()) - 1, i + k); j++) {
                 if (nums[j] == key) {
-                    ans.push_back(j);
+                    ans.push_back(i);
                     break;
                 }
             }
