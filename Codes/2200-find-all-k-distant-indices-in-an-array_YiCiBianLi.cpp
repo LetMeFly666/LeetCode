59c648107742a92e35969f2226c65e9087bfee7f
LetMeFly666 <814114971@qq.com>
2025-06-25 22:44:32 +0800

    2200: AC.all(CPP+JAVA+PYTHON+GOLANG) - O(n) (#997)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2200-find-all-k-distant-indices-in-an-array_YiCiBianLi.cpp
+++ Codes/2200-find-all-k-distant-indices-in-an-array_YiCiBianLi.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-06-25 22:30:38
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-06-25 22:32:27
+ * @LastEditTime: 2025-06-25 22:34:30
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -14,7 +14,8 @@ public:
         vector<int> ans;
         for (int i = 0, j = 0; i < nums.size(); i++) {
             if (nums[i] == key) {
-                for (j = max(j, i - k); j <= i + k; j++) {
+                int l = max(j, i - k), r = min(i + k, (int)nums.size() - 1);
+                for (j = l; j <= r; j++) {
                     ans.push_back(j);
                 }
             }
--- Codes/2200-find-all-k-distant-indices-in-an-array_YiCiBianLi.py
+++ Codes/2200-find-all-k-distant-indices-in-an-array_YiCiBianLi.py
@@ -1,3 +1,18 @@
+'''
+Author: LetMeFly
+Date: 2025-06-25 22:24:22
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-06-25 22:38:19
+'''
+from typing import List
+
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
-        
+        ans = []
+        last = 0
+        for i in range(len(nums)):
+            if nums[i] == key:
+                for j in range(max(last, i - k), min(len(nums), i + k + 1)):
+                    ans.append(j)
+                last = i + k + 1
+        return ans
