2de14095c230b0097b93f600b76251d9cd141d19
LetMeFly666 <814114971@qq.com>
2025-06-26 22:30:06 +0800

    2311: AC.cpp + WA.py (#1000)
    cpp - AC,4.00%,64.00%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-06-26 22:16:30
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-06-26 22:25:08
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        int ans = 0, val = 0, k_length = 32 - __builtin_clz(k), n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (s[n - i - 1] == '0') {
+                ans++;
+                continue;
+            }
+            if (i <= k_length && val + (1 << i) <= k) {
+                val += 1 << i;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
