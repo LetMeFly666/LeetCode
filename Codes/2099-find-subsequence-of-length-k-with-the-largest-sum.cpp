8df9aa58b7eb173f98fa72a58470bf29a3572fff
LetMeFly666 <814114971@qq.com>
2025-07-04 12:48:27 +0800

    2099: WA.cpp (#1003) - 似乎可以排序（昨晚submitted）
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-03 21:31:48
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-03 21:34:56
+ * @LastEditTime: 2025-07-03 21:36:06
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -11,7 +11,7 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        int M = 0, l = 0, r = k - 1;
+        int M = 0, l = 0;
         for (int i = 0; i < k; i++) {
             M += nums[i];
         }
@@ -20,9 +20,8 @@ public:
             if (now > M) {
                 M = now;
                 l = i;
-                r = i + k - 1;
             }
         }
-        return vector<int>(nums.begin() + i, nums.begin() + i + k - 1);
+        return vector<int>(nums.begin() + l, nums.begin() + l + k - 1);
     }
 };
