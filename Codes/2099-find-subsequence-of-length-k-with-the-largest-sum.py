1d448c135b92cfadba1a437156891700c8633ab0
LetMeFly666 <814114971@qq.com>
2025-07-06 00:09:34 +0800

    2099: RE.py (#1003)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.py
+++ Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,3 +1,15 @@
+'''
+Author: LetMeFly
+Date: 2025-07-03 21:31:48
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-07-06 00:08:16
+'''
+from typing import List
+
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        
+        idx = [i for i in range(len(nums))]
+        idx.sort(lambda i : nums[i])
+        idx = idx[:k]
+        idx.sort()
+        return [nums[i] for i in idx]
