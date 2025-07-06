a9d72efb2884191099342d6bde45fb950e21f60d
LetMeFly666 <814114971@qq.com>
2025-07-06 21:57:49 +0800

    2099: WA.java (#1003)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -2,14 +2,14 @@
  * @Author: LetMeFly
  * @Date: 2025-07-03 21:31:48
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-06 21:50:33
+ * @LastEditTime: 2025-07-06 21:53:56
  */
 import java.util.Arrays;
 
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-        int[] idx = new int[len(nums)];
-        for (int i = 0; i < len(nums); i++) {
+        Integer[] idx = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
             idx[i] = i;
         }
         Arrays.sort(idx, (i, j) -> nums[j] - nums[i]);
