35dd1fd4848a1856b401c56a00d52510ed532050
LetMeFly666 <814114971@qq.com>
2025-07-06 21:52:08 +0800

    2099: CE.java (#1003)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,5 +1,23 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-03 21:31:48
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-06 21:50:33
+ */
+import java.util.Arrays;
+
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-        
+        int[] idx = new int[len(nums)];
+        for (int i = 0; i < len(nums); i++) {
+            idx[i] = i;
+        }
+        Arrays.sort(idx, (i, j) -> nums[j] - nums[i]);
+        int[] ans = new int[k];
+        Arrays.sort(idx);
+        for (int i = 0; i < k; i++) {
+            ans[i] = nums[idx[i]];
+        }
+        return ans;
     }
 }
