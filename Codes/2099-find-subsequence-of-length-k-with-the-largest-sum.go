23b6ee82260cdba8c4e2ec17719545a0e1fc4495
LetMeFly666 <814114971@qq.com>
2025-07-07 21:01:00 +0800

    archive: go on the train (#1003)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.go
+++ Codes/2099-find-subsequence-of-length-k-with-the-largest-sum.go
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-03 21:31:48
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-06 22:08:12
+ * @LastEditTime: 2025-07-06 22:16:49
  */
 package main
 
@@ -14,7 +14,7 @@ func maxSubsequence(nums []int, k int) []int {
         idx[i] = i
     }
     sort.Slice(idx, func(i, j int) bool {
-        return nums[i] > nums[j] 
+        return nums[idx[i]] > nums[idx[j]]  // 不可以nums[i] > nums[j]，因为排序过程中idx[i]可能不再是i 
     })
     idx = idx[:k]
     sort.Ints(idx)
