303493c7cf07674a8df6881c96819067b3d3fb71
LetMeFly666 <814114971@qq.com>
2025-07-11 23:35:21 +0800

    3269: AC.cpp (#1011)
    AC,5.02%,5.02%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3169-count-days-without-meetings.cpp
+++ Codes/3169-count-days-without-meetings.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-11 23:25:31
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-11 23:31:10
+ * @LastEditTime: 2025-07-11 23:33:35
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -15,13 +15,14 @@ public:
         int ans = 0;
         int last = 0;
         for (vector<int> me : meetings) {
+            // printf("last = %d, me = [%d, %d]\n", last, me[0], me[1]);
             if (me[0] > last + 1) {
                 ans += me[0] - last - 1;
+                // printf("ans += %d\n", me[0] - last - 1);
             }
-            // printf("last = %d, me = [%d, %d]\n", last, me[0], me[1]);
             last = max(last, me[1]);
         }
-        ans += days - last - 1;
+        ans += days - last;
         return ans;
     }
 };
