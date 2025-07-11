cb5db70e4a5bb6fc121220dda8850e9382ca2bb1
LetMeFly666 <814114971@qq.com>
2025-07-11 23:29:55 +0800

    3269: wa.cpp (#1011)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3169-count-days-without-meetings.cpp
+++ Codes/3169-count-days-without-meetings.cpp
@@ -1,6 +1,25 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-11 23:25:31
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-11 23:28:50
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        
+        sort(meetings.begin(), meetings.end());
+        int ans = 0;
+        int last = 0;
+        for (vector<int> me : meetings) {
+            if (me[0] > last + 1) {
+                ans += me[0] - last;
+            }
+            last = max(last, me[1]);
+        }
+        return ans;
     }
 };
