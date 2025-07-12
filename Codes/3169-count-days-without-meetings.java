3e4024fa6cdace8f9482fe84cc00f0f7966ca958
LetMeFly666 <814114971@qq.com>
2025-07-12 16:56:00 +0800

    3169: RE.java (#1011)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3169-count-days-without-meetings.java
+++ Codes/3169-count-days-without-meetings.java
@@ -10,7 +10,7 @@ class Solution {
     public int countDays(int days, int[][] meetings) {
         int ans = 0;
         int last = 0;
-        Arrays.sort(meetings, (a, b) -> {a[0] == b[0] ? a[1] < b[1] : a[0] < b[0];});
+        Arrays.sort(meetings, (a, b) -> a[0] == b[0] ? a[1] < b[1] : a[0] < b[0]);
         for (int[] me : meetings) {
             if (me[0] > last + 1) {
                 ans += me[0] - last - 1;
