da5cc56eb5ebb1642bdb6ba57ee623af8b15f44c
LetMeFly666 <814114971@qq.com>
2025-07-12 16:59:20 +0800

    3169: AC.java (#1011)
    AC,74.09%,61.14%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3169-count-days-without-meetings.java
+++ Codes/3169-count-days-without-meetings.java
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-11 23:25:31
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-12 16:54:49
+ * @LastEditTime: 2025-07-12 16:58:44
  */
 import java.util.Arrays;
 
@@ -10,7 +10,7 @@ class Solution {
     public int countDays(int days, int[][] meetings) {
         int ans = 0;
         int last = 0;
-        Arrays.sort(meetings, (a, b) -> a[0] == b[0] ? a[1] < b[1] : a[0] < b[0]);
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
         for (int[] me : meetings) {
             if (me[0] > last + 1) {
                 ans += me[0] - last - 1;
