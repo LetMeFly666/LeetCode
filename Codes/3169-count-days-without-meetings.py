3058f1d3e7d86c8d044cb475e9d748e54aa8076d
LetMeFly666 <814114971@qq.com>
2025-07-12 11:59:37 +0800

    3169: begin.py (#1011)
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3169-count-days-without-meetings.py
+++ Codes/3169-count-days-without-meetings.py
@@ -1,3 +1,11 @@
+'''
+Author: LetMeFly
+Date: 2025-07-11 23:25:31
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-07-12 11:59:19
+'''
+from typing import List
+
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         
