3150d581a0d1ec6236810beac2ca29fd4510e078
LetMeFly666 <814114971@qq.com>
2025-07-12 12:00:49 +0800

    3169: ac.py (#1011)
    AC,32.88%,96.40%
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

--- Codes/3169-count-days-without-meetings.py
+++ Codes/3169-count-days-without-meetings.py
@@ -2,10 +2,17 @@
 Author: LetMeFly
 Date: 2025-07-11 23:25:31
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-07-12 11:59:19
+LastEditTime: 2025-07-12 12:00:22
 '''
 from typing import List
 
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        
+        ans = last = 0
+        meetings.sort()
+        for l, r in meetings:
+            if l > last + 1:
+                ans += l - last - 1
+            last = max(last, r)
+        ans += days - last
+        return ans
