a70d2eb29980977a8750f5f719adac90d233638f
LetMeFly666 <814114971@qq.com>
2025-07-13 16:57:47 +0800

    2410: AC.cpp (#1022)

--- Codes/2410-maximum-matching-of-players-with-trainers.cpp
+++ Codes/2410-maximum-matching-of-players-with-trainers.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-13 16:44:48
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-13 16:55:18
+ * @LastEditTime: 2025-07-13 16:57:27
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -14,7 +14,7 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        sort(players.begin(). players.end());
+        sort(players.begin(), players.end());
         sort(trainers.begin(), trainers.end());
         int ans = 0;
         for (int t = 0, p = 0; t < trainers.size() && p < players.size(); t++) {
