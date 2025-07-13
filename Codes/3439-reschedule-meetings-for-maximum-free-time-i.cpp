c19f03b77781627ddef524ace32084a48cb6abdc
LetMeFly666 <814114971@qq.com>
2025-07-13 22:04:14 +0800

    3439: AC.cpp (#1024)
    cpp - AC,100.00%,94.69%

--- Codes/3439-reschedule-meetings-for-maximum-free-time-i.cpp
+++ Codes/3439-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-13 21:44:10
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-13 21:56:30
+ * @LastEditTime: 2025-07-13 22:03:24
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -18,9 +18,10 @@ public:
         int ans = 0;
         int n = startTime.size();
         for (int i = k; i <= n; i++) {
-            int l = (i == k ? 0 : startTime[i - k]) + cnt;
+            int l = (i == k ? 0 : endTime[i - k - 1]) + cnt;
             int r = (i == n ? eventTime : startTime[i]);
             ans = max(ans, r - l);
+            // printf("i = %d, l = %d, r = %d, cnt = %d, ans = %d\n", i, l, r, cnt, ans);
             if (i == n) {
                 break;
             }
