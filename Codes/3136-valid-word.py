a70333bc09cd98c86cef8720026c890d814e1b66
LetMeFly666 <814114971@qq.com>
2025-07-15 23:31:30 +0800

    3136: AC.py (#1028)
    py - AC,27.85%,25.32%

--- Codes/3136-valid-word.py
+++ Codes/3136-valid-word.py
@@ -2,20 +2,16 @@
 Author: LetMeFly
 Date: 2025-07-15 23:15:03
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-07-15 23:28:33
+LastEditTime: 2025-07-15 23:30:52
 '''
 class Solution:
-    def isYuan(self, c: str) -> bool:
-        return c in 'aeiou'
-    
     def isValid(self, word: str) -> bool:
         if len(word) < 3:
             return False
         ok = [False, False]
         for c in word:
             if c.isalpha():
-                ok[self.isYuan(c)] = True
+                ok[c.lower() in 'aeiou'] = True
             elif not c.isdigit():
                 return False
-        print(ok)
         return all(ok)
