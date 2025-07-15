c1324fb9b661efa4b0d1de72aeb5e2a09f9c63c5
LetMeFly666 <814114971@qq.com>
2025-07-15 23:35:26 +0800

    3136: CE.java (#1028)
    Line 17: error: for-each not applicable to expression type
            for (char c : word) {
                          ^
      required: array or java.lang.Iterable
      found:    String

--- Codes/3136-valid-word.java
+++ Codes/3136-valid-word.java
@@ -1,5 +1,33 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-15 23:15:03
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-15 23:33:39
+ */
 class Solution {
+    private boolean isYuan(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
     public boolean isValid(String word) {
-        
+        if (word.length() < 3) {
+            return false;
+        }
+        boolean hasYuan = false, hasFu = false;
+        for (char c : word) {
+            if ('A' <= c && c <= 'Z') {
+                c += 32;
+            }
+            if ('a' <= c && c <= 'z') {
+                if (isYuan(c)) {
+                    hasYuan = true;
+                } else {
+                    hasFu = true;
+                }
+            } else if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return hasYuan && hasFu;
     }
 }
