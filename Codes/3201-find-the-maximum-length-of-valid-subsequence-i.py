61b04f1a1df1405bb9a764338b23d09a55c245e9
LetMeFly666 <814114971@qq.com>
2025-07-16 13:37:10 +0800

    3201: WA.py (#1030)

--- Codes/3201-find-the-maximum-length-of-valid-subsequence-i.py
+++ Codes/3201-find-the-maximum-length-of-valid-subsequence-i.py
@@ -1,3 +1,23 @@
+'''
+Author: LetMeFly
+Date: 2025-07-16 13:16:29
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-07-16 13:36:53
+'''
+from typing import List
+
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        
+        ans = odd = 0
+        last = False if nums[0] % 2 else True
+        for t in nums:
+            if t % 2:
+                odd += 1
+                if not last:
+                    last = True
+                    ans += 1
+            else:
+                if last:
+                    last = False
+                    ans += 1
+        return ans
