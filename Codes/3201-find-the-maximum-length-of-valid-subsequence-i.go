89ba82e63af37b0412ee3a02cd074f27d45afc31
LetMeFly666 <814114971@qq.com>
2025-07-16 13:42:52 +0800

    3201: AC.java+go (#1030)
    java - AC,80.00%,75.00%
    go - AC,72.73%,72.73%

--- Codes/3201-find-the-maximum-length-of-valid-subsequence-i.go
+++ Codes/3201-find-the-maximum-length-of-valid-subsequence-i.go
@@ -1,3 +1,27 @@
-func maximumLength(nums []int) int {
-    
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-16 13:16:29
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-16 13:40:18
+ */
+package main
+
+func maximumLength(nums []int) (ans int) {
+    odd := 0
+    last := nums[0] % 2 == 0
+    for _, t := range nums {
+        if t % 2 == 0 {
+            if last {
+                last = false
+                ans++
+            }
+        } else {
+            odd++
+            if !last {
+                last = true
+                ans++
+            }
+        }
+    }
+    return max(ans, max(odd, len(nums) - odd))
 }
--- Codes/3201-find-the-maximum-length-of-valid-subsequence-i.java
+++ Codes/3201-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,5 +1,28 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-16 13:16:29
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-16 13:41:23
+ */
 class Solution {
     public int maximumLength(int[] nums) {
-        
+        int ans = 0;
+        int odd = 0;
+        boolean last = nums[0] % 2 == 0;
+        for (int t : nums) {
+            if (t % 2 == 0) {
+                if (last) {
+                    last = false;
+                    ans++;
+                }
+            } else {
+                odd++;
+                if (!last) {
+                    last = true;
+                    ans++;
+                }
+            }
+        }
+        return Math.max(ans, Math.max(odd, nums.length - odd));
     }
 }
