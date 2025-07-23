f14796e05f73a77bc34dd2725727aae7e043e8eb
LetMeFly666 <814114971@qq.com>
2025-07-23 13:18:26 +0800

    1695: WA.cpp (#1041)

--- Codes/1695-maximum-erasure-value.cpp
+++ Codes/1695-maximum-erasure-value.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-23 10:31:42
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-23 10:46:04
+ * @LastEditTime: 2025-07-23 13:18:00
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -15,15 +15,22 @@ public:
         int ans = 0, cnt = 0;
         for (int l = 0, r = 0; r < nums.size(); r++) {
             if (had.count(nums[r])) {
-                while (nums[l] != nums[r]) {
-                    cnt -= nums[l++];
-                }
+                do {
+                    cnt -= nums[l];
+                } while (nums[l++] != nums[r]);
             } else {
                 had.insert(nums[r]);
             }
             cnt += nums[r];
+            printf("l = %d, r = %d, cnt = %d\n", l, r, cnt);
             ans = max(ans, cnt);
         }
         return ans;
     }
 };
+
+/*
+[187,470,25,436,538,809,441,167,477,110,275,133,666,345,411,459,490,266,987,965,429,166,809,340,467,318,125,165,809,610,31,585,970,306,42,189,169,743,78,810,70,382,367,490,787,670,476,278,775,673,299,19,893,817,971,458,409,886,434]
+9252
+16911
+*/
