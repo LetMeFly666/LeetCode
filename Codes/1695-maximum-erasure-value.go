c11d76bbe2af01a2826e39d89450cd8633474576
LetMeFly666 <814114971@qq.com>
2025-07-23 13:31:20 +0800

    1695: archive.go (#1041)

--- Codes/1695-maximum-erasure-value.go
+++ Codes/1695-maximum-erasure-value.go
@@ -1,3 +1,12 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-23 10:31:42
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-23 13:31:00
+ */
+package main
+
+// 这个可能晚上再写了 - 一次写多种语言的效果可能没有多次写多种语言的效果好
 func maximumUniqueSubarray(nums []int) int {
     
 }
