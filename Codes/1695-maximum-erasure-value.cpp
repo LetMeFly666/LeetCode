1a1e513cb18006158084e4bbcb70ff808adeb65f
LetMeFly666 <814114971@qq.com>
2025-07-23 10:46:26 +0800

    1695: WA.cpp (#1041)

--- Codes/1695-maximum-erasure-value.cpp
+++ Codes/1695-maximum-erasure-value.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-23 10:31:42
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-23 10:43:50
+ * @LastEditTime: 2025-07-23 10:46:04
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -12,15 +12,17 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         unordered_set<int> had;
-        int ans = 0;
+        int ans = 0, cnt = 0;
         for (int l = 0, r = 0; r < nums.size(); r++) {
             if (had.count(nums[r])) {
                 while (nums[l] != nums[r]) {
-                    l++;
+                    cnt -= nums[l++];
                 }
+            } else {
+                had.insert(nums[r]);
             }
-            had.insert(nums[r]);
-            ans = max(ans, r - l + 1);
+            cnt += nums[r];
+            ans = max(ans, cnt);
         }
         return ans;
     }
