dcf4c196643c5483d55ec58438fcc26c014efbcb
LetMeFly666 <814114971@qq.com>
2025-07-23 13:30:14 +0800

    1695: AC.py (#1041)
    py - AC,87.44%,37.89%

--- Codes/1695-maximum-erasure-value.py
+++ Codes/1695-maximum-erasure-value.py
@@ -1,3 +1,21 @@
+'''
+Author: LetMeFly
+Date: 2025-07-23 10:31:42
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-07-23 13:29:36
+'''
+from typing import List
+
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        
+        had = set()
+        ans = l = cnt = 0
+        for v in nums:
+            while v in had:
+                had.remove(nums[l])
+                cnt -= nums[l]
+                l += 1
+            had.add(v)
+            cnt += v
+            ans = max(ans, cnt)
+        return ans
