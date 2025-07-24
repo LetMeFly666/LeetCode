e49166f81856b088925720af58fa0a98117dc74b
LetMeFly666 <814114971@qq.com>
2025-07-24 13:16:40 +0800

    1695: WA.go (#1041)

--- Codes/1695-maximum-erasure-value.go
+++ Codes/1695-maximum-erasure-value.go
@@ -2,18 +2,17 @@
  * @Author: LetMeFly
  * @Date: 2025-07-23 10:31:42
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-24 13:08:34
+ * @LastEditTime: 2025-07-24 13:16:17
  */
 package main
 
 import "fmt"
 
-// 昨天晚上干别的去了，今天中午补上，间隔时间更久效果更好(bushi)
 func maximumUniqueSubarray(nums []int) (ans int) {
     had := map[int]struct{}{}
     l, cnt := 0, 0
     for _, t := range nums {
-        for _, ok := had[t]; ok; {
+        for _, ok := had[t]; ok; _, ok = had[t] {
             delete(had, nums[l])
             cnt -= nums[l]
             l++
@@ -24,8 +23,8 @@ func maximumUniqueSubarray(nums []int) (ans int) {
     return
 }
 
-func main() {
-    nums := []int{4, 2, 4, 5, 6}
-    ans := maximumUniqueSubarray(nums)
-    fmt.Println(ans)
-}
+// func main() {
+//     nums := []int{4, 2, 4, 5, 6}
+//     ans := maximumUniqueSubarray(nums)
+//     fmt.Println(ans)
+// }
