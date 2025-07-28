cbf101757a21ad353f853e28c10d568137a52952
LetMeFly666 <814114971@qq.com>
2025-07-28 21:44:43 +0800

    2210: WA.cpp (#1047)

--- Codes/2210-count-hills-and-valleys-in-an-array.cpp
+++ Codes/2210-count-hills-and-valleys-in-an-array.cpp
@@ -12,12 +12,13 @@ class Solution {
 public:
     int countHillValley(vector<int>& nums) {
         int ans = 0;
+        int last = nums[0];
         for (int i = 1; i < nums.size() - 1; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+            if (nums[i] > last && nums[i] > nums[i + 1]) {
                 ans++;
+            }
+            if (nums[i] != nums[i - 1]) {
+                last = nums[i];
             }
         }
         return ans;
