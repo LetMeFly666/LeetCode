3ade025d6a2f003c4eed5f1043bfa0938dd11ed4
LetMeFly666 <814114971@qq.com>
2025-07-28 22:28:37 +0800

    2210: AC.java+go (#1047)
    java - AC,100.00%,26.13%
    go - AC,100.00%,38.98%

--- Codes/2210-count-hills-and-valleys-in-an-array.go
+++ Codes/2210-count-hills-and-valleys-in-an-array.go
@@ -1,3 +1,20 @@
-func countHillValley(nums []int) int {
-    
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-28 21:39:52
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-28 22:27:50
+ */
+package main
+
+func countHillValley(nums []int) (ans int) {
+    last := nums[0]
+    for i := 1; i < len(nums) - 1; i++ {
+        if nums[i] > last && nums[i] > nums[i + 1] || nums[i] < last && nums[i] < nums[i + 1] {
+            ans++
+        }
+        if nums[i] != nums[i + 1] {
+            last = nums[i]
+        }
+    }
+    return
 }
--- Codes/2210-count-hills-and-valleys-in-an-array.java
+++ Codes/2210-count-hills-and-valleys-in-an-array.java
@@ -1,5 +1,21 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-28 21:39:52
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-28 22:25:46
+ */
 class Solution {
     public int countHillValley(int[] nums) {
-        
+        int last = nums[0];
+        int ans = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] > last && nums[i] > nums[i + 1] || nums[i] < last && nums[i] < nums[i + 1]) {
+                ans++;
+            }
+            if (nums[i] != nums[i + 1]) {
+                last = nums[i];
+            }
+        }
+        return ans;
     }
 }
