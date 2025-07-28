3b842e61c39799e99432d6bcaa5388ec4fff551d
LetMeFly666 <814114971@qq.com>
2025-07-28 22:24:26 +0800

    2210: AC.py (#1047) - remarkable - 值得记录的错误修复
    py - AC,100.00%,78.02%

--- Codes/2210-count-hills-and-valleys-in-an-array.py
+++ Codes/2210-count-hills-and-valleys-in-an-array.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-07-28 21:39:52
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-07-28 22:20:34
+LastEditTime: 2025-07-28 22:23:33
 '''
 from typing import List
 
@@ -10,8 +10,9 @@ class Solution:
     def countHillValley(self, nums: List[int]) -> int:
         last = nums[0]
         ans = 0
-        for i, t in enumerate(nums[1:-1]):
-            ans += t > last and t > nums[i + 1] or t < last and t < nums[i + 1]
+        # for i, t in enumerate(nums[1:-1]):  # 不可！这样i=0时t=nums[1]
+        for i in range(1, len(nums) - 1):
+            ans += nums[i] > last and nums[i] > nums[i + 1] or nums[i] < last and nums[i] < nums[i + 1]
             if nums[i] != nums[i + 1]:
                 last = nums[i]
         return ans
