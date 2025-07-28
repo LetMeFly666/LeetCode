f5be6386f6364416237e68a21de4efaf37235dcd
LetMeFly666 <814114971@qq.com>
2025-07-28 20:46:29 +0800

    2044: AC.go+py - DFS回溯 - 剪枝 (#1048)
    go - AC,100.00%,73.68%
    py - AC,88.09%,83.33%
    java - AC,95.77%,78.87%

--- Codes/2044-count-number-of-maximum-bitwise-or-subsets_20250728-huisu-JianZhi.go
+++ Codes/2044-count-number-of-maximum-bitwise-or-subsets_20250728-huisu-JianZhi.go
@@ -2,9 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2025-07-28 13:38:06
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-28 20:12:27
+ * @LastEditTime: 2025-07-28 20:31:11
  */
 package main
+
+// import "fmt"
 
 var ans2044, maxium2044 int = 0, 0
 var nums2044 []int
@@ -22,9 +24,16 @@ func dfs2044(th, now int) {
 }
 
 func countMaxOrSubsets(nums []int) int {
+    nums2044 = nums
+    ans2044 = 0
+    maxium2044 = 0
     for _, t := range nums {
         maxium2044 |= t
     }
     dfs2044(0, 0)
     return ans2044
 }
+
+// func main() {
+//     fmt.Println(countMaxOrSubsets([]int{2, 2, 2}))
+// }
--- Codes/2044-count-number-of-maximum-bitwise-or-subsets_20250728-huisu-JianZhi.py
+++ Codes/2044-count-number-of-maximum-bitwise-or-subsets_20250728-huisu-JianZhi.py
@@ -1,3 +1,26 @@
+'''
+Author: LetMeFly
+Date: 2025-07-28 13:38:06
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-07-28 20:40:58
+'''
+from typing import List
+
 class Solution:
+    def dfs(self, th: int, now: int) -> None:
+        if now == self.M:
+            self.ans += 1 << (len(self.nums) - th)
+            return
+        if th == len(self.nums):
+            return
+        self.dfs(th + 1, now)
+        self.dfs(th + 1, now | self.nums[th])
+    
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        
+        self.ans = 0
+        self.nums = nums
+        self.M = 0
+        for t in nums:
+            self.M |= t
+        self.dfs(0, 0)
+        return self.ans
