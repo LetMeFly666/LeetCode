0b249ffe00fd04943d3d497da1755dafa1e72445
LetMeFly666 <814114971@qq.com>
2025-07-30 00:00:37 +0800

    2411: AC.cpp (#1052)
    cpp - AC,13.12%,37.10%

--- Codes/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ Codes/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-29 23:42:57
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-29 23:58:55
+ * @LastEditTime: 2025-07-30 00:00:01
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -12,13 +12,13 @@ class Solution {
 public:
     vector<int> smallestSubarrays(vector<int>& nums) {
         vector<int> ans(nums.size());
-        vector<int> lastPos(31, -1);
+        vector<int> lastPos(31);
         for (int i = nums.size() - 1; i >= 0; i--) {
             int last = i;
             for (int j = 0; j < 31; j++) {
                 if (nums[i] >> j & 1) {
                     lastPos[j] = i;
-                } else if (lastPos[j] != -1) {
+                } else {
                     last = max(last, lastPos[j]);
                 }
             }
