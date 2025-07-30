a2589ce7883e9d5455ccec6b08bed2e962e2ba94
LetMeFly666 <814114971@qq.com>
2025-07-30 20:58:21 +0800

    2411: WA.rust (#1052)
    Line 20: Char 45: index out of bounds: the len is 31 but the index is 31 (solution.rs)

--- Codes/2411-smallest-subarrays-with-maximum-bitwise-or.rs
+++ Codes/2411-smallest-subarrays-with-maximum-bitwise-or.rs
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-29 23:42:57
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-30 20:44:21
+ * @LastEditTime: 2025-07-30 20:56:38
  */
 use std::cmp::max;
 
@@ -11,15 +11,16 @@ impl Solution {
         let mut ans: Vec<i32> = vec![0; nums.len()];
         let mut lastPos: Vec<i32> = vec![0; 31];
         for i in (0..ans.len()).rev() {
-            let mut last: i32 = i;
+            let ii32: i32 = i.try_into().unwrap();
+            let mut last: i32 = ii32;
             for j in (0..32) {
                 if nums[i] >> j & 1 == 1 {
-                    lastPos[j] = i;
+                    lastPos[j] = ii32;
                 } else {
                     last = max(last, lastPos[j])
                 }
             }
-            ans[i] = max(ans[i], last - i + 1)
+            ans[i] = max(ans[i], last - ii32 + 1)
         }
         ans
     }
