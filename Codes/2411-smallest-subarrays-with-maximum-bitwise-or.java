8e17a4e7fff9c3e833ce1f23ff8a08ba7fae1f08
LetMeFly666 <814114971@qq.com>
2025-07-30 10:27:43 +0800

    2411: AC.py+go + half.java (#1052)
    py - AC,5.88%,93.14%
    go - AC,9.09%,43.18%

--- Codes/2411-smallest-subarrays-with-maximum-bitwise-or.java
+++ Codes/2411-smallest-subarrays-with-maximum-bitwise-or.java
@@ -1,5 +1,14 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-07-29 23:42:57
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-07-30 10:22:35
+ */
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
-        
+        int[] ans = new int[nums.length];
+        int[] lastPos = new int[31];
+        for (int i = )
+        return ans;
     }
 }
--- Codes/2411-smallest-subarrays-with-maximum-bitwise-or.py
+++ Codes/2411-smallest-subarrays-with-maximum-bitwise-or.py
@@ -2,13 +2,13 @@
 Author: LetMeFly
 Date: 2025-07-29 23:42:57
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-07-30 09:57:24
+LastEditTime: 2025-07-30 10:14:12
 '''
 from typing import List
 
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        ans = [0] * 31
+        ans = [0] * len(nums)
         lastPos = [0] * 31
         for i in range(len(nums) - 1, -1, -1):
             last = i
@@ -16,6 +16,6 @@ class Solution:
                 if nums[i] >> j & 1:
                     lastPos[j] = i
                 else:
-                    last = max(last, j)
+                    last = max(last, lastPos[j])
             ans[i] = max(ans[i], last - i + 1)
         return ans
