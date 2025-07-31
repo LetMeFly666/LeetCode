ce31fa4763b594a19d8946aeed16324380b230b9
LetMeFly666 <814114971@qq.com>
2025-07-31 10:44:48 +0800

    2411: RE.java (#1052)
    Line 14: error: bad operand types for binary operator '&'
                    if (nums[i] >> j & 1 == 1) {
                                     ^
      first type:  int
      second type: boolean

--- Codes/2411-smallest-subarrays-with-maximum-bitwise-or.java
+++ Codes/2411-smallest-subarrays-with-maximum-bitwise-or.java
@@ -2,13 +2,23 @@
  * @Author: LetMeFly
  * @Date: 2025-07-29 23:42:57
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-30 10:22:35
+ * @LastEditTime: 2025-07-31 10:43:36
  */
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
         int[] ans = new int[nums.length];
         int[] lastPos = new int[31];
-        for (int i = )
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int last = i;
+            for (int j = 0; j < 31; j++) {
+                if (nums[i] >> j & 1 == 1) {
+                    lastPos[j] = i;
+                } else {
+                    last = Math.max(last, lastPos[j]);
+                }
+            }
+            ans[i] = Math.max(ans[i], last - i + 1);
+        }
         return ans;
     }
 }
