520fdae6fc208cdfd35e04782abf9c96d5e54e75
LetMeFly666 <814114971@qq.com>
2025-07-31 10:45:38 +0800

    2411: AC.java (#1052)
    java - AC,27.05%,90.75%

--- Codes/2411-smallest-subarrays-with-maximum-bitwise-or.java
+++ Codes/2411-smallest-subarrays-with-maximum-bitwise-or.java
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-07-29 23:42:57
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-07-31 10:43:36
+ * @LastEditTime: 2025-07-31 10:45:02
  */
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
@@ -11,7 +11,7 @@ class Solution {
         for (int i = nums.length - 1; i >= 0; i--) {
             int last = i;
             for (int j = 0; j < 31; j++) {
-                if (nums[i] >> j & 1 == 1) {
+                if ((nums[i] >> j & 1) == 1) {
                     lastPos[j] = i;
                 } else {
                     last = Math.max(last, lastPos[j]);
