67a141d771f1b867334cca943c5ba3343d151b48
LetMeFly666 <814114971@qq.com>
2025-08-02 13:25:44 +0800

    0118: - AC.rust (#1057)
    rust - AC,100.00%,80.39%

--- Codes/0118-pascals-triangle_20250801.rs
+++ Codes/0118-pascals-triangle_20250801.rs
@@ -2,13 +2,14 @@
  * @Author: LetMeFly
  * @Date: 2025-08-01 23:51:32
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-02 13:20:39
+ * @LastEditTime: 2025-08-02 13:25:16
  */
 impl Solution {
     pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
-        let mut ans = vec![vec![]; num_rows];
-        for i in 0..num_rows {
-            ans[i].resize(i + 1, 1)
+        let size = num_rows as usize;
+        let mut ans = vec![vec![]; size];
+        for i in 0..size {
+            ans[i].resize(i + 1, 1);
             for j in 1..i {
                 ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
             }
