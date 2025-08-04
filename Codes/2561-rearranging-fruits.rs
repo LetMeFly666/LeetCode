07308d0da32fb5e9462b7b8996ffa17d7dd60455
LetMeFly666 <814114971@qq.com>
2025-08-04 23:04:54 +0800

    2561: CE.rust (#1059)

--- Codes/2561-rearranging-fruits.rs
+++ Codes/2561-rearranging-fruits.rs
@@ -1,5 +1,39 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-03 22:53:42
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-04 23:02:22
+ */
+use std::collections::HashMap;
+
 impl Solution {
     pub fn min_cost(basket1: Vec<i32>, basket2: Vec<i32>) -> i64 {
+        let mut m: i32 = i32::MAX;
+        let mut timeDiffs = HashMap::new();
+        for &t in &basket1 {
+            *timeDiffs.entry(b).or_insert(0) += 1;
+            m = m.min(t);
+        }
+        for &t in &basket2 {
+            *timeDiffs.entry(b).or_insert(0) -= 1;
+            m = m.min(t);
+        }
         
+        let mut change: Vec = vec![];
+        for (&val, &times) in freq.iter() {
+            if v % 2 != 0 {
+                return -1;
+            }
+            for _ in 0..((times as i32).abs() / 2) {
+                change.push(val);
+            }
+        }
+
+        change.sort_unstable();
+        let ans: i64 = 0;
+        for i in 0..(change.len() / 2) {
+            ans += i64::from(change[i].min(2 * m));
+        }
+        ans
     }
 }
