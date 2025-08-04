116c8f91518cb690692d1be631f2b463a4b8e15b
LetMeFly666 <814114971@qq.com>
2025-08-04 13:37:20 +0800

    word(2561): WA.go (#1059)
    不要被trae的自动补全所干扰

--- Codes/2561-rearranging-fruits.go
+++ Codes/2561-rearranging-fruits.go
@@ -1,3 +1,39 @@
-func minCost(basket1 []int, basket2 []int) int64 {
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-03 22:53:42
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-04 13:35:42
+ */
+package main
+
+import "sort"
+
+func minCost(basket1 []int, basket2 []int) (ans int64) {
+    timeDiffs := map[int]int{}
+    m := 1000000000
+    for _, t := range basket1 {
+        timeDiffs[t]++
+        m = min(m, t)
+    }
+    for _, t := range basket2 {
+        timeDiffs[t]--
+        m = min(m, t)
+    }
     
+    change := []int{}
+    for val, times := range timeDiffs {
+        if times == 0 {
+            continue
+        }
+        if times % 2 != 0 {
+            return -1
+        }
+        change = append(change, val)
+    }
+
+    sort.Ints(change)
+    for i := 0; i < len(change) / 2; i++ {
+        ans += int64(min(2 * m, change[i]))
+    }
+    return
 }
