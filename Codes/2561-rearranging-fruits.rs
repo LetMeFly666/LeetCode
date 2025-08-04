776d2a71ec2327b8a0a878f025ef1cd35a926ca4
LetMeFly666 <814114971@qq.com>
2025-08-04 23:06:26 +0800

    2561: CE.rust (#1059)
    困了说明，也太不走心了

--- Codes/2561-rearranging-fruits.rs
+++ Codes/2561-rearranging-fruits.rs
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-08-03 22:53:42
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-04 23:02:22
+ * @LastEditTime: 2025-08-04 23:05:04
  */
 use std::collections::HashMap;
 
@@ -11,17 +11,17 @@ impl Solution {
         let mut m: i32 = i32::MAX;
         let mut timeDiffs = HashMap::new();
         for &t in &basket1 {
-            *timeDiffs.entry(b).or_insert(0) += 1;
+            *timeDiffs.entry(t).or_insert(0) += 1;
             m = m.min(t);
         }
         for &t in &basket2 {
-            *timeDiffs.entry(b).or_insert(0) -= 1;
+            *timeDiffs.entry(t).or_insert(0) -= 1;
             m = m.min(t);
         }
         
-        let mut change: Vec = vec![];
-        for (&val, &times) in freq.iter() {
-            if v % 2 != 0 {
+        let mut change: Vec<i32> = vec![];
+        for (&val, &times) in timeDiffs.iter() {
+            if times % 2 != 0 {
                 return -1;
             }
             for _ in 0..((times as i32).abs() / 2) {
