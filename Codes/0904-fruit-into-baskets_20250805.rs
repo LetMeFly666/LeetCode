8b95da7ac802751e706eb6712c0aaf769ee76eb7
LetMeFly666 <814114971@qq.com>
2025-08-07 10:39:54 +0800

    904: CE.rust (#1062)

--- Codes/0904-fruit-into-baskets_20250805.rs
+++ Codes/0904-fruit-into-baskets_20250805.rs
@@ -1,5 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-05 10:28:59
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-07 10:38:25
+ */
+use std::collections::HashMap;
+
 impl Solution {
     pub fn total_fruit(fruits: Vec<i32>) -> i32 {
-        
+        let mut ans = 0;
+        let mut l = 0;
+        let mut window = HashMap::new();
+        for (r, &x) in fruits.iter().enumerate() {
+            *window.entry(x).or_insert(0) += 1;
+            while window.len() > 2 {
+                *window.entry(fruits[l]) -= 1;
+                if (window[fruits[l]] == 0) {
+                    window.remove(fruits[l]);
+                }
+                l += 1;
+            }
+            ans = ans.max(r - l + 1);
+        }
+        ans
     }
 }
