7d059a0cc2a3f3018a2c8dabdf497646a9ff80c9
LetMeFly666 <814114971@qq.com>
2025-08-10 20:47:12 +0800

    869: AC.py+WA.go (#1070)
    py - AC,100.00%,92.31%

--- Codes/0869-reordered-power-of-2_20250810.go
+++ Codes/0869-reordered-power-of-2_20250810.go
@@ -1,3 +1,34 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-10 17:20:07
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-10 20:44:18
+ */
+package main
+
+import (
+	"slices"
+	"strconv"
+)
+
+var can0869 = map[string]bool{}
+
+func init0869() {
+    if len(can0869) > 0 {
+        return
+    }
+    for i := 0; i < 31; i++ {
+        can0869[itoa869(i)] = true
+    }
+}
+
+func itoa869(i int) string {
+    s := []byte(strconv.Itoa(i))
+    slices.Sort(s)
+    return string(s)
+}
+
 func reorderedPowerOf2(n int) bool {
-    
+    init0869()
+    return can0869[itoa869(n)]
 }
--- Codes/0869-reordered-power-of-2_20250810.py
+++ Codes/0869-reordered-power-of-2_20250810.py
@@ -2,10 +2,10 @@
 Author: LetMeFly
 Date: 2025-08-10 17:20:07
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-08-10 20:11:21
+LastEditTime: 2025-08-10 20:12:28
 '''
-can = set(sorted(str(1 << i)) for i in range(31))
+can = set(''.join(sorted(str(1 << i))) for i in range(31))
 
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        return sorted(str(n)) in can
+        return ''.join(sorted(str(n))) in can
