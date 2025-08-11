76a903a0b12c01f57c59c4b9ee927ccbfd3000b8
LetMeFly <56995506+LetMeFly666@users.noreply.github.com>
2025-08-12 00:05:07 +0800

    update: 添加问题“2438.二的幂数组中查询范围内的乘积”的代码和题解 (#1073)
    * 2438: WA.cpp (#1072)
    
    * 2438: WA.cpp (#1072) - 溢出
    
    * 2438: RE.cpp (#1072)
    
    刚刚debug一直不显示pows到结果，最后一看vscode的cpp debug插件直接占了100多G(虚拟)内存，杀掉也很卡，强制重启了最后
    
    * 2438: AC.cpp (#1072)
    
    cpp - AC,60.94%,95.31%
    
    上次commit报错信息：
    Line 1122: Char 9: runtime error: reference binding to misaligned address 0xbebebebebebebec2 for type 'int', which requires 4 byte alignment (stl_vector.h)
    0xbebebebebebebec2: note: pointer points here
    <memory cannot be printed>
    SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior /usr/lib/gcc/x86_64-linux-gnu/14/../../../../include/c++/14/bits/stl_vector.h:1131:9
    
    * 2438: AC.py (#1072)
    
    python - AC,80.79%,7.28%
    
    * update: 添加问题“2438.二的幂数组中查询范围内的乘积”的代码和题解 (#1073)
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>
    
    * clean: non implementated files (#1072)
    
    * clean: non implementated files (#1072)
    
    ---------
    
    Signed-off-by: LetMeFly666 <814114971@qq.com>

---
+++ Codes/2438-range-product-queries-of-powers.cpp
@@ -0,0 +1,55 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-11 18:41:58
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-11 21:43:41
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+typedef long long ll;
+const ll MOD = 1e9 + 7;
+
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> pows;
+        int th = 0;
+        while (n) {
+            if (n & 1) {
+                pows.push_back(1 << th);
+            }
+            n >>= 1;
+            th++;
+        }
+        vector<int> ans(queries.size());
+        for (int i = 0; i < queries.size(); i++) {
+            ll thisVal = 1;
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                thisVal = thisVal * pows[j] % MOD;
+            }
+            ans[i] = thisVal;
+        }
+        return ans;
+    }
+};
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+15
+[[0,1],[2,2],[0,3]]
+
+[2,4,64]
+*/
+int main() {
+    int n;
+    string s;
+    while (cin >> n >> s) {
+        vector<vector<int>> q = stringToVectorVector(s);
+        Solution sol;
+        debug(sol.productQueries(n, q));
+    }
+    return 0;
+}
+#endif
