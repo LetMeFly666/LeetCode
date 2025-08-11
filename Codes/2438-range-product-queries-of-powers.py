6ca978953434e0e851c527af6ef283d5c3272ed0
LetMeFly666 <814114971@qq.com>
2025-08-11 21:47:59 +0800

    2438: AC.py (#1072)
    python - AC,80.79%,7.28%

--- Codes/2438-range-product-queries-of-powers.py
+++ Codes/2438-range-product-queries-of-powers.py
@@ -1,3 +1,21 @@
+'''
+Author: LetMeFly
+Date: 2025-08-11 21:37:10
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-08-11 21:46:52
+'''
+from typing import List
+
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        
+        pows = []
+        th = 0
+        while n:
+            if n & 1:
+                pows.append(1 << th)
+            th += 1
+            n >>= 1
+        perfix = [1] * (len(pows) + 1)
+        for i in range(1, len(pows) + 1):
+            perfix[i] = perfix[i - 1] * pows[i - 1]
+        return [perfix[q[1] + 1] // perfix[q[0]] % 1000000007 for q in queries]
