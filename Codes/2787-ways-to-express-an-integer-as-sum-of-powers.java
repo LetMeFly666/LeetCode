a9a070a9f7b05695946b384f0c6a8c3911469817
LetMeFly666 <814114971@qq.com>
2025-08-12 22:01:21 +0800

    2787: AC.java+rust (#1074) - rust编译过就AC了，safe(bushi)
    java - AC,98.08%,42.58%
    rust - AC,57.14%,71.43%
    
    刚刚：
    1. ArrayList<>(n + 1)只是预分配空间，还需要n+1次.add(0)
    2. dp.get(xxa) = xxb不可，需要dp.set(xxa, xxb)
    3. long完要int回去
    4. pow完也要强制转int

--- Codes/2787-ways-to-express-an-integer-as-sum-of-powers.java
+++ Codes/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -2,24 +2,24 @@
  * @Author: LetMeFly
  * @Date: 2025-08-12 09:48:56
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-12 21:46:26
+ * @LastEditTime: 2025-08-12 21:50:02
  */
 import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
     public int numberOfWays(int n, int x) {
-        List<Integer> dp = new ArrayList<>(n + 1);
-        dp.get(0) = 1;
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
         for (int th = 1; ; th++) {
-            int p = Math.pow(th, x);
+            int p = (int)Math.pow(th, x);
             if (p > n) {
                 break;
             }
             for (int i = n; i >= p; i--) {
-                dp.get(i) = ((long)dp.get(i) + dp.get(i - p)) % 1000000007;
+                dp[i] += dp[i - p];
             }
         }
-        return dp.get(n);
+        return (int)(dp[n] % 1000000007);
     }
 }
