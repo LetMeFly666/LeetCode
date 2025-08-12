8b3425fc4c97ca844bcd4244520ee9ee18a84429
LetMeFly666 <814114971@qq.com>
2025-08-12 22:13:57 +0800

    fix: Mac上debug cpp后会newSolution.py读文件夹的“文本内容”失败 (#1074)
    close #1075

--- newSolution.py
+++ newSolution.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-07-03 11:21:14
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-08-12 22:02:59
+LastEditTime: 2025-08-12 22:11:48
 Command: python newSolution.py 102. 二叉树的层序遍历
 What's more: 当前仅支持数字开头的题目
 What's more: 代码结构写的很混乱 - 想单文件实现所有操作
@@ -133,7 +133,7 @@ def genSolutionPart(num):
             pass
         if first != num:
             continue
-        if not os.path.isfile(os.path.join('Codes', file)):
+        if not os.path.isfile(os.path.join('Codes', file)): # 1075
             continue
         print(file)
         with open(os.path.join('Codes', file), 'r', encoding='utf-8') as f:
