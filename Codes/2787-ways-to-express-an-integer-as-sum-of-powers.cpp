c28d1e309de815ecd73cf5f8e7ffe73e1fc0a01f
LetMeFly666 <814114971@qq.com>
2025-08-12 10:07:03 +0800

    2787: WA.cpp (#1074) - 要的是互不相同整数
    这种方法每个正数可以被复用

--- Codes/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ Codes/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,6 +1,49 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-12 09:48:56
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-12 10:04:08
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+// dp[i]: 和为i的方案数
 class Solution {
 public:
     int numberOfWays(int n, int x) {
-        
+        vector<int> pows;
+        int now = x;
+        while (now <= n) {
+            pows.push_back(now);
+            now *= x;
+        }
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int p : pows) {
+                if (p > i) {
+                    break;
+                }
+                dp[i] += dp[i - p];
+            }
+        }
+        return dp.back();
     }
 };
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+10 2
+
+1
+*/
+int main() {
+    int a, b;
+    while (cin >> a >> b) {
+        Solution sol;
+        cout << sol.numberOfWays(a, b) << endl;
+    }
+    return 0;
+}
+#endif
