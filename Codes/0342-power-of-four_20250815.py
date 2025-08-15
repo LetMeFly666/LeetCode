78f0d61cb502e81d19f30597526fa7e84d681fa3
LetMeFly666 <814114971@qq.com>
2025-08-15 18:53:01 +0800

    342: py.cpp (#1082) - mod 3
    py - AC,12.55%,65.26%

--- Codes/0342-power-of-four_20250815.py
+++ Codes/0342-power-of-four_20250815.py
@@ -1,3 +1,9 @@
+'''
+Author: LetMeFly
+Date: 2025-08-15 18:29:00
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-08-15 18:52:04
+'''
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        
+        return n > 0 and n & (n - 1) == 0 and n % 3 == 1
