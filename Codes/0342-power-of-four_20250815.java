ffa0b1466e537f4904cea931c60bedd3f704793a
LetMeFly666 <814114971@qq.com>
2025-08-15 23:56:24 +0800

    342: AC.go+WA.java (#1082) - mod 3
    go - AC,100.00%,42.17%

--- Codes/0342-power-of-four_20250815.java
+++ Codes/0342-power-of-four_20250815.java
@@ -1,5 +1,11 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-15 18:29:00
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-15 23:56:02
+ */
 class Solution {
     public boolean isPowerOfFour(int n) {
-        
+        return n > 0 && (n & (n - 1) == 0) && n % 3 == 1;
     }
 }
