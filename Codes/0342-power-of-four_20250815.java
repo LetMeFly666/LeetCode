661c54d58f2d0bc4b5984f839c9950955f223b82
LetMeFly666 <814114971@qq.com>
2025-08-15 23:56:57 +0800

    342: AC.java (#1082) - mod 3
    java - AC,100.00%,40.06%

--- Codes/0342-power-of-four_20250815.java
+++ Codes/0342-power-of-four_20250815.java
@@ -6,6 +6,6 @@
  */
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return n > 0 && (n & (n - 1) == 0) && n % 3 == 1;
+        return n > 0 && (n & (n - 1)) == 0 && n % 3 == 1;
     }
 }
