5ef5355cd51dff809bb1095a02f301d7201b08b4
LetMeFly666 <814114971@qq.com>
2025-08-22 21:32:55 +0800

    3195: AC.go+CE.java (#1092)
    go - AC,68.42%,42.10%

--- Codes/3195-find-the-minimum-area-to-cover-all-ones-i.java
+++ Codes/3195-find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,5 +1,22 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-22 21:18:33
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-22 21:31:34
+ */
 class Solution {
     public int minimumArea(int[][] grid) {
-        
+        int l = 1000, r = 0, u = 1000, d = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < gird[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    l = Math.min(l, j);
+                    r = Math.max(r, j);
+                    u = Math.min(u, i);
+                    d = Math.max(d, i);
+                }
+            }
+        }
+        return (r - l + 1) * (d - u + 1);
     }
 }
