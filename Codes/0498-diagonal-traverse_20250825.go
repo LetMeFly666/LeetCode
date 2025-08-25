853d6d99ebff22587d31893769c7451a8f2fbdc5
LetMeFly666 <814114971@qq.com>
2025-08-26 00:00:35 +0800

    498: AC.go (#1094)
    go - AC,100.00%,66.42%

--- Codes/0498-diagonal-traverse_20250825.go
+++ Codes/0498-diagonal-traverse_20250825.go
@@ -2,13 +2,13 @@
  * @Author: LetMeFly
  * @Date: 2025-08-25 18:56:28
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-25 23:59:01
+ * @LastEditTime: 2025-08-26 00:00:06
  */
 package main
 
 func findDiagonalOrder(mat [][]int) []int {
     n, m := len(mat), len(mat[0])
-    ans := make([]int, m * n)
+    ans := make([]int, 0, m * n)
     for k := 0; k < m + n - 1; k++ {
         if k % 2 == 0 {
             i := min(k, n - 1)
