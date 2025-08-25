f557d3d9e02766c411649ab84c2d4702836ac1f9
LetMeFly666 <814114971@qq.com>
2025-08-25 23:59:55 +0800

    498: AC.py + WA.go (#1094) - go的make([]int, l)是直接len=l了
    py - AC,62.90%,80.10% - 一遍过

--- Codes/0498-diagonal-traverse_20250825.py
+++ Codes/0498-diagonal-traverse_20250825.py
@@ -1,3 +1,28 @@
+'''
+Author: LetMeFly
+Date: 2025-08-25 18:56:28
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-08-25 23:54:37
+'''
+from typing import List
+
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        
+        ans: List[int] = []
+        n, m = len(mat), len(mat[0])
+        for k in range(m + n - 1):
+            if k % 2:
+                j = min(k, m - 1)
+                i = k - j
+                while i < n and j >= 0:
+                    ans.append(mat[i][j])
+                    i += 1
+                    j -= 1
+            else:
+                i = min(k, n - 1)
+                j = k - i
+                while i >= 0 and j < m:
+                    ans.append(mat[i][j])
+                    i -= 1
+                    j += 1
+        return ans
