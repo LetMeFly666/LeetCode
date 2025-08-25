d1667f5f6a9558c273b2d39f2d1a83859bf8c3f6
LetMeFly666 <814114971@qq.com>
2025-08-25 21:59:51 +0800

    498: RE.cpp (#1094)

--- Codes/0498-diagonal-traverse_20250825.cpp
+++ Codes/0498-diagonal-traverse_20250825.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-08-25 18:56:28
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-25 21:49:38
+ * @LastEditTime: 2025-08-25 21:59:34
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -12,33 +12,15 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         vector<int> ans;
-        int i = 0, j = 0;
-        bool d = true;
-        int total = mat.size() * mat[0].size();
-        while (ans.size() < total) {
-            printf("i = %d, j = %d, d = %d\n", i, j, d);  // *****
-            ans.push_back(mat[i][j]);
-            if (d) {
-                i--, j++;
-                if (i == -1 || j == mat[0].size()) {
-                    d = !d;
-                }
-                if (i == -1) {
-                    i = 0;
-                }
-                if (j == mat[0].size()) {
-                    j = mat[0].size() - 1;
+        int n = mat.size(), m = mat[0].size();
+        for (int k = 0; k < m + n - 1; k++) {
+            if (k % 2) {
+                for (int i = 0, j = k; j >= 0; i++, j--) {
+                    ans.push_back(mat[i][j]);
                 }
             } else {
-                i++, j--;
-                if (j == -1 || i == mat.size()) {
-                    d = !d;
-                }
-                if (j == -1) {
-                    j = 0;
-                }
-                if (i == mat.size()) {
-                    i = mat.size() - 1;
+                for (int i = k, j = 0; i >= 0; i--, j++) {
+                    ans.push_back(mat[i][j]);
                 }
             }
         }
