4bcbade3d5b40a3b2d5061712b283ea31c3c9245
LetMeFly666 <814114971@qq.com>
2025-08-26 10:35:35 +0800

    498: AC.java+RE.rust (#1094)
    java - AC,67.55%,54.53% - cnt一次枚举对角线不只加一

--- Codes/0498-diagonal-traverse_20250825.java
+++ Codes/0498-diagonal-traverse_20250825.java
@@ -2,20 +2,20 @@
  * @Author: LetMeFly
  * @Date: 2025-08-25 18:56:28
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-26 10:20:44
+ * @LastEditTime: 2025-08-26 10:24:47
  */
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int n = mat.length, m = mat[0].length;
         int[] ans = new int[m * n];
-        for (int k = 0, cnt = 0; k < m + n - 1; k++, cnt++) {
+        for (int k = 0, cnt = 0; k < m + n - 1; k++) {
             if (k % 2 == 0) {
                 for (int i = Math.min(k, n - 1), j = k - i; i >= 0 && j < m; i--, j++) {
-                    ans[cnt] = mat[i][j];
+                    ans[cnt++] = mat[i][j];
                 }
             } else {
                 for (int j = Math.min(k, m - 1), i = k - j; i < n && j >= 0; i++, j--) {
-                    ans[cnt] = mat[i][j];
+                    ans[cnt++] = mat[i][j];
                 }
             }
         }
--- Codes/0498-diagonal-traverse_20250825.rs
+++ Codes/0498-diagonal-traverse_20250825.rs
@@ -1,5 +1,36 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-25 18:56:28
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-26 10:33:40
+ */
 impl Solution {
     pub fn find_diagonal_order(mat: Vec<Vec<i32>>) -> Vec<i32> {
-        
+        let n: usize = mat.len();
+        let m: usize = mat[0].len();
+        let mut ans: Vec<i32> = vec![0; m * n];
+        let mut cnt: usize = 0;
+        for k in 0..(m + n - 1) {
+            if k % 2 == 0 {
+                let mut i: usize = k.min(n - 1);
+                let mut j: usize = k - i;
+                while i >= 0 && j < m {
+                    ans[cnt] = mat[i][j];
+                    cnt += 1;
+                    i -= 1;
+                    j += 1;
+                }
+            } else {
+                let mut j: usize = k.min(m - 1);
+                let mut i: usize = k - j;
+                while i < n && j >= 0 {
+                    ans[cnt] = mat[i][j];
+                    cnt += 1;
+                    i += 1;
+                    j -= 1;
+                }
+            }
+        }
+        ans
     }
 }
