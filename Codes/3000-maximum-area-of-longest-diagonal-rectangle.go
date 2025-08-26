6a061528da3264ba47a116d0ba154b64ed7b8fe5
LetMeFly666 <814114971@qq.com>
2025-08-26 21:41:47 +0800

    3000: AC.py+go+java+rust (#1098)
    py - AC,100.00%,82.26%
    go - AC,100.00%,11.11%
    java - AC,100.00%,33.33%
    rust - AC,100.00%,80.00%
    
    中间和室友聊了一会儿哈哈

--- Codes/3000-maximum-area-of-longest-diagonal-rectangle.go
+++ Codes/3000-maximum-area-of-longest-diagonal-rectangle.go
@@ -1,3 +1,21 @@
-func areaOfMaxDiagonal(dimensions [][]int) int {
-    
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-26 21:25:10
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-26 21:36:14
+ */
+package main
+
+func areaOfMaxDiagonal(dimensions [][]int) (ans int) {
+    M := 0
+    for _, d := range dimensions {
+        l2 := d[0] * d[0] + d[1] * d[1]
+        if l2 > M {
+            M = l2
+            ans = d[0] * d[1]
+        } else if l2 == M {
+            ans = max(ans, d[0] * d[1])
+        }
+    }
+    return
 }
--- Codes/3000-maximum-area-of-longest-diagonal-rectangle.py
+++ Codes/3000-maximum-area-of-longest-diagonal-rectangle.py
@@ -1,3 +1,18 @@
+'''
+Author: LetMeFly
+Date: 2025-08-26 21:25:10
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-08-26 21:34:38
+'''
+from typing import List
 class Solution:
     def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
-        
+        ans = M = 0
+        for a, b in dimensions:
+            l2 = a * a + b * b
+            if l2 > M:
+                M = l2
+                ans = a * b
+            elif l2 == M:
+                ans = max(ans, a * b)
+        return ans
