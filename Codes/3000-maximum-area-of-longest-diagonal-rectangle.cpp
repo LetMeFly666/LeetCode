a13a49de070b1a22afcbc106c33e28eb6dda9df2
LetMeFly666 <814114971@qq.com>
2025-08-26 21:32:54 +0800

    3000: AC.cpp (#1098) - 刚刚晕了晕了
    cpp - AC,100.00%,88.24%

--- Codes/3000-maximum-area-of-longest-diagonal-rectangle.cpp
+++ Codes/3000-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-08-26 21:25:10
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-08-26 21:27:10
+ * @LastEditTime: 2025-08-26 21:32:11
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -17,9 +17,26 @@ public:
             int l2 = d[0] * d[0] + d[1] * d[1];
             if (l2 > M) {
                 M = l2;
+                ans = d[0] * d[1];
+            } else if (l2 == M) {
                 ans = max(ans, d[0] * d[1]);
             }
         }
         return ans;
     }
 };
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+[[6,5],[8,6],[2,10],[8,1],[9,2],[3,5],[3,5]]
+*/
+int main() {
+    string s;
+    while (cin >> s) {
+        vector<vector<int>> v = stringToVectorVector(s);
+        Solution sol;
+        cout << sol.areaOfMaxDiagonal(v) << endl;
+    }
+    return 0;
+}
+#endif
