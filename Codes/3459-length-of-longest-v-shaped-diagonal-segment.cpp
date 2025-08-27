cd9518698036fe292ccac4aa49568ec73444c500
LetMeFly666 <814114971@qq.com>
2025-08-27 23:31:27 +0800

    3459: CE.cpp (#1100)
    Line 13: Char 48: error: constant expression evaluates to -1 which cannot be narrowed to type 'bool' [-Wc++11-narrowing]
       13 |     const bool directions[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
          |                                                ^~
    Line 13: Char 48: note: insert an explicit cast to silence this issue
       13 |     const bool directions[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
          |                                                ^~
          |                                                static_cast<bool>( )

--- Codes/3459-length-of-longest-v-shaped-diagonal-segment.cpp
+++ Codes/3459-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,6 +1,76 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-08-27 23:08:01
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-08-27 23:30:36
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
 class Solution {
+private:
+    const bool directions[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+    vector<vector<int>> grid;
+    unordered_map<int, int> cache;
+
+    inline bool canContinue(int i, int j, int ni, int nj) {
+        int thisVal = grid[i][j], nextVal = grid[ni][nj];
+        return (thisVal == 1 && nextVal != 1) || (thisVal != 1 && nextVal != 1 && thisVal != nextVal);
+    }
+
+    /*
+    >>> 500 * 500
+    250000
+    >>> 500 * 500 * 4 * 2
+    2000000
+    >>> 500 * 4 * 2
+    4000
+    >>> 4 * 2
+    8
+    */
+    inline int getCacheKey(int i, int j, int d, int times) {
+        return i * 4000 + j * 8 + d * 2;
+    }
+
+    int dfs(int i, int j, int d, int times) {
+        int cacheKey = getCacheKey(i, j, d, times);
+        if (cache.count(cacheKey)) {
+            return cache[cacheKey];
+        }
+        int toAdd = 0;
+        int ni = i + directions[d][0], nj = j + directions[d][1];
+        if (ni >= 0 && ni < grid.size() && nj >= 0 && nj < grid[0].size()) {
+            if (canContinue(i, j, ni, nj)) {
+                toAdd = dfs(ni, nj, d, times);
+            }
+        }
+        if (times == 0) {
+            for (int nd = 0; nd < 4; nd++) {
+                if (abs(nd - d) == 2) {
+                    continue;
+                }
+                if (canContinue(i, j, ni, nj)) {
+                    toAdd = max(toAdd, dfs(ni, nj, nd, 1));
+                }
+            }
+        }
+        return cache[cacheKey] = 1 + toAdd;
+    }
 public:
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-        
+        this->grid = move(grid);
+        int ans = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] != 1) {
+                    continue;
+                }
+                for (int d = 0; d < 4; d++) {
+                    ans = max(ans, dfs(i, j, d, 0));
+                }
+            }
+        }
+        return ans;
     }
 };
