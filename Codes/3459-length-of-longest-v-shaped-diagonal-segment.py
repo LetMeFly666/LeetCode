f4a269d33aa700468e9bc622262c4317e96000fb
LetMeFly666 <814114971@qq.com>
2025-08-31 19:12:52 +0800

    3459: 极限AC.py (#1100) - 前面commitMsg里关于py的一直写成cpp了
    py - AC,10.07%,97.44%
    
    执行用时分布
    5814
    ms
    击败
    10.07%
    复杂度分析
    消耗内存分布
    54.82
    MB
    击败
    97.44%

--- Codes/3459-length-of-longest-v-shaped-diagonal-segment.py
+++ Codes/3459-length-of-longest-v-shaped-diagonal-segment.py
@@ -2,10 +2,9 @@
 Author: LetMeFly
 Date: 2025-08-28 12:54:06
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-08-31 19:08:33
+LastEditTime: 2025-08-31 19:11:00
 '''
 from typing import List
-from functools import cache
 
 class Solution:
     directions = [
@@ -25,9 +24,14 @@ class Solution:
         now = self.grid[i][j]
         next = self.grid[ni][nj]
         return now == 1 and next == 2 or now != 1 and next != 1 and now != next
+    
+    def getCacheKey(self, i: int, j: int, d: int, canChange: bool) -> int:
+        return i * self.m * 8 + j * 8 + d * 2 + canChange
 
-    @cache
     def dfs(self, i: int, j: int, d: int, canChange: bool) -> int:
+        cacheKey = self.getCacheKey(i, j, d, canChange)
+        if self.cache[cacheKey] != -1:
+            return self.cache[cacheKey]
         then = 0
         if self.canContinue(i, j, d):
             then = self.dfs(i + self.directions[d][0], j + self.directions[d][1], d, canChange)
@@ -35,11 +39,13 @@ class Solution:
             nd = (d + 1) % 4
             if self.canContinue(i, j, nd):
                 then = max(then, self.dfs(i + self.directions[nd][0], j + self.directions[nd][1], nd, False))
+        self.cache[cacheKey] = then + 1
         return then + 1
 
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
         self.grid = grid
         self.n, self.m = len(grid), len(grid[0])
+        self.cache = [-1] * (self.m * self.n * 8)
         ans = 0
         for i, line in enumerate(grid):
             for j, g in enumerate(line):
