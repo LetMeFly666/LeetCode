d94da10679a9753b1aacf0a77571caca771d886a
LetMeFly666 <814114971@qq.com>
2025-09-04 13:41:24 +0800

    3516: AC.go+rust (#1110)
    go - AC,100.00%,25.00%
    rust - AC,100.00%,9.09%

--- Codes/3516-find-closest-person.go
+++ Codes/3516-find-closest-person.go
@@ -2,12 +2,19 @@
  * @Author: LetMeFly
  * @Date: 2025-09-04 13:33:33
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-09-04 13:37:51
+ * @LastEditTime: 2025-09-04 13:39:11
  */
 package main
 
+func abs3516(a int) int {
+    if a < 0 {
+        return -a
+    }
+    return a
+}
+
 func findClosest(x int, y int, z int) int {
-    diff := abs(x - z) - abs(y - z)
+    diff := abs3516(x - z) - abs3516(y - z)
     if diff == 0 {
         return 0
     } else if diff > 0 {
