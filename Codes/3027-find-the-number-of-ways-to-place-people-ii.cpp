b774f426421ed9efaf63d593a0369e8269bfc3d6
LetMeFly666 <814114971@qq.com>
2025-09-05 10:05:33 +0800

    3027: WA.cpp (#1112) - 好吧，minY可以是10^{-9}
    input: [[2,5],[128653,-2370425]]
    should: 1
    output: 0

--- Codes/3027-find-the-number-of-ways-to-place-people-ii.cpp
+++ Codes/3027-find-the-number-of-ways-to-place-people-ii.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-09-05 09:55:20
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-09-05 10:02:08
+ * @LastEditTime: 2025-09-05 10:04:27
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -18,7 +18,7 @@ public:
         for (int i = 0; i < points.size(); i++) {
             int mxY = -1;
             for (int j = i + 1; j < points.size(); j++) {
-                if (points[j][1] > mxY) {
+                if (points[j][1] > mxY && points[j][1] <= points[i][1]) {
                     ans++;
                     mxY = points[j][1];
                 }
