9d383286b46d2ce62ea75fb5864ad298869004b4
LetMeFly666 <814114971@qq.com>
2025-09-05 10:51:36 +0800

    3027: AC.rust (#1112)
    rust - AC,36.20%,7.97%

--- Codes/3027-find-the-number-of-ways-to-place-people-ii.rs
+++ Codes/3027-find-the-number-of-ways-to-place-people-ii.rs
@@ -8,7 +8,7 @@ impl Solution {
     pub fn number_of_pairs(mut points: Vec<Vec<i32>>) -> i32 {  // 想原地sort points的话记得在points前面加上mut
         points.sort_by(|a, b| {
             if a[0] == b[0] {
-                b[1].cmp(&a[0])
+                b[1].cmp(&a[1])
             } else {
                 a[0].cmp(&b[0])
             }
