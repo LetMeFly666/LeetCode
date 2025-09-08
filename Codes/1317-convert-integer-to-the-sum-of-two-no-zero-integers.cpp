5260cc44eac0920e6b0cf2288f590701232b5270
LetMeFly666 <814114971@qq.com>
2025-09-08 23:12:08 +0800

    1317: WA.cpp (#1118) - OMG - 笔误了

--- Codes/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ Codes/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,10 +1,31 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-09-08 23:05:58
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-09-08 23:11:26
+ */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
 class Solution {
+private:
+    inline bool ok(int n) {
+        while (n) {
+            if (n % 10 == 0) {
+                return false;
+            }
+            n /= 10;
+        }
+        return true;
+    }
 public:
     vector<int> getNoZeroIntegers(int n) {
-        
+        for (int i = 1; i < n; i++) {
+            if (ok(i) && ok(n - i)) {
+                return {n, n - i};
+            }
+        }
+        return {};  // FAKE RETURN
     }
 };
