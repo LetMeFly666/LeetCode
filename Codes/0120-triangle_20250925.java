3a0d5f13c667c7e332719fce0753cfeba5a25c22
LetMeFly666 <814114971@qq.com>
2025-09-25 23:34:19 +0800

    210: AC.java+CE.rust (#1143)
    java - AC,13.87%,45.96%

--- Codes/0120-triangle_20250925.java
+++ Codes/0120-triangle_20250925.java
@@ -2,11 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2025-09-25 23:12:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-09-25 23:30:09
+ * @LastEditTime: 2025-09-25 23:31:13
  */
-class Solution {  // 说错了，是CE
+class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        for (int i = triangle.length() - 2; i >= 0; i--) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
                 triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
             }
--- Codes/0120-triangle_20250925.rs
+++ Codes/0120-triangle_20250925.rs
@@ -1,5 +1,16 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-09-25 23:12:43
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-09-25 23:33:34
+ */
 impl Solution {
-    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
-        
+    pub fn minimum_total(triangle: mut Vec<Vec<i32>>) -> i32 {
+        for i := (triangle.len()-2)..-1 {
+            for j := 0..(i+1) {
+                triangle[i][j] += triangle[i+1][j].min(triangle[i+1][j+1]);
+            }
+        }
+        return tirangle[0][0];
     }
 }
