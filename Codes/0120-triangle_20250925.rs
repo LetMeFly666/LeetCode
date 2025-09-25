6a7e4f8f64247c817770c0c45361f8d70877694f
LetMeFly666 <814114971@qq.com>
2025-09-25 23:35:28 +0800

    210: CE.rust (#1143) - typo

--- Codes/0120-triangle_20250925.rs
+++ Codes/0120-triangle_20250925.rs
@@ -2,12 +2,12 @@
  * @Author: LetMeFly
  * @Date: 2025-09-25 23:12:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-09-25 23:33:34
+ * @LastEditTime: 2025-09-25 23:34:54
  */
 impl Solution {
-    pub fn minimum_total(triangle: mut Vec<Vec<i32>>) -> i32 {
-        for i := (triangle.len()-2)..-1 {
-            for j := 0..(i+1) {
+    pub fn minimum_total(mut triangle: Vec<Vec<i32>>) -> i32 {
+        for i in (0..triangle.len()-1).rev() {
+            for j in 0..=i {
                 triangle[i][j] += triangle[i+1][j].min(triangle[i+1][j+1]);
             }
         }
