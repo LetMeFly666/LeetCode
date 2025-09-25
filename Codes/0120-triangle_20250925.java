12de0fcc58360b218d16a2bc02dfe4ef4cf2db41
LetMeFly666 <814114971@qq.com>
2025-09-25 23:29:44 +0800

    210: AC.go+CE.java (#1143)
    go - AC,100.00%,68.48%
    
    Line 9: error: cannot find symbol
            for (int i = triangle.length() - 2; i >= 0; i--) {
                                 ^
      symbol:   method length()
      location: variable triangle of type List<List<Integer>>
    Line 11: error: cannot find symbol
                    triangle.at(i).set(j, triangle.at(i).at(j) + Math.min(triangle.at(i + 1).at(j), triangle.at(i + 1).at(j + 1)));
                                                  ^
      symbol:   method at(int)
      location: variable triangle of type List<List<Integer>>
    Line 11: error: cannot find symbol
                    triangle.at(i).set(j, triangle.at(i).at(j) + Math.min(triangle.at(i + 1).at(j), triangle.at(i + 1).at(j + 1)));
                                                                                  ^
      symbol:   method at(int)
      location: variable triangle of type List<List<Integer>>
    Line 11: error: cannot find symbol
                    triangle.at(i).set(j, triangle.at(i).at(j) + Math.min(triangle.at(i + 1).at(j), triangle.at(i + 1).at(j + 1)));
                                                                                                            ^
      symbol:   method at(int)
      location: variable triangle of type List<List<Integer>>
    Line 11: error: cannot find symbol
                    triangle.at(i).set(j, triangle.at(i).at(j) + Math.min(triangle.at(i + 1).at(j), triangle.at(i + 1).at(j + 1)));
                            ^
      symbol:   method at(int)
      location: variable triangle of type List<List<Integer>>
    Line 14: error: cannot find symbol
            return triangle.at(i).at(j);
                                     ^
      symbol:   variable j
      location: class Solution
    Line 14: error: cannot find symbol
            return triangle.at(i).at(j);
                               ^
      symbol:   variable i
      location: class Solution
    7 errors

--- Codes/0120-triangle_20250925.java
+++ Codes/0120-triangle_20250925.java
@@ -1,5 +1,16 @@
-class Solution {
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-09-25 23:12:43
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-09-25 23:29:00
+ */
+class Solution {  // 这个交上去肯定RE
     public int minimumTotal(List<List<Integer>> triangle) {
-        
+        for (int i = triangle.length() - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle.at(i).set(j, triangle.at(i).at(j) + Math.min(triangle.at(i + 1).at(j), triangle.at(i + 1).at(j + 1)));
+            }
+        }
+        return triangle.at(i).at(j);
     }
 }
