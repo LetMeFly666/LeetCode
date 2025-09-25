716386df7b7f8fc577caea67d9a1c07cd2c6404d
LetMeFly666 <814114971@qq.com>
2025-09-25 23:20:07 +0800

    210: AC.cpp (#1143)
    AC,从上到下,100.00%,88.73%

--- Codes/0120-triangle_20250925.cpp
+++ Codes/0120-triangle_20250925.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-09-25 23:12:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-09-25 23:16:48
+ * @LastEditTime: 2025-09-25 23:18:09
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -13,9 +13,12 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         for (int i = 1; i < triangle.size(); i++) {
             for (int j = 0; j <= i; j++) {
-                int toAdd = triangle[i - 1][j];
+                int toAdd = 100000;
                 if (j - 1 >= 0) {
                     toAdd = min(toAdd, triangle[i - 1][j - 1]);
+                }
+                if (j < i) {
+                    toAdd = min(toAdd, triangle[i - 1][j]);
                 }
                 triangle[i][j] += toAdd;
             }
