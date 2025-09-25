906d0de33dbbf2bf1493d04e8374cb6cbbf394f1
LetMeFly666 <814114971@qq.com>
2025-09-25 23:25:28 +0800

    210: WA.py (#1143) - j-1和j+1

--- Codes/0120-triangle_20250925.py
+++ Codes/0120-triangle_20250925.py
@@ -1,5 +1,15 @@
+'''
+Author: LetMeFly
+Date: 2025-09-25 23:12:43
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-09-25 23:24:36
+'''
 from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        
+        # for line in triangle[-2:-1:-1]:
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(i + 1):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j - 1])
+        return triangle[0][0]
