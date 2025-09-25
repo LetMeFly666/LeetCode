654f39c7053629f227d1b35b930f691d2aef52dc
LetMeFly666 <814114971@qq.com>
2025-09-25 23:26:03 +0800

    210: AC.py (#1143)
    AC,44.14%,84.46%

--- Codes/0120-triangle_20250925.py
+++ Codes/0120-triangle_20250925.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-09-25 23:12:43
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-09-25 23:24:36
+LastEditTime: 2025-09-25 23:25:37
 '''
 from typing import List
 
@@ -11,5 +11,5 @@ class Solution:
         # for line in triangle[-2:-1:-1]:
         for i in range(len(triangle) - 2, -1, -1):
             for j in range(i + 1):
-                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j - 1])
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
         return triangle[0][0]
