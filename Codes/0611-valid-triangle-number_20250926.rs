79418ef9a7a82d5c9be92f484243652d1fed4092
LetMeFly666 <814114971@qq.com>
2025-09-26 22:59:13 +0800

    0611: WA.rust (#1145)

--- Codes/0611-valid-triangle-number_20250926.rs
+++ Codes/0611-valid-triangle-number_20250926.rs
@@ -2,13 +2,13 @@
  * @Author: LetMeFly
  * @Date: 2025-09-26 22:40:03
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-09-26 22:56:15
+ * @LastEditTime: 2025-09-26 22:58:20
  */
 impl Solution {
     pub fn triangle_number(mut nums: Vec<i32>) -> i32 {
         nums.sort();
         let mut ans: usize = 0;
-        for i in nums.len()-1..=0 {
+        for i in (0..nums.len()).rev() {
             let mut l: usize = 0;
             let mut r: usize = i - 1;
             while l < r {
