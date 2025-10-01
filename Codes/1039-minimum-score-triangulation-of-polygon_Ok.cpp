e2c91b96f4830fb5328ac72b605c448e392020d6
LetMeFly666 <814114971@qq.com>
2025-10-01 20:33:55 +0800

    1039: dbg.cpp (#1151)

--- Codes/1039-minimum-score-triangulation-of-polygon_Ok.cpp
+++ Codes/1039-minimum-score-triangulation-of-polygon_Ok.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-09-29 18:44:48
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-01 20:16:32
+ * @LastEditTime: 2025-10-01 20:32:59
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -23,12 +23,23 @@ private:
             return cache[key];
         }
         if (j - i == 2) {
+            printf("dfs(%d, %d) = %d\n", i, j, values[i] * values[i + 1] * values[i + 2]);  //*****
             return cache[key] = values[i] * values[i + 1] * values[i + 2];
         }
         int ans = 1000000000;
+        /*
+        0 1 2 3 -> 0 1 2 + 0 2 3
+
+        0   1
+
+        3   2
+
+        
+        */
         for (int k = i + 1; k < j; k++) {
             ans = min(ans, dfs(i, k) + dfs(k, j));
         }
+        printf("dfs(%d, %d) = %d\n", i, j, ans);  //*****
         return cache[key] = ans;
     }
 public:
@@ -38,3 +49,20 @@ public:
         return dfs(0, n - 1);
     }
 };
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+[3,7,4,5]
+
+144
+*/
+int main() {
+    string s;
+    while (cin >> s) {
+        vector<int> v = stringToVector(s);
+        Solution sol;
+        cout << sol.minScoreTriangulation(v) << endl;
+    }
+    return 0;
+}
+#endif
