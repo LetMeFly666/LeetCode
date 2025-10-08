9dd928b8510d863e161efc8136d2514f79bd5d68
LetMeFly666 <814114971@qq.com>
2025-10-08 22:18:33 +0800

    2300: AC.cpp (#1161) - AC,80.67%,75.97%

--- Codes/2300-successful-pairs-of-spells-and-potions_20251008.cpp
+++ Codes/2300-successful-pairs-of-spells-and-potions_20251008.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-10-08 21:37:50
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-08 21:56:07
+ * @LastEditTime: 2025-10-08 22:17:48
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -16,7 +16,8 @@ s b s/b a
 6 3  2  2
 6 4 1.5 2
 6 5 1.x 2
-a >= ⌈s//b⌉
+a >= ⌈s/b⌉
+a >= ⌊(s+b-1)/b⌋
 */
 typedef long long ll;
 class Solution {
@@ -24,7 +25,7 @@ public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(), potions.end());
         for (int& s : spells) {
-            ll target = success / s;
+            ll target = (success + s - 1) / s;
             if (target > 100000) {
                 s = 0;
             } else {
