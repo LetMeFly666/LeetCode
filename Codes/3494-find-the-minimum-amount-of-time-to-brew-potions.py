debaf2d08d3c7bb2c99b37fcf8cdfa326c4582f3
LetMeFly666 <814114971@qq.com>
2025-10-09 23:13:16 +0800

    3494: AC.py (#1164) - AC,14209ms击败5.77%,76.92%
    把len(skill)提前用变量n存起来就过了。。。
    看评论官解也TLE：
    https://leetcode.cn/problems/find-the-minimum-amount-of-time-to-brew-potions/solutions/3800529/niang-zao-yao-shui-xu-yao-de-zui-shao-zo-ojii/comments/3139053/

--- Codes/3494-find-the-minimum-amount-of-time-to-brew-potions.py
+++ Codes/3494-find-the-minimum-amount-of-time-to-brew-potions.py
@@ -2,18 +2,19 @@
 Author: LetMeFly
 Date: 2025-10-09 22:52:05
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-10-09 23:07:02
+LastEditTime: 2025-10-09 23:11:26
 '''
 from typing import List
 
 class Solution:
     def minTime(self, skill: List[int], mana: List[int]) -> int:
-        times = [0] * len(skill)
+        n = len(skill)
+        times = [0] * n
         for m in mana:
             lastTime = times[0] + skill[0] * m
-            for i in range(1, len(skill)):
+            for i in range(1, n):
                 lastTime = max(lastTime, times[i]) + skill[i] * m
             times[-1] = lastTime
-            for i in range(len(skill) - 2, -1, -1):
+            for i in range(n - 2, -1, -1):
                 times[i] = times[i + 1] - skill[i + 1] * m
         return times[-1]
