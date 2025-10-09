6ec67850adb50b51fb6e7ff15c021bb1924d3baa
LetMeFly666 <814114971@qq.com>
2025-10-09 23:19:39 +0800

    3494: AC.rust (#1164) - AC,86ms击败100.00%,2.24MB击败100.00%
    好小的内存占用好短的时间消耗

--- Codes/3494-find-the-minimum-amount-of-time-to-brew-potions.rs
+++ Codes/3494-find-the-minimum-amount-of-time-to-brew-potions.rs
@@ -1,5 +1,23 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-10-09 22:52:05
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-10-09 23:18:22
+ */
 impl Solution {
     pub fn min_time(skill: Vec<i32>, mana: Vec<i32>) -> i64 {
-        
+        let n: usize = skill.len();
+        let mut times: Vec<i64> = vec![0; n];
+        for m in mana {
+            let mut last_time: i64 = times[0] + (skill[0] * m) as i64;
+            for i in 1..n {
+                last_time = last_time.max(times[i]) + (skill[i] * m) as i64;
+            }
+            times[n-1] = last_time;
+            for i in (0..n-1).rev() {
+                times[i] = times[i + 1] - (skill[i + 1] * m) as i64;
+            }
+        }
+        times[n-1]
     }
 }
--- Codes/lib.rs
+++ Codes/lib.rs
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-08-07 14:11:36
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-01 20:40:27
+ * @LastEditTime: 2025-10-09 23:13:33
  */
 pub struct Solution;
-include!("3494-find-the-minimum-amount-of-time-to-brew-potions_20251009.rs");  // 这个fileName是会被脚本替换掉的
+include!("3494-find-the-minimum-amount-of-time-to-brew-potions.rs");  // 这个fileName是会被脚本替换掉的
