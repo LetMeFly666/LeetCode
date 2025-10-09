9ed6235ea0edde2e488ff8b63f093b16e1db4359
LetMeFly666 <814114971@qq.com>
2025-10-09 23:10:58 +0800

    3494: TLE.py (#1164) - what???

--- Codes/3494-find-the-minimum-amount-of-time-to-brew-potions.py
+++ Codes/3494-find-the-minimum-amount-of-time-to-brew-potions.py
@@ -1,5 +1,19 @@
+'''
+Author: LetMeFly
+Date: 2025-10-09 22:52:05
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-10-09 23:07:02
+'''
 from typing import List
 
 class Solution:
     def minTime(self, skill: List[int], mana: List[int]) -> int:
-        
+        times = [0] * len(skill)
+        for m in mana:
+            lastTime = times[0] + skill[0] * m
+            for i in range(1, len(skill)):
+                lastTime = max(lastTime, times[i]) + skill[i] * m
+            times[-1] = lastTime
+            for i in range(len(skill) - 2, -1, -1):
+                times[i] = times[i + 1] - skill[i + 1] * m
+        return times[-1]
