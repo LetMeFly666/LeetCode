a0f47a1d1a02210d8e75f08b4d19dca5f30b6da2
LetMeFly666 <814114971@qq.com>
2025-10-11 19:02:46 +0800

    3186: WA.py O(1)空间 (#1167)

---
+++ Codes/3186-maximum-total-damage-with-spell-casting_O1.py
@@ -0,0 +1,26 @@
+'''
+Author: LetMeFly
+Date: 2025-10-11 18:10:29
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-10-11 19:02:11
+'''
+from typing import List
+from collections import Counter
+
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        cnt = Counter(power)
+        values = sorted(cnt)
+        dp0 = dp1 = dp2 = 0
+        val1 = val2 = -10
+        for val in values:
+            valSum = val * cnt[val]
+            if val - val2 > 2:  # 无冲突
+                thisVal = valSum + dp2
+            elif val - val1 > 2:  # 和val2相差小于2，但和val1不冲突
+                thisVal = valSum + dp1
+            else:  # 和val1、val2都冲突(一定不会再和前面的冲突了)
+                thisVal = valSum + dp0
+            dp0, dp1, dp2 = dp1, dp2, thisVal
+            val1, val2 = val2, val
+        return dp2
