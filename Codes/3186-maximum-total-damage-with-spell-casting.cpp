e45040c8af509bc0a5dc661c9a786292c7fdf123
LetMeFly666 <814114971@qq.com>
2025-10-11 18:39:34 +0800

    3186: AC.cpp (#1167) - AC,88.61%,87.17%

--- Codes/3186-maximum-total-damage-with-spell-casting.cpp
+++ Codes/3186-maximum-total-damage-with-spell-casting.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-10-11 18:10:29
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-11 18:33:47
+ * @LastEditTime: 2025-10-11 18:38:56
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -18,7 +18,15 @@ public:
             cnt[t]++;
         }
         vector<pair<int, int>> values(cnt.begin(), cnt.end());
-        std::ranges::sort(values);
-        return 0;
+        sort(values.begin(), values.end());
+        vector<ll> dp(values.size() + 1);
+        for (int i = 0, j = 0; i < values.size(); i++) {
+            auto& [value, times] = values[i];
+            while (values[j].first < value - 2) {
+                j++;
+            }
+            dp[i + 1] = max(dp[i], dp[j] + (ll) value * times);
+        }
+        return dp.back();
     }
 };
