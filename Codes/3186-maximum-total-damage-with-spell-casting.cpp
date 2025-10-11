b8d405ba80820b5d689364b52f2156ceb6724aea
LetMeFly666 <814114971@qq.com>
2025-10-11 18:15:17 +0800

    3186: half.cpp (#1167) - 可以不这么写

--- Codes/3186-maximum-total-damage-with-spell-casting.cpp
+++ Codes/3186-maximum-total-damage-with-spell-casting.cpp
@@ -1,10 +1,26 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-10-11 18:10:29
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-10-11 18:14:19
+ */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
+typedef long long ll;
+
 class Solution {
 public:
-    long long maximumTotalDamage(vector<int>& power) {
+    ll maximumTotalDamage(vector<int>& power) {
+        int n = power.size();
+        if (n == 1) {
+            return power[0];
+        }
+        if (n == 2) {
+            return max(power[0], power[1]);
+        }
+        vector<int> dp(3);
         
     }
 };
