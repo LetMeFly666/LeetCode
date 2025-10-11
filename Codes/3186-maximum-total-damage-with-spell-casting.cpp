d0c41520c859dfcd017f27bad0931648a129a830
LetMeFly666 <814114971@qq.com>
2025-10-11 18:35:19 +0800

    3186: dbg.cpp (#1167) - 尝试使用c20的ranges::失败

--- Codes/3186-maximum-total-damage-with-spell-casting.cpp
+++ Codes/3186-maximum-total-damage-with-spell-casting.cpp
@@ -2,26 +2,23 @@
  * @Author: LetMeFly
  * @Date: 2025-10-11 18:10:29
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-11 18:18:57
+ * @LastEditTime: 2025-10-11 18:33:47
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
+#include <algorithm>
 #endif
 
 typedef long long ll;
-// 感觉会WA
 class Solution {
 public:
     ll maximumTotalDamage(vector<int>& power) {
-        int n = power.size();
-        vector<ll> dp(4);
-        for (int i = 0; i < min(n, 3); i++) {
-            dp[i + 1] = power[i];
+        unordered_map<int, int> cnt;
+        for (int t : power) {
+            cnt[t]++;
         }
-        for (int i = 3; i < n; i++) {
-            ll thisVal = max(dp[0] + power[i], dp[3]);
-            dp[0] = dp[1], dp[1] = dp[2], dp[2] = dp[3], dp[3] = thisVal;
-        }
-        return max(dp[1], max(dp[2], dp[3]));
+        vector<pair<int, int>> values(cnt.begin(), cnt.end());
+        std::ranges::sort(values);
+        return 0;
     }
 };
