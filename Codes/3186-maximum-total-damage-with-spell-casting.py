90c5f25c358f8b02c5cf6d431369db3926975187
LetMeFly666 <814114971@qq.com>
2025-10-11 18:46:05 +0800

    3186: CE.py (#1167)

--- Codes/3186-maximum-total-damage-with-spell-casting.py
+++ Codes/3186-maximum-total-damage-with-spell-casting.py
@@ -1,5 +1,20 @@
+'''
+Author: LetMeFly
+Date: 2025-10-11 18:10:29
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-10-11 18:45:38
+'''
 from typing import List
+from collections import Counter
 
 class Solution:
     def maximumTotalDamage(self, power: List[int]) -> int:
-        
+        cnt = Counter(power)
+        values = sorted(cnt)
+        dp = [0] * len(values + 1)
+        j = 0
+        for i, val in enumerate(values):
+            while values[j] < val + 2:
+                j += 1
+            dp[i + 1] = max(dp[i], dp[j] + val * cnt[val])
+        return dp[-1]
