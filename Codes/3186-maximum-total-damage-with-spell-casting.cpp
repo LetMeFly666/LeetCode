fe51d4be6e269e46c395d39f5ed0b9a648930f95
LetMeFly666 <814114971@qq.com>
2025-10-11 18:21:25 +0800

    3186: WA.cpp (#1167)
    使用power[i]不是不能使用power[i - 1]，而是不能使用power[i] - 1

--- Codes/3186-maximum-total-damage-with-spell-casting.cpp
+++ Codes/3186-maximum-total-damage-with-spell-casting.cpp
@@ -2,25 +2,26 @@
  * @Author: LetMeFly
  * @Date: 2025-10-11 18:10:29
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-11 18:14:19
+ * @LastEditTime: 2025-10-11 18:18:57
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
 typedef long long ll;
-
+// 感觉会WA
 class Solution {
 public:
     ll maximumTotalDamage(vector<int>& power) {
         int n = power.size();
-        if (n == 1) {
-            return power[0];
+        vector<ll> dp(4);
+        for (int i = 0; i < min(n, 3); i++) {
+            dp[i + 1] = power[i];
         }
-        if (n == 2) {
-            return max(power[0], power[1]);
+        for (int i = 3; i < n; i++) {
+            ll thisVal = max(dp[0] + power[i], dp[3]);
+            dp[0] = dp[1], dp[1] = dp[2], dp[2] = dp[3], dp[3] = thisVal;
         }
-        vector<int> dp(3);
-        
+        return max(dp[1], max(dp[2], dp[3]));
     }
 };
