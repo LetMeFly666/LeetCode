320e0d1dd39ec17cbdd0afe46d6cf797fef0a677
LetMeFly666 <814114971@qq.com>
2025-10-11 19:05:00 +0800

    3186: AC.py O(1)空间 (#1167)  - AC,O(1)空间,98.02%,97.69%

--- Codes/3186-maximum-total-damage-with-spell-casting_O1.py
+++ Codes/3186-maximum-total-damage-with-spell-casting_O1.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-10-11 18:10:29
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-10-11 19:02:11
+LastEditTime: 2025-10-11 19:04:17
 '''
 from typing import List
 from collections import Counter
@@ -16,11 +16,12 @@ class Solution:
         for val in values:
             valSum = val * cnt[val]
             if val - val2 > 2:  # 无冲突
-                thisVal = valSum + dp2
+                useThis = valSum + dp2
             elif val - val1 > 2:  # 和val2相差小于2，但和val1不冲突
-                thisVal = valSum + dp1
+                useThis = valSum + dp1
             else:  # 和val1、val2都冲突(一定不会再和前面的冲突了)
-                thisVal = valSum + dp0
-            dp0, dp1, dp2 = dp1, dp2, thisVal
+                useThis = valSum + dp0
+            dp = max(useThis, dp2)
+            dp0, dp1, dp2 = dp1, dp2, dp
             val1, val2 = val2, val
         return dp2
