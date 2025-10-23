9491d765f50c19037786275e3b0e9ec844ec131c
LetMeFly666 <814114971@qq.com>
2025-10-23 23:31:59 +0800

    3461: AC.cpp+py + CE.go (#1184)
    cpp - AC,43.56%,21.78%
    py - AC,84.04%,89.36%

--- Codes/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ Codes/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-10-23 23:05:13
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-23 23:23:01
+ * @LastEditTime: 2025-10-23 23:24:34
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -24,11 +24,11 @@ public:
         while (s.size() > 2) {
             string newS;
             for (int i = 1; i < s.size(); i++) {
-                cout << '(' << s[0] << '+' << s[1] << " - '`') % 10 + '0' = " << char((s[0] + s[1] - '`') % 10 + '0') << endl;
-                newS.push_back((s[0] + s[1] - '`') % 10 + '0');
+                // cout << '(' << s[i - 1] << '+' << s[i] << " - '`') % 10 + '0' = " << char((s[i - 1] + s[i] - '`') % 10 + '0') << endl;
+                newS.push_back((s[i - 1] + s[i] - '`') % 10 + '0');
             }
             s = move(newS);
-            cout << s << endl;
+            // cout << s << endl;
         }
         return s[0] == s[1];
     }
--- Codes/3461-check-if-digits-are-equal-in-string-after-operations-i.go
+++ Codes/3461-check-if-digits-are-equal-in-string-after-operations-i.go
@@ -1,5 +1,22 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-10-23 23:05:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-10-23 23:30:56
+ */
 package main
 
 func hasSameDigits(s string) bool {
-    
+    a := make([]byte, len(s))
+    for i := range s {
+        a[i] = s[i] - '0'
+    }
+    for len(a) > 2 {
+        b := make([]byte,len(a) - 1)
+        for i := range b {
+            b[i] = (a[i] + a[i + 1]) % 10
+        }
+        a = b
+        return a[0] == a[1]
+    }
 }
