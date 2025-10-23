d96929e7c3859c64eafa5d807e524de64457372f
LetMeFly666 <814114971@qq.com>
2025-10-23 23:24:07 +0800

    3461: WA.cpp (#1184)

--- Codes/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ Codes/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-10-23 23:05:13
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-23 23:19:30
+ * @LastEditTime: 2025-10-23 23:23:01
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -24,9 +24,11 @@ public:
         while (s.size() > 2) {
             string newS;
             for (int i = 1; i < s.size(); i++) {
-                s.push_back((s[0] + s[1] - '`') % 10 + '0');
+                cout << '(' << s[0] << '+' << s[1] << " - '`') % 10 + '0' = " << char((s[0] + s[1] - '`') % 10 + '0') << endl;
+                newS.push_back((s[0] + s[1] - '`') % 10 + '0');
             }
             s = move(newS);
+            cout << s << endl;
         }
         return s[0] == s[1];
     }
