acceb687077b42d386457ca6af724f6c98998af2
LetMeFly666 <814114971@qq.com>
2025-10-27 09:45:27 +0800

    refactor: format Java code

--- Codes/0669-trim-a-binary-search-tree.java
+++ Codes/0669-trim-a-binary-search-tree.java
@@ -1,4 +1,10 @@
-import javax.swing.tree.TreeNode;
+/*
+ * @Author: laughWinter
+ * @Date: 2025-10-26 23:16:50
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-10-27 09:42:30
+ */
+// import javax.swing.tree.TreeNode;
 
 /**
  * Definition for a binary tree node.
@@ -19,29 +25,31 @@ import javax.swing.tree.TreeNode;
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
         //寻找新的根节点
-        while(root!=null && (root.val<low || root.val>high)) {
-            if(root.val<low) {
-                root=root.right;
-            }else if(root.val>high) {
-                root=root.left;
+        while ((root != null) && ((root.val < low) || (root.val > high))) {
+            if (root.val < low) {
+                root = root.right;
+            } else if (root.val > high) {
+                root = root.left;
             }
         }
-        if(root==null) return null;
+        if (root == null) {
+            return null;
+        }
         //修建root的左右子树
-        TreeNode tmp=root;
-        while(tmp.left!=null) {
-            if(tmp.left.val<low){
-                tmp.left=tmp.left.right;
-            }else{
-                tmp=tmp.left;
+        TreeNode tmp = root;
+        while (tmp.left != null) {
+            if (tmp.left.val < low) {
+                tmp.left = tmp.left.right;
+            } else {
+                tmp = tmp.left;
             }
         }
-        tmp=root;
-        while(tmp.right!=null) {
-            if(tmp.right.val>high) {
-                tmp.right=tmp.right.left;
-            }else{
-                tmp=tmp.right;
+        tmp = root;
+        while (tmp.right != null) {
+            if (tmp.right.val > high) {
+                tmp.right = tmp.right.left;
+            } else {
+                tmp = tmp.right;
             }
         }
 
