847a284d857fa367fa1e81f01d41006de6cf0a0a
LetMeFly666 <814114971@qq.com>
2025-10-30 22:53:57 +0800

    3346: WA.cpp (#1179) - target不能只nums[i]

--- Codes/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ Codes/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-10-21 18:51:21
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-21 18:51:51
+ * @LastEditTime: 2025-10-30 22:51:38
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -12,13 +12,19 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         sort(nums.begin(), nums.end());
+        unordered_map<int, int> frequency;
+        for (int t : nums) {
+            frequency[t]++;
+        }
         int ans = 0;
-        k *= 2;
-        for (int l = 0, r = 0; r < nums.size(); r++) {
-            while (nums[r] - nums[l] > k) {
+        for (int l = 0, r = 0, i = 0; i < nums.size(); i++) {
+            while (nums[i] - nums[l] > k) {
                 l++;
             }
-            ans = max(ans, r - l + 1);
+            while (r < nums.size() && nums[r] - nums[i] <= k) {
+                r++;
+            }
+            ans = max(ans, min(r - l, numOperations + frequency[nums[i]]));
         }
         return ans;
     }
