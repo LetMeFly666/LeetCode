e20f21c99341d1312f580103bb551113a61713db
LetMeFly666 <814114971@qq.com>
2025-10-30 23:20:19 +0800

    3346: AC.rust (#1179)  - AC,75.63%,42.86%

--- Codes/3346-maximum-frequency-of-an-element-after-performing-operations-i.rs
+++ Codes/3346-maximum-frequency-of-an-element-after-performing-operations-i.rs
@@ -2,16 +2,16 @@
  * @Author: LetMeFly
  * @Date: 2025-10-30 22:39:36
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-10-30 23:16:49
+ * @LastEditTime: 2025-10-30 23:19:32
  */
 use std::collections::HashMap;
 // 这台机器上没有安装过rust，故无IDE语法检查了
 impl Solution {
-    pub fn max_frequency(nums: mut Vec<i32>, k: i32, num_operations: i32) -> i32 {
+    pub fn max_frequency(mut nums: Vec<i32>, k: i32, num_operations: i32) -> i32 {
         nums.sort();
         let mut frequency = HashMap::new();
         for &t in &nums {
-            *frequency.entry(t).or_insert(t) += 1;
+            *frequency.entry(t).or_insert(0) += 1;  // 不存在时默认值为0
         }
         
         let mut ans: i32 = 0;
@@ -24,7 +24,7 @@ impl Solution {
             while r < nums.len() && nums[r] - target <= k {
                 r += 1;
             }
-            ans = ans.max((r - l) as i32.min(num_operations + *frequency.get(&target).unwrap_or(&0)));
+            ans = ans.max(((r - l) as i32).min(num_operations + *frequency.get(&target).unwrap_or(&0)));
         }
         ans
     }
