430a6e0c95434c2039562d7e18bb7d717beadc32
LetMeFly666 <814114971@qq.com>
2025-11-14 19:17:30 +0800

    2536: AC.py (#1202) - AC,60.80%,29.60%

--- Codes/2536-increment-submatrices-by-one.py
+++ Codes/2536-increment-submatrices-by-one.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-11-14 17:51:14
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-11-14 19:07:10
+LastEditTime: 2025-11-14 19:16:40
 '''
 from typing import List
 
@@ -11,14 +11,14 @@ class Solution:
         diff = [[0] * (n + 1) for _ in range(n + 1)]
         for x1, y1, x2, y2 in queries:
             diff[x1][y1] += 1
-            diff[x1 + 1][y2] -= 1
-            diff[x1][y1 + 1] -= 1
+            diff[x2 + 1][y1] -= 1
+            diff[x1][y2 + 1] -= 1
             diff[x2 + 1][y2 + 1] += 1
         ans = [[0] * n for _ in range(n)]
         for i in range(n):
             for j in range(n):
                 ans[i][j] = diff[i][j] + \
-                            0 if not i else ans[i - 1][j] + \
-                            0 if not j else ans[i][j - 1] + \
-                            0 if not i or not j else ans[i - 1][j - 1]
+                            (0 if not i else ans[i - 1][j]) + \
+                            (0 if not j else ans[i][j - 1]) - \
+                            (0 if not i or not j else ans[i - 1][j - 1])
         return ans
