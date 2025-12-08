73552bd24a48230159f3125850e54d51aa72ed78
LetMeFly666 <814114971@qq.com>
2025-12-08 21:54:42 +0800

    1925: CE.rust (#1204) - 无编译器检查

--- Codes/1925-count-square-sum-triples.rs
+++ Codes/1925-count-square-sum-triples.rs
@@ -1,5 +1,21 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-12-08 21:36:58
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-12-08 21:53:08
+ */
 impl Solution {
     pub fn count_triples(n: i32) -> i32 {
-        
+        let mut ans: i32 = 0;
+        for a in 1..n+1 {
+            for b in 1..n+1 {
+                let k = a * a + b * b;
+                let c = k.sqrt();
+                if c as i32 <= n && c * c == k {
+                    ans += 1;
+                }
+            }
+        }
+        ans
     }
 }
