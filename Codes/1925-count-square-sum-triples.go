d20f2728a02a874ace917393b5e71f307c98b4c3
LetMeFly666 <814114971@qq.com>
2025-12-08 19:00:41 +0800

    1925: AC.py+go+java (#1204)
    py - AC,51.53%,59.51%
    go - AC,55.56%,83.33%
    java - AC,65.28%,22.22%

--- Codes/1925-count-square-sum-triples.go
+++ Codes/1925-count-square-sum-triples.go
@@ -1,5 +1,22 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-12-08 18:40:41
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-12-08 18:57:02
+ */
 package main
 
-func countTriples(n int) int {
-    
+import "math"
+
+func countTriples(n int) (ans int) {
+    for a := 1; a <= n; a++ {
+        for b := 1; b <= n; b++ {
+            k := a * a + b * b
+            c := int(math.Sqrt(float64(k)))
+            if c <= n && c * c == k {
+                ans++
+            }
+        }
+    }
+    return
 }
--- Codes/1925-count-square-sum-triples.py
+++ Codes/1925-count-square-sum-triples.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2025-12-08 18:40:41
 LastEditors: LetMeFly.xyz
-LastEditTime: 2025-12-08 18:49:37
+LastEditTime: 2025-12-08 18:53:01
 '''
 from math import sqrt
 
@@ -12,6 +12,11 @@ class Solution:
         for a in range(1, n + 1):
             for b in range(1, n + 1):
                 k = a * a + b * b
-                c = sqrt(k)
+                c = int(sqrt(k))
                 ans += c <= n and c * c == k
+                # if c <= n and c * c == k:
+                #     print(f"({a}, {b}, {c})")
         return ans
+
+# a = Solution()
+# print(a.countTriples(5))
