deceb3f1cfca1af7d05401c47ea77039f2d42b81
LetMeFly666 <814114971@qq.com>
2025-12-08 18:51:05 +0800

    1925: AC.cpp+WA.py (#1204)
    cpp - AC,64.52%,44.35%

--- Codes/1925-count-square-sum-triples.cpp
+++ Codes/1925-count-square-sum-triples.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-12-08 18:40:41
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-12-08 18:42:32
+ * @LastEditTime: 2025-12-08 18:47:09
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -16,9 +16,21 @@ public:
             for (int b = 1; b <= n; b++) {
                 int k = a * a + b * b;
                 int c = sqrt(k);
-                ans += c * c == k;
+                ans += c * c == k && c <= n;
+                // if (c * c == k) {
+                //     printf("(%d, %d, %d)\n", a, b, c);
+                // }
             }
         }
         return ans;
     }
 };
+
+#if defined(_WIN32) || defined(__APPLE__)
+int main() {
+    Solution sol;
+    cout << sol.countTriples(5) << endl;
+    cout << sol.countTriples(12) << endl;
+    return 0;
+}
+#endif
--- Codes/1925-count-square-sum-triples.py
+++ Codes/1925-count-square-sum-triples.py
@@ -1,3 +1,17 @@
+'''
+Author: LetMeFly
+Date: 2025-12-08 18:40:41
+LastEditors: LetMeFly.xyz
+LastEditTime: 2025-12-08 18:49:37
+'''
+from math import sqrt
+
 class Solution:
     def countTriples(self, n: int) -> int:
-        
+        ans = 0
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+                k = a * a + b * b
+                c = sqrt(k)
+                ans += c <= n and c * c == k
+        return ans
