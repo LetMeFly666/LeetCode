7b4adc66d43a5d1dde9f0f2ed3fa907824f45c4a
LetMeFly666 <814114971@qq.com>
2025-12-17 23:03:20 +0800

    3573: WA.cpp (#1263)

--- Codes/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ Codes/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2025-12-17 21:38:59
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-12-17 22:58:48
+ * @LastEditTime: 2025-12-17 23:02:36
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -37,14 +37,14 @@ private:
         if (status == 0) {
             return cache[key] = max({dfs(i - 1, j, 0), dfs(i - 1, j - 1, 1) + prices[i], dfs(i - 1, j - 1, 2) - prices[i]});
         } else if (status == 1) {
-            return cache[key] = dfs(i - 1, j, 0);
+            return cache[key] = max(dfs(i - 1, j - 1, 0) - prices[i], dfs(i - 1, j, 0));
         } else {
-            return cache[key] = dfs(i - 1, j, 0) + prices[i];
+            return cache[key] = max(dfs(i - 1, j - 1, 0) + prices[i], dfs(i - 1, j, 2));
         }
     }
 public:
     ll maximumProfit(vector<int>& prices, int k) {
         this->prices = move(prices);
-        return dfs(this->prices.size(), k, 0);
+        return dfs(this->prices.size() - 1, k, 0);
     }
 };
