6cc5673b92783eaa7f39b3ca49cff0792be40965
LetMeFly666 <814114971@qq.com>
2025-12-22 23:05:53 +0800

    960: AC.rust (#1272)
    rust - AC,16.67%,33.33%

--- Codes/0960-delete-columns-to-make-sorted-iii.rs
+++ Codes/0960-delete-columns-to-make-sorted-iii.rs
@@ -2,20 +2,20 @@
  * @Author: LetMeFly
  * @Date: 2025-12-22 22:28:49
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-12-22 23:04:04
+ * @LastEditTime: 2025-12-22 23:05:28
  */
 impl Solution {
     pub fn min_deletion_size(strs: Vec<String>) -> i32 {
         let m = strs[0].len();
-        let mut ans = vec![0; m];
+        let mut dp = vec![0; m];
         for cur in 0..m {
             for prev in 0..cur {
-                if ans[prev] > ans[cur] && strs.iter().all(|s| s.as_bytes()[prev] <= s.as_bytes()[cur]) {
-                    ans[cur] = ans[prev];
+                if dp[prev] > dp[cur] && strs.iter().all(|s| s.as_bytes()[prev] <= s.as_bytes()[cur]) {
+                    dp[cur] = dp[prev];
                 }
             }
-            ans[cur]++;
+            dp[cur] += 1;
         }
-        m as i32 - *dp.iter().max().unwarp()
+        m as i32 - *dp.iter().max().unwrap()
     }
 }
