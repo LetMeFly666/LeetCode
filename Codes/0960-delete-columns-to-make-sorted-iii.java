aca28ac583c783108cbbb65781d9af80e4962cae
LetMeFly666 <814114971@qq.com>
2025-12-22 23:04:24 +0800

    960: AC.java+CE.rust (#1272)
    java - AC,87.23%,40.42%
    rust - 边搜边写

--- Codes/0960-delete-columns-to-make-sorted-iii.java
+++ Codes/0960-delete-columns-to-make-sorted-iii.java
@@ -2,12 +2,12 @@
  * @Author: LetMeFly
  * @Date: 2025-12-22 22:28:49
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2025-12-22 22:58:00
+ * @LastEditTime: 2025-12-22 22:59:50
  */
 class Solution {
     private boolean can(String[] strs, int prev, int cur) {
         for (String s : strs) {
-            if (s[prev] > s[cur]) {
+            if (s.charAt(prev) > s.charAt(cur)) {
                 return false;
             }
         }
@@ -20,7 +20,7 @@ class Solution {
         int longest = 0;
         for (int cur = 0; cur < m; cur++) {
             for (int prev = 0; prev < cur; prev++) {
-                if (dp[cur] < dp[prev] && can(strs, cur, prev)) {
+                if (dp[cur] < dp[prev] && can(strs, prev, cur)) {
                     dp[cur] = dp[prev];
                 }
             }
--- Codes/0960-delete-columns-to-make-sorted-iii.rs
+++ Codes/0960-delete-columns-to-make-sorted-iii.rs
@@ -1,5 +1,21 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2025-12-22 22:28:49
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2025-12-22 23:04:04
+ */
 impl Solution {
     pub fn min_deletion_size(strs: Vec<String>) -> i32 {
-        
+        let m = strs[0].len();
+        let mut ans = vec![0; m];
+        for cur in 0..m {
+            for prev in 0..cur {
+                if ans[prev] > ans[cur] && strs.iter().all(|s| s.as_bytes()[prev] <= s.as_bytes()[cur]) {
+                    ans[cur] = ans[prev];
+                }
+            }
+            ans[cur]++;
+        }
+        m as i32 - *dp.iter().max().unwarp()
     }
 }
