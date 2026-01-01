e6c69349ba4763902aaae8b6816b6a2de33f7f52
LetMeFly666 <814114971@qq.com>
2026-01-01 15:43:10 +0800

    66: WA.cpp

---
+++ Codes/0066-plus-one.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-01-01 15:40:30
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-01-01 15:42:17
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int cnt = 0;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            digits[i] += cnt;
+            cnt = digits[i] / 10;
+            digits[i] %= 10;
+        }
+        if (cnt) {
+            digits.insert(digits.begin(), cnt);
+        }
+        return digits;
+    }
+};
