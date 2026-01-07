fffd7b445deeff6b9336de332c9bd0a75a7f3e20
LetMeFly666 <814114971@qq.com>
2026-01-07 21:39:50 +0800

    1339: WA.cpp (#1308) - all 0

---
+++ Codes/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,44 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-01-07 21:29:44
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-01-07 21:38:35
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int ans;
+    int dfs(TreeNode* root, int cnt = 0) {
+        int leftSum = 0, rightSum = 0;
+        cnt += root->val;
+        if (root->left) {
+            int leftSum = dfs(root->left, cnt);
+        }
+        if (root->right) {
+            int rightSum = dfs(root->right, cnt);
+        }
+        ans = max(ans, (cnt + leftSum) * rightSum);
+        ans = max(ans, (cnt + rightSum) * leftSum);
+        return root->val + leftSum + rightSum;
+    }
+public:
+    int maxProduct(TreeNode* root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+};
