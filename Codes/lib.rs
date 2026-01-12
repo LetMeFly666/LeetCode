f9f133cc2de8d7bdb611be2cd39eaf72697b9cfe
LetMeFly666 <Tisfy@qq.com>
2026-01-12 23:37:44 +0800

    1266: AC.cpp+py+java+go+rust (#1321) (#1318) + word(en)
    cpp - AC,100.00%,16.04%
    py - AC,100.00%,5.26%
    java - AC,91.80%,8.20%
    go - AC,100.00%,85.71%
    rust - AC,100.00%,75.00%

--- Codes/lib.rs
+++ Codes/lib.rs
@@ -5,7 +5,7 @@
  * @LastEditTime: 2026-01-06 13:17:16
  */
 pub struct Solution;
-include!("1458-max-dot-product-of-two-subsequences.rs");  // 这个fileName是会被脚本替换掉的
+include!("1266-minimum-time-visiting-all-points.rs");  // 这个fileName是会被脚本替换掉的
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
