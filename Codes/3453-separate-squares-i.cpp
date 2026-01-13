932caf436fbe52e52ff02ec58d9dd7677f73290a
LetMeFly666 <Tisfy@qq.com>
2026-01-13 21:41:39 +0800

    3453: init (#1322)

---
+++ Codes/3453-separate-squares-i.cpp
@@ -0,0 +1,10 @@
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    double separateSquares(vector<vector<int>>& squares) {
+        
+    }
+};
