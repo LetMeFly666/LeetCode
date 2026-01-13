41c42a5bad0dabc0694ad8067ac3c0df665eb3c9
LetMeFly666 <Tisfy@qq.com>
2026-01-13 22:21:58 +0800

    3453: AC.cpp (#1322) - AC,50.66%,77.15%

--- Codes/3453-separate-squares-i.cpp
+++ Codes/3453-separate-squares-i.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-01-13 21:41:10
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-01-13 22:08:40
+ * @LastEditTime: 2026-01-13 22:21:20
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -17,15 +17,16 @@ private:
         double total = 0;
         for (vector<int>& s : squares) {
             double from = max(double(s[1]), h);
-            double to = s[0] + s[1];
-            total += max(0., (to - from) * s[1]);
+            double to = s[1] + s[2];
+            total += max(0., (to - from) * s[2]);
+            // printf("(%llf -> %llf), total = %llf\n", from, to, total);
         }
 
         return total > half;
     }
 public:
     double separateSquares(vector<vector<int>>& squares) {
-        long long total;
+        long long total = 0;  // !!!!!记得初始化
         for (vector<int>& s : squares) {
             total += ((long long) s[2]) * s[2];
         }
@@ -36,10 +37,11 @@ public:
         for (int _ = 0; _ < 50; _++) {
             double mid = (l + r) / 2;
             if (check(mid)) {
-                r = mid;
-            } else {
                 l = mid;
+            } else {
+                r = mid;
             }
+            // printf("l = %llf, r = %llf, mid = %llf\n", l, r, mid);
         }
         return l;
     }
@@ -51,3 +53,19 @@ public:
 >>> 10**9 / 2**47
 7.105427357601002e-06
 */
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+[[0,0,1],[2,2,1]]
+
+*/
+int main() {
+    string s;
+    while (cin >> s) {
+        vector<vector<int>> a = stringToVectorVector(s);
+        Solution sol;
+        cout << sol.separateSquares(a) << endl;
+    }
+    return 0;
+}
+#endif
