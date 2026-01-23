f2cda5a5d7fd1d6a72f3fe30ae17b852552b9566
LetMeFly666 <814114971@qq.com>
2026-01-23 21:23:25 +0800

    3510: refactor+RE.cpp (#1342)

--- Codes/3510-minimum-pair-removal-to-sort-array-ii.cpp
+++ Codes/3510-minimum-pair-removal-to-sort-array-ii.cpp
@@ -1,8 +1,8 @@
 /*
  * @Author: LetMeFly
- * @Date: 2026-01-23 19:26:50
+ * @Date: 2026-01-23 20:28:25
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-01-23 20:28:25
+ * @LastEditTime: 2026-01-23 21:22:45
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -10,99 +10,57 @@
 
 
 typedef long long ll;
+typedef pair<ll, int> node;  // <nums[idx]+nums[right(idx)], idx>
 class Solution {
-private:
-    vector<int> lFather, rFather;
-    vector<bool> isNode;
-
-    int left(int idx) {
-        int lIdx = lFather[idx];
-        if (lIdx == -1) {
-            return -1;
-        }
-        if (isNode[lIdx]) {
-            return lIdx;
-        }
-        return lFather[idx] = left(lIdx);
-    }
-
-    int right(int idx) {
-        int rIdx = rFather[idx];
-        if (rIdx == -1) {
-            return -1;
-        }
-        if (isNode[rIdx]) {
-            return rIdx;
-        }
-        return rFather[idx] = right(rIdx);
-    }
-
-    int getRank(ll a, ll b, ll c, ll d) {
-        return 0 + (a > b) + (b > c) + (c > d);
-    }
-
-    int getRank(ll a, ll b, ll c) {
-        return 0 + (a > b) + (b > c);
-    }
-
-    // merge nums[idx]和nums[idx]的right，并返回熵减了多少
-    int merge(vector<ll>& nums, int idx) {
-        int lIdx = left(idx);
-        int secondIdx = right(idx);  // assert: idx一定存在right
-        int rIdx = right(secondIdx);
-        ll lValue = lIdx == -1 ? -1e18 : nums[lIdx], rValue = rIdx == -1 ? 1e18 : nums[rIdx];
-
-        int beforeRank = getRank(lValue, nums[idx], nums[secondIdx], rValue);
-        nums[idx] += nums[secondIdx];
-        int afterRank = getRank(lValue, nums[idx], rValue);
-        
-        isNode[secondIdx] = false;
-        rFather[idx] = rIdx;
-        if (rIdx != -1) {
-            lFather[rIdx] = idx;
-        }
-        
-        return beforeRank - afterRank;
-    }
 public:
     int minimumPairRemoval(vector<int>& numsInt) {
-        isNode.resize(numsInt.size(), true);
-        lFather.resize(numsInt.size());
-        rFather.resize(numsInt.size());
-        for (int i = 0; i < numsInt.size(); i++) {
-            lFather[i] = i - 1;
-            rFather[i] = i + 1;
-        }
-        rFather[numsInt.size() - 1] = -1;
-
+        vector<ll> nums(numsInt.begin(), numsInt.end());
+        set<node> nodes;
+        set<int> idxs;
         int cntRev = 0;
-        vector<ll> nums(numsInt.size());
-        nums[0] = numsInt[0];
-        for (int i = 1; i < numsInt.size(); i++) {
-            if (numsInt[i] < numsInt[i - 1]) {
-                cntRev++;
-            }
-            nums[i] = numsInt[i];
-        }
-
-        auto cmp = [this, &nums](int i, int j) {
-            return nums[i] + nums[right(i)] > nums[j] + nums[(right(j))];
-        };
-        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
         for (int i = 0; i + 1 < nums.size(); i++) {
-            pq.push(i);
+            idxs.insert(i);
+            nodes.insert({nums[i] + nums[i + 1], i});
+            cntRev += nums[i] > nums[i + 1];
         }
+        idxs.insert(numsInt.size() - 1);
 
         int ans = 0;
         while (cntRev) {
+            set<node>::iterator nodeIt = nodes.begin();
+            ll nodeVal = nodeIt->first;
+            int nodeIdx = nodeIt->second;
+            nodes.erase(nodeIt);
+
+            set<int>::iterator idxIt = idxs.find(nodeIdx);
+            set<int>::iterator secondIdxIt = next(idxIt);  // 一定有next
+            cntRev -= nums[*idxIt] > nums[*secondIdxIt];
+            
+            // 左
+            if (idxIt != idxs.begin()) {
+                set<int>::iterator lIdxIt = prev(idxIt);
+                node lNode = node{nums[*lIdxIt] + nums[*idxIt], *lIdxIt};
+                nodes.erase(lNode);
+                node lNodeNew = node{lNode.first + nums[*secondIdxIt], *lIdxIt};
+                nodes.insert(lNodeNew);
+                cntRev -= nums[*lIdxIt] > nums[*idxIt];
+                cntRev += nums[*lIdxIt] > nodeVal;
+            }
+
+            // 右
+            set<int>::iterator rIdxIt = next(secondIdxIt);
+            if (rIdxIt != idxs.end()) {
+                node rNode = node{nums[*secondIdxIt] + nums[*rIdxIt], *secondIdxIt};
+                nodes.erase(rNode);
+                node rNodeNew = node{nodeVal + nums[*rIdxIt], *idxIt};
+                nodes.insert(rNodeNew);
+                cntRev -= nums[*secondIdxIt] > nums[*rIdxIt];
+                cntRev += nodeVal > nums[*rIdxIt];
+            }
+
+            nums[*idxIt] = nodeVal;
+            idxs.erase(idxIt);
             ans++;
-            int idx;
-            do {
-                idx = pq.top();
-                pq.pop();
-            } while (!isNode[idx]);
-            pq.push(idx);
-            cntRev -= merge(nums, idx);
         }
         return ans;
     }
