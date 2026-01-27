478487880959d253b9bf67fca55f4a1cded0134e
LetMeFly666 <Tisfy@qq.com>
2026-01-27 23:38:57 +0800

    3650: TLE.cpp(#1350) - WTF? 一个dijisktra

--- Codes/3650-minimum-cost-path-with-edge-reversals.cpp
+++ Codes/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -2,44 +2,43 @@
  * @Author: LetMeFly
  * @Date: 2026-01-27 23:03:10
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-01-27 23:24:36
+ * @LastEditTime: 2026-01-27 23:38:15
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
 #endif
 
 class Solution {
-private:
-    inline void go(int to, int nowCost, vector<bool>& visited, unordered_map<int, vector<pair<int, int>>> graph, priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>>& pq) {
-        for (pair<int, int>& toInfo : graph[to]) {
-            int nextTo = toInfo.first, w = toInfo.second;
-            if (visited[nextTo]) {
-                continue;
-            }
-            visited[nextTo] = true;
-            pq.push({nowCost + w, to, nextTo});
-        }
-    }
 public:
     int minCost(int n, vector<vector<int>>& edges) {
-        unordered_map<int, vector<pair<int, int>>> graph;
-        for (vector<int>& edge : edges) {
-            int from = edge[0], to = edge[1], w = edge[2];
-            graph[from].push_back({to, w});
-            graph[to].push_back({from, 2 * w});
+        vector<vector<pair<int, int>>> graph(n);  // graph[from]: [<to, cost>, ...]
+        for (vector<int>& edge: edges) {
+            int from = edge[0], to = edge[1], cost = edge[2];
+            graph[from].push_back({to, cost});
+            graph[to].push_back({from, 2 * cost});
         }
-        vector<bool> visited(n);
-        visited[0] = true;
-
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;  // <cost, from, to>
-        go(0, 0,  visited, graph, pq);
+        vector<int> costs(n, INT_MAX);
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, 0});
+        costs[0] = 0;
         while (pq.size()) {
-            auto[cost, from, to] = pq.top();
+            auto [cost, to] = pq.top();
             pq.pop();
+            if (cost > costs[to]) {
+                continue;
+            }
             if (to == n - 1) {
                 return cost;
             }
-            go(to, cost, visited, graph, pq);
+            for (auto[nextTo, nextCost] : graph[to]) {
+                nextCost += cost;
+                if (nextCost > costs[nextTo]) {
+                    continue;
+                }
+                costs[nextTo] = nextCost;
+                pq.push({nextCost, nextTo});
+            }
         }
         return -1;  // FAKE RETURN
     }
