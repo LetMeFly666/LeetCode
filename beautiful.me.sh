096728c4a1832df419bbdd8657ce6186cf41d5f4
LetMeFly666 <814114971@qq.com>
2026-01-30 19:27:45 +0800

    feat: by hand 2
    worker=2 round=0
    
    worker=2 round=1
    
    worker=2 round=2
    
    worker=2 round=3
    
    worker=2 round=4
    
    worker=2 round=5
    
    worker=2 round=6
    
    worker=2 round=7
    
    worker=2 round=8
    
    worker=2 round=9
    
    worker=2 round=10
    
    worker=2 round=11
    
    worker=2 round=12
    
    worker=2 round=13
    
    worker=2 round=14
    
    worker=2 round=15
    
    worker=2 round=16
    
    worker=2 round=17
    
    worker=2 round=18

--- beautiful.me.sh
+++ beautiful.me.sh
@@ -7,16 +7,16 @@ MEM_FILE_PATH="$(realpath "../memFiles")"
 FLAG="$MEM_FILE_PATH/flag"
 
 if [[ -z "$PREFIX" ]]; then
-  echo "Usage: $0 <hex-prefix> [threads]"
-  exit 1
+    echo "Usage: $0 <hex-prefix> [threads]"
+    exit 1
 fi
 if ! [[ "$PREFIX" =~ ^[0-9a-fA-F]+$ ]]; then
-  echo "prefix must be in range [0-9a-fA-F]"
-  exit 1
+    echo "prefix must be in range [0-9a-fA-F]"
+    exit 1
 fi
 if (( ${#PREFIX} > 40 )); then
-  echo "SHA1's max length is 40"
-  exit 1
+    echo "SHA1's max length is 40"
+    exit 1
 fi
 echo "prefix=$PREFIX  threads=$THREADS"
 
@@ -30,54 +30,51 @@ echo "cp first dir"
 rsync -a "$ORIG_REPO/" "$firstDir/"
 echo "cp first dir done"
 for i in $(seq 2 "$THREADS"); do
-  echo "cp dir $i"
-  rsync -a "$firstDir/" "$MEM_FILE_PATH/$i/"
+    echo "cp dir $i"
+    rsync -a "$firstDir/" "$MEM_FILE_PATH/$i/"
 done
 echo "all dirs copied"
 
 pids=()
 worker() {
-  local id=$1
-  local dir="$MEM_FILE_PATH/$id"
-  cd "$dir"
-  echo "worker $id: started at $dir"
+    local id=$1
+    local dir="$MEM_FILE_PATH/$id"
+    cd "$dir"
+    echo "worker $id: started at $dir"
 
-  local round=0
-  while true; do
-    if [ -e "$FLAG" ]; then
-      echo "worker $id: detected finished flag; exiting"
-      return
-    fi
-    export GIT_COMMITTER_DATE="$(date +%s.%N)"
-    export GIT_AUTHOR_DATE="$GIT_COMMITTER_DATE"
-    git commit --amend -S --no-edit --allow-empty >/dev/null 2>&1
-    sha="$(git rev-parse HEAD)"
-    ((round++))
-    if (( found % 100 == 0 )); then
-      echo "🧵 [$id] tried=$i sha=${sha:0:12}"
-    fi
-    if (( $round > 3 )); then
-      break
-    fi
+    local round=0
+    while true; do
+        if [ -e "$FLAG" ]; then
+            echo "worker $id: detected finished flag; exiting"
+            return
+        fi
 
-    # # if found
-    # echo "worker $id: FOUND!"
-    # echo "$1" > "$FLAG"
-    # rm -rf "$ORIG_REPO/.git"
-    # rsync -a --delete "$dir/.git" "$ORIG_REPO/.git"
-    # echo "worker $id: result synced to $ORIG_REPO/.git; exiting"
-  done
+        git commit --amend -S --no-edit --allow-empty -m "$(printf "%s\n\nworker=%s round=%s" "$(git log -1 --pretty=%B)" "$id" "$round")" >/dev/null 2>&1
+        sha="$(git rev-parse HEAD)"
+        ((round++))
+        if (( round % 100 == 0 )); then
+            echo "🧵 [$id] tried=$round sha=${sha:0:12}"
+        fi
+
+        if [[ "$sha" == "$PREFIX"* ]]; then
+            echo "worker $id: FOUND!"
+            echo "$1" > "$FLAG"
+            rm -rf "$ORIG_REPO/.git"
+            rsync -a --delete "$dir/.git" "$ORIG_REPO/.git"
+            echo "worker $id: result synced to $ORIG_REPO/.git; exiting"
+        fi
+    done
 
 }
 for i in $(seq 1 "$THREADS"); do
-  worker "$i" & pids+=($!)
+    worker "$i" & pids+=($!)
 done
 # wait -n
 # echo "parent: detected a worker finished; setting finished"
 # for pid in "${pids[@]}"; do
-#   if kill -0 "$pid" 2>/dev/null; then
-#     kill "$pid" 2>/dev/null || true
-#   fi
+#     if kill -0 "$pid" 2>/dev/null; then
+#         kill "$pid" 2>/dev/null || true
+#     fi
 # done
 wait
 sudo umount "$MEM_FILE_PATH"
