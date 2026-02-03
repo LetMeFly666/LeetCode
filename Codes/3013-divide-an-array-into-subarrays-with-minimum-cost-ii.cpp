13b67e3c15a4d199c96e5205127dc9a08232a814
LetMeFly666 <Tisfy@qq.com>
2026-02-03 22:11:41 +0800

    3651: AC.cpp (#1361) - AC,81.84%,38.83%

--- Codes/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ Codes/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-02-03 21:39:44
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-02-03 22:01:16
+ * @LastEditTime: 2026-02-03 22:11:11
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -12,8 +12,17 @@ typedef long long ll;
 class Solution {
 public:
     ll minimumCost(vector<int>& nums, int k, int dist) {
+        k--;
         multiset<ll> stage(nums.begin() + 1, nums.begin() + dist + 2), candidate;
-        ll ans = accumulate(nums.begin() + 1, nums.begin() + dist + 2, 0ll), nowAns = ans;
+        ll ans = accumulate(nums.begin(), nums.begin() + dist + 2, 0ll);
+        while (stage.size() > k) {
+                int retiree = *stage.rbegin();
+                stage.erase(prev(stage.end()));
+                ans -= retiree;
+                candidate.insert(retiree);
+        }
+
+        ll nowAns = ans;
         for (int end = dist + 2; end < nums.size(); end++) {
             int in = nums[end], out = nums[end - dist - 1];
             
@@ -47,8 +56,28 @@ public:
                 candidate.insert(retiree);
             }
 
-            ans = max(ans, nowAns);
+            ans = min(ans, nowAns);
         }
         return ans;
     }
 };
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+[1,3,2,6,4,2]
+3
+3
+
+5
+*/
+int main() {
+    string s;
+    int a, b;
+    while (cin >> s >> a >> b) {
+        Solution sol;
+        vector<int> v = stringToVector(s);
+        cout << sol.minimumCost(v, a, b) << endl;
+    }
+    return 0;
+}
+#endif
