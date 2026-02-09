1402f0c71e8becf5b334242e18804ed4740ab772
LetMeFly666 <Tisfy@qq.com>
2026-02-09 23:10:50 +0800

    1382: WA.cpp (#1373)

---
+++ Codes/1382-balance-a-binary-search-tree.cpp
@@ -0,0 +1,58 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-02-09 23:05:29
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-02-09 23:09:10
+ */
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*
+1 2 3 4 5 6 7 8 9
+
+     5
+  2     7
+1  3   6 8
+   4      9
+*/
+class Solution {
+private:
+    vector<TreeNode*> nodes;
+
+    void dfs(TreeNode* node) {
+        if (!node) {
+            return;
+        }
+        dfs(node->left);
+        nodes.push_back(node);
+        dfs(node->right);
+    }
+
+    TreeNode* build(int l, int r) {
+        if (l <= r) {
+            return nullptr;
+        }
+        int mid = (l + r) >> 1;
+        TreeNode* root = nodes[mid];
+        root->left = build(l, mid);
+        root->right = build(mid, r);
+        return root;
+    }
+public:
+    TreeNode* balanceBST(TreeNode* root) {
+        dfs(root);
+        return build(0, nodes.size());
+    }
+};
