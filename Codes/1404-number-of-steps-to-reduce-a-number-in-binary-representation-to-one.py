bb57e171695df8655fe031bf1d1600233c8a1940
Tisfy <Tisfy@foxmail.com>
2026-02-26 23:59:22 +0800

    1404: WA.py (#1413)

--- Codes/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ Codes/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -2,16 +2,28 @@
 Author: LetMeFly
 Date: 2026-02-26 23:55:52
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-02-26 23:56:54
+LastEditTime: 2026-02-26 23:58:03
 '''
+"""
+1101
+1110
+111
+1000
+100
+10
+1
+"""
 class Solution:
     def numSteps(self, s: str) -> int:
         a = int(s)
         ans = 0
-        while a:
+        while a != 1:
             if a % 2:
                 a += 1
             else:
                 a //= 2
             ans += 1
+            # print(a)
         return ans
+
+# print(Solution.numSteps('', '1101'))
