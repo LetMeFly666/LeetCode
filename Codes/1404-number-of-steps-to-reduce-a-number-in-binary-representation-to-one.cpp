5a664597fb4f5e72e96d6e1503149aa7eb8bd481
Tisfy <Tisfy@foxmail.com>
2026-02-27 21:51:59 +0800

    1404: WA.cpp (#1413)

--- Codes/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ Codes/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-02-26 22:06:13
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-02-26 23:55:06
+ * @LastEditTime: 2026-02-27 21:50:19
  */
 #if defined(_WIN32) || defined(__APPLE__)
 #include "_[1,2]toVector.h"
@@ -17,16 +17,113 @@
 10
 1
 */
-// TODO: 高精度模拟
+
+class LetsNum {
+private:
+    // 低位->高位
+    deque<char> num;
+public:
+    // From binary string
+    LetsNum(string& s) {
+        for (char c : s) {
+            *this <<= 1;
+            *this += c;
+        }
+    }
+
+    LetsNum& operator++(int) {
+        int carry = 1;
+        for (deque<char>::iterator it = num.begin(); it != num.end(); it++) {
+            carry += *it - '0';
+            *it = carry % 10 + '0';
+            carry /= 10;
+        }
+        if (carry) {
+            num.push_back(carry + '0');
+        }
+        return *this;
+    }
+
+    // only 0/1 supported
+    LetsNum& operator+=(const int& n) {
+        if (!n) {
+            return *this;
+        }
+        (*this)++;
+        return *this;
+    }
+
+    // only 0/1 supported
+    LetsNum& operator+=(const char& c) {
+        *this += c - '0';
+        return *this;
+    }
+
+    // only <<1 supported
+    LetsNum& operator<<=(const int&) {
+        int carry = 0;
+        for (deque<char>::iterator it = num.begin(); it != num.end(); it++) {
+            carry += (*it - '0') * 2;
+            *it = carry % 10 + '0';
+            carry /= 10;
+        }
+        if (carry) {
+            num.push_back(carry + '0');
+        }
+        return *this;
+    }
+
+    // only >>1 supported
+    LetsNum& operator>>=(const int&) {
+        num.erase(num.begin());
+        return *this;
+    }
+
+    // only 1 supported
+    bool operator!=(const int&) {
+        return num.size() != 1 || *num.begin() != '1';
+    }
+
+    bool isOdd() {
+        return (*num.begin() - '0') % 2;
+    }
+
+    void print() {
+        for (deque<char>::reverse_iterator it = num.rbegin(); it != num.rend(); it++) {
+            putchar(*it);
+        }
+        puts("");
+    }
+};
+
 class Solution {
 public:
     int numSteps(string s) {
-        int now = 0, ans = 0;
-        reverse(s.begin(), s.end());
-        for (int i = 0; i < s.size(); i++) {
-            now += s[i] - '0';
-            
+        LetsNum num(s);
+        int ans = 0;
+        while (num != 1) {
+            if (num.isOdd()) {
+                num += 1;
+            } else {
+                num >>= 1;
+            }
+            ans++;
         }
         return ans;
     }
 };
+
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+1101
+*/
+int main() {
+    string s;
+    while (cin >> s) {
+        Solution sol;
+        cout << sol.numSteps(s) << endl;
+    }
+    return 0;
+}
+#endif
