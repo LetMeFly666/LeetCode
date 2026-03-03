c6724f1307ea20b964ab07b86fc87a6c9cfe14a1
LetMeFly666 <Tisfy@qq.com>
2026-03-03 20:15:19 +0800

    1545: WA.py (#1423)

--- Codes/1545-find-kth-bit-in-nth-binary-string_dfs_eaiserToUnderstand.py
+++ Codes/1545-find-kth-bit-in-nth-binary-string_dfs_eaiserToUnderstand.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2026-03-03 20:11:50
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-03-03 20:13:10
+LastEditTime: 2026-03-03 20:15:07
 '''
 class Solution:
     def invert(self, n: str) -> str:
@@ -11,7 +11,7 @@ class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         if n == 1:
             return '0'
-        len = 1 << n - 1
+        len = (1 << n) - 1
         half = len >> 1
         if k == half + 1:
             return '1'
