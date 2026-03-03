b27f06fed33bbd7a13a8ca027717599ec603ed52
LetMeFly666 <Tisfy@qq.com>
2026-03-03 09:12:57 +0800

    1545: CE.cpp (#1423) - 悬垂引用

---
+++ Codes/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,28 @@
+#if defined(_WIN32) || defined(__APPLE__)
+#include "_[1,2]toVector.h"
+#endif
+// x - 1的字符串
+class Solution {
+private:
+    void invert(string& s) {
+        for (char& c : s) {
+            c = c == '0' ? '1' : '0';
+        }
+    }
+
+    string& next(string& now) {
+        string ans = now + '1';
+        invert(now);
+        reverse(now.begin(), now.end());
+        ans += now;
+        return ans;
+    }
+public:
+    char findKthBit(int n, int k) {
+        string now = "0";
+        for (int i = 2; i <= n; i++) {
+            now = next(now);
+        }
+        return now[k];
+    }
+};
