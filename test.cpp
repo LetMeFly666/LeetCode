447c689a140ddaff4d0fbc00720ae6cd05ef4071
LetMeFly666 <Tisfy@qq.com>
2026-03-05 22:15:42 +0800

    1758: WA.cpp (#1427)

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-03-05 09:35:39
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-03-05 09:36:03
+ */
+#ifdef _DEBUG
+#include <stdio.h>
+int main() {
+    printf("1\n");
+    return 0;
+}
+#else
+int main() {
+    
+}
+#endif
--- toSay.md
+++
@@ -1,7 +0,0 @@
-<!--
- * @Author: LetMeFly
- * @Date: 2026-03-03 20:26:53
- * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-03-03 20:41:15
--->
-昨天连CSDN发文都变得需要审核好久了 （不知道是不是巧合
