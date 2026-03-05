f53f3e5eedc6f2ef7953de6f5615b5ec709f60b3
LetMeFly666 <Tisfy@qq.com>
2026-03-05 22:17:18 +0800

    1758: AC.cpp (#1427) - AC,100.00%,32.67%

--- Codes/1758-minimum-changes-to-make-alternating-binary-string_20260305.cpp
+++ Codes/1758-minimum-changes-to-make-alternating-binary-string_20260305.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-03-05 22:13:51
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-03-05 22:14:17
+ * @LastEditTime: 2026-03-05 22:16:57
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -13,8 +13,8 @@ public:
     int minOperations(string s) {
         int c0 = 0, c1 = 0;
         for (int i = 0; i < s.size(); i++) {
-            c0 += s[i] % 2 == 1;  // 不用减'0'就行
-            c1 += s[i] % 2 == 0;
+            c0 += s[i] % 2 == i % 2;  // 不用减'0'就行
+            c1 += s[i] % 2 != i % 2;
         }
         return min(c0, c1);
     }
