7e0b1770b9b1f58aee38cf7947683e710fe7d44e
LetMeFly666 <Tisfy@qq.com>
2026-03-05 22:38:49 +0800

    fix: header reg
    only codex found this.
    
    https://github.com/LetMeFly666/LeetCode/pull/1429#discussion_r2890327215

--- newSolution.py
+++ newSolution.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2022-07-03 11:21:14
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-03-05 22:12:36
+LastEditTime: 2026-03-05 22:38:46
 Command: python newSolution.py 102. 二叉树的层序遍历
 What's more: 当前仅支持数字开头的题目
 What's more: 代码结构写的很混乱 - 想单文件实现所有操作
@@ -357,7 +357,7 @@ def genSolutionPart(num):
             new_comment = '\n'.join(kept)
             # 2. 删除紧跟的单行 #if defined(...) #include "_[1,2]toVector.h" #endif
             rest = re.sub(
-                r'^\s*#if\s+defined\([^\)]*\)(?:\s*\|\|\s*defined\([^\)]*\))*\s*\n\s*#include\s+"_\[1,2\]toVector\.h"\s*\n\s*#endif\s*\n?',
+                r'^\s*#ifdef\s+\w+\s*\n\s*#include\s+"_\[1,2\]toVector\.h"\s*\n\s*#endif\s*\n?',
                 '',
                 rest,
                 count=1
