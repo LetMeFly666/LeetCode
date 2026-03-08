bd3d503f208924a09b65f6ff79e8c8901037664d
LetMeFly666 <Tisfy@qq.com>
2026-03-08 11:21:24 +0800

    1888: why.cpp (#1431)
    input: 001000000010
    should: 4
    my: 3

--- Codes/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ Codes/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-03-07 17:46:05
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-03-08 11:07:29
+ * @LastEditTime: 2026-03-08 11:19:50
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -31,6 +31,9 @@ public:
         for (int i = 0; i < n; i++) {
             total += s[i] % 2 == i % 2;
         }
+        #ifdef _DEBUG
+        cout << "total: " << total << endl;
+        #endif
         int ans = min(total, n - total);
         for (int i = 0, now = 0; i < n; i++) {
             now += s[i] % 2 == i % 2;
@@ -39,7 +42,38 @@ public:
             int changed_front = i + 1 - original_front;
             int changed_back = n - i - 1 - original_back;
             ans = min(ans, min(original_front + changed_back, changed_front + original_back));
+            #ifdef _DEBUG
+            printf("i = %d, original_front = %d, original_back = %d, changed_front = %d, changed_back = %d, original_front + changed_back = %d, changed_front + original_back = %d\n", i, original_front, original_back, changed_front, changed_back, original_front + changed_back, changed_front + original_back);
+            if (min(original_front + changed_back, changed_front + original_back) == 3) {
+                printf("ans = %d\n", ans);
+            }
+            #endif
         }
         return ans;
     }
 };
+
+#if defined(_WIN32) || defined(__APPLE__)
+/*
+001000000010
+|   | | |
+101010101010
+
+001000000010
+    | | |
+001010101010
+-  右移一位
+010101010100
+   3次不可
+
+4
+*/
+int main() {
+    string s;
+    while (cin >> s) {
+        Solution sol;
+        cout << sol.minFlips(s) << endl;
+    }
+    return 0;
+}
+#endif
