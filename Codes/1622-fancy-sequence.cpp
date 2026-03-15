d74ecd16e5a8342fcf969de064d61eae204585a5
LetMeFly666 <Tisfy@qq.com>
2026-03-15 10:57:52 +0800

    1662: CE.cpp (#1443) - Line 29: Char 19: runtime error: signed integer overflow: -4702111234474983746 * -4702111234474983746 cannot be represented in type 'll' (aka 'long long') (solution.cpp) SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior prog_joined.cpp:34:19

---
+++ Codes/1622-fancy-sequence.cpp
@@ -0,0 +1,83 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-03-15 10:21:03
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-03-15 10:56:07
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+/*
+
+
+*/
+
+typedef long long ll;
+
+const ll MOD = 1e9 + 7;
+class Num {
+private:
+    ll val;
+
+    ll power(ll a, ll n) const {
+        ll ans = 1;
+        for (; n; n >>= 1) {
+            if (n & 1) {
+                ans = ans * a % MOD;
+            }
+            a = a * a % MOD;
+        }
+        return ans;
+    }
+public:
+    Num() {}
+    Num(ll n) : val(n % MOD) {}
+    Num(int n) : val(n % MOD) {}
+    Num operator+(const ll& b) const { return Num((val + b) % MOD); }
+    Num operator-(const ll& b) const { return Num((val - b + MOD) % MOD); }
+    Num operator*(const ll& b) const { return Num(val * b % MOD); }
+    Num operator/(const ll& b) const { return Num(val * power(b, MOD - 2) % MOD); }
+    Num operator-(const Num& b) const { return Num((val - b.val + MOD) % MOD); }
+    Num operator/(const Num& b) const { return Num(val * power(b.val, MOD - 2) % MOD); }
+    Num& operator+=(const ll& b) { return *this = *this + b; }
+    Num& operator-=(const ll& b) { return *this = *this - b; }
+    Num& operator*=(const ll& b) { return *this = *this * b; }
+    Num& operator/=(const ll& b) { return *this = *this / b; }
+    operator int() const { return static_cast<int>(val); }
+};
+
+class Fancy {
+private:
+    vector<Num> vals;
+    Num add;
+    Num mul;
+public:
+    Fancy() {}
+    
+    void append(int val) {
+        vals.push_back((Num(val) - add) / mul);
+    }
+    
+    void addAll(int inc) {
+        add += inc;
+    }
+    
+    void multAll(int m) {
+        add *= m;
+        mul *= m;
+    }
+    
+    int getIndex(int idx) {
+        return idx >= vals.size() ? -1 : (int)vals[idx];
+    }
+};
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy* obj = new Fancy();
+ * obj->append(val);
+ * obj->addAll(inc);
+ * obj->multAll(m);
+ * int param_4 = obj->getIndex(idx);
+ */
