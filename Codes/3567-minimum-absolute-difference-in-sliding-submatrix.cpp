8eaa69b6d3fbcc69fba9100bb70c9f463feb04d3
LetMeFly666 <Tisfy@qq.com>
2026-03-20 23:23:43 +0800

    3567: WA.cpp (#1453) - ...

--- Codes/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ Codes/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,3 +1,9 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-03-20 23:10:42
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-03-20 23:21:43
+ */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
 #endif
@@ -9,14 +15,19 @@ public:
         vector<vector<int>> ans(n - k + 1, vector<int>(m - k + 1));
         for (int i = 0; i < n - k + 1; i++) {
             for (int j = 0; j < m - k + 1; j++) {
-                int m = grid[i][j], M = m;
+                vector<int> vals;
+                vals.reserve(k * k);
                 for (int di = 0; di < k; di++) {
                     for (int dj = 0; dj < k; dj++) {
-                        m = min(m, grid[i + di][j + dj]);
-                        M = max(M, grid[i + di][j + dj]);
+                        vals.push_back(grid[i + di][j + dj]);
                     }
                 }
-                ans[i][j] = M - m;
+                sort(vals.begin(), vals.end());
+                int diff = 1000000;
+                for (int i = 1; i < k * k; i++) {
+                    diff = min(diff, vals[i] - vals[i - 1]);
+                }
+                ans[i][j] = diff == 1000000 ? 0 : diff;
             }
         }
         return ans;
