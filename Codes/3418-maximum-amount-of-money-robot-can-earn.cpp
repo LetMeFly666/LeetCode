9c4c1bfb77913fb12fb8d6ad60aada6756ff5a6b
LetMeFly666 <Tisfy@qq.com>
2026-04-03 08:14:28 +0800

    3418: WA.cpp (#1478)

---
+++ Codes/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,31 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-04-02 23:07:35
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-04-03 08:13:03
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+// THIS CANNOT BE ACCEPTED - 是不抢走不是变负为正 - 并且abs范围也笔误写大了
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        int n = coins.size(), m = coins[0].size();
+        array<vector<vector<int>>, 3> dp;
+        dp.fill(vector<vector<int>>(n, vector<int>(m)));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int th = 0; th < 3; th++) {
+                    dp[th][i][j] = coins[i][j] + max(i ? dp[th][i - 1][j] : 0, j ? dp[th][i][j - 1] : 0);
+                }
+                for (int th = 1; th < 3; th++) {
+                    dp[th][i][j] = max(dp[th][i][j], abs(coins[i][j] + max(i ? dp[th - 1][i - 1][j] : 0, j ? dp[th - 1][i][j - 1] : 0)));
+                }
+            }
+        }
+
+        return dp[2][n - 1][m - 1];
+    }
+};
