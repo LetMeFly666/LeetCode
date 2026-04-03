eabb8456bb6bda729082f8cbaae4082e9324a5f1
LetMeFly666 <Tisfy@qq.com>
2026-04-03 08:37:43 +0800

    3418: clean.cpp (#1478) - AC,65.79%,53.35%

--- Codes/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ Codes/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-04-02 23:07:35
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-04-03 08:29:08
+ * @LastEditTime: 2026-04-03 08:32:33
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -25,18 +25,8 @@ public:
                 }
                 for (int th = 0; th < 3; th++) {
                     dp[th][i][j] = coins[i][j] + max(i ? dp[th][i - 1][j] : -INF, j ? dp[th][i][j - 1] : -INF);
-                    // printf("dp[%d][%d][%d] = %d\n", th, i, j, dp[th][i][j]);
                 }
                 for (int th = 1; th < 3; th++) {
-                    // int a = dp[th][i][j], b = max(i ? dp[th - 1][i - 1][j] : -INF, j ? dp[th - 1][i][j - 1] : -INF);
-                    // if (b > a) {
-                    //     printf("dp[%d][%d][%d]: %d -> ", th, i, j, a);
-                    //     if (i && dp[th - 1][i - 1][j] > (j ? dp[th - 1][i][j - 1] : 0)) {
-                    //         printf
-                    //     } else {
-                    //         printf
-                    //     }
-                    // }
                     dp[th][i][j] = max(dp[th][i][j], max(i ? dp[th - 1][i - 1][j] : -INF, j ? dp[th - 1][i][j - 1] : -INF));
                 }
             }
