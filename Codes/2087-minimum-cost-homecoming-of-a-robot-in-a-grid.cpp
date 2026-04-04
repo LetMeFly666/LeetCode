edcbf85ad807f07debe8e402a584e4ba6ef85b5a
LetMeFly666 <Tisfy@qq.com>
2026-04-04 14:09:31 +0800

    2087: AC.cpp (#1481) - AC,100.00%,14.71%

--- Codes/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
+++ Codes/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-04-04 13:57:29
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-04-04 14:01:52
+ * @LastEditTime: 2026-04-04 14:05:23
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -12,11 +12,11 @@ class Solution {
 public:
     int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
         int ans = 0;
-        for (int from = min(startPos[0], homePos[0]), to = max(startPos[0], homePos[0]); from < to; from++) {
-            ans += rowCosts[from + 1];
+        for (int from = startPos[0], to = homePos[0], direction = to > from ? 1 : -1; from != to; from += direction) {
+            ans += rowCosts[from + direction];
         }
-        for (int from = min(startPos[1], homePos[1]), to = max(startPos[1], homePos[1]); from < to; from++) {
-            ans += colCosts[from + 1];
+        for (int from = startPos[1], to = homePos[1], direction = to > from ? 1 : -1; from != to; from += direction) {
+            ans += colCosts[from + direction];
         }
         return ans;
     }
