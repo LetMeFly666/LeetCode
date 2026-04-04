51d8d98204faa5e6bbdadadd61366c029253a4e4
LetMeFly666 <Tisfy@qq.com>
2026-04-04 14:03:33 +0800

    2087: WA.cpp (#1481) - 不一定是[from + 1]

--- Codes/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
+++ Codes/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-04-04 13:57:29
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-04-04 13:59:37
+ * @LastEditTime: 2026-04-04 14:01:52
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -12,11 +12,11 @@ class Solution {
 public:
     int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
         int ans = 0;
-        for (int from = min(startPos[0], homePos[0]), to = max(startPos[0], homePos[0]); from + 1 < to; from++) {
-            ans += colCosts[from + 1];
-        }
-        for (int from = min(startPos[1], homePos[1]), to = max(startPos[1], homePos[1]); from + 1 < to; from++) {
+        for (int from = min(startPos[0], homePos[0]), to = max(startPos[0], homePos[0]); from < to; from++) {
             ans += rowCosts[from + 1];
+        }
+        for (int from = min(startPos[1], homePos[1]), to = max(startPos[1], homePos[1]); from < to; from++) {
+            ans += colCosts[from + 1];
         }
         return ans;
     }
