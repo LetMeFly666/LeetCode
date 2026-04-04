f88f4b900fbecb001fa5cdaac1ed3b499a434c9f
LetMeFly666 <Tisfy@qq.com>
2026-04-04 14:11:20 +0800

    2087: AC.py (#1481) - AC,84.21%,26.32%

--- Codes/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
+++ Codes/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.py
@@ -2,12 +2,12 @@
 Author: LetMeFly
 Date: 2026-04-04 13:57:29
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-04-04 14:09:01
+LastEditTime: 2026-04-04 14:10:12
 '''
 from typing import List
 
 class Solution:
     def minCost(self, startPos: List[int], homePos: List[int], rowCosts: List[int], colCosts: List[int]) -> int:
         sx, sy = startPos
-        hx, hy = homePos
-        return sum(rowCosts[sx : hx + 1]) + sum(colCosts[sy : hy + 1]) -rowCosts[sx] - colCosts[sy]
+        ex, ey = homePos
+        return sum(rowCosts[min(sx, ex) : max(sx, ex) + 1]) + sum(colCosts[min(sy, ey) : max(sy, ey) + 1]) - rowCosts[sx] - colCosts[sy]
