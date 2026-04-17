42c99307c66637d3a2abeba29797f8fa63186710
Tisfy <Tisfy@foxmail.com>
2026-04-17 23:08:59 +0800

    3761: WA.cpp (#1510)

---
+++ Codes/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -0,0 +1,35 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-04-17 23:04:32
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-04-17 23:07:32
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+constexpr int inf = 1000000;
+class Solution {
+private:
+    int reverse(int n) {
+        int ans = 0;
+        while (n) {
+            ans = ans * 10 + n % 10;
+            n >>= 10;
+        }
+        return ans;
+    }
+public:
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int, int> ma;
+        int ans = inf;
+        for (int i = 0, n = nums.size(); i < n; i++) {
+            int r = reverse(nums[i]);
+            if (ma.count(r)) {
+                ans = min(ans, i - ma[r]);
+            }
+            ma[r] = i;
+        }
+        return ans == inf ? -1 : ans;
+    }
+};
