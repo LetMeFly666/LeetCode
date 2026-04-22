2c6dc1c95dee613df11014fc7ef2addc0180eafb
LetMeFly <Tisfy@qq.com>
2026-04-22 23:32:52 +0800

    update: 添加问题“2452.距离字典两次编辑以内的单词”的代码和题解 (#1537)
    * 2452: WA.cpp (#1535) - forgot to break
    
    * 2452: AC.cpp (#1535) -AC,43.18%,29.55%
    
    * update: 添加问题“2452.距离字典两次编辑以内的单词”的代码和题解 (#1537)
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>
    
    * typo: ￥->$
    
    Update Solutions/LeetCode 2452.距离字典两次编辑以内的单词.md
    
    Co-authored-by: gh-pr-review[bot] <268385713+gh-pr-review[bot]@users.noreply.github.com>
    
    * typo: remove多余汉字
    
    Update Solutions/LeetCode 2452.距离字典两次编辑以内的单词.md
    
    Co-authored-by: gh-pr-review[bot] <268385713+gh-pr-review[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>
    Co-authored-by: gh-pr-review[bot] <268385713+gh-pr-review[bot]@users.noreply.github.com>

---
+++ Codes/2452-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,32 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-04-22 23:07:22
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-04-22 23:10:53
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+inline bool ok(const string& a, const string& b) {
+    int diff = 0;
+    for (int i = 0; i < a.size(); i++) {
+        diff += a[i] != b[i];
+    }
+    return diff <= 2;
+}
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> ans;
+        for (string& a : queries) {
+            for (string& b : dictionary) {
+                if (ok(a, b)) {
+                    ans.push_back(a);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
