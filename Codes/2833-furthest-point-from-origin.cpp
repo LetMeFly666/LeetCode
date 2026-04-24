76ff4af70c4f48475a6a7bf1b99f4acf875a7202
Tisfy <Tisfy@foxmail.com>
2026-04-24 22:46:00 +0800

    test: WHAT A BIG BUG!!! (#1543)

---
+++ Codes/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-04-24 22:43:17
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-04-24 22:45:59
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        test
+    }
+};
+
---
+++ Codes/2833-furthest-point-from-origin.py
@@ -0,0 +1,4 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        
+
