8f1f301b20dee0346ebb41369ad894bff98dfe59
LetMeFly666 <Tisfy@qq.com>
2026-04-24 13:55:20 +0800

    2615: AC.cpp (#1538) - suffix数组2total变量 - AC,86.08%,80.59%
    https://github.com/LetMeFly666/LeetCode/pull/1541#discussion_r3135705424

---
+++ Codes/2615-sum-of-distances_better.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-04-24 13:53:24
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-04-24 13:53:47
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+typedef long long ll;
+class Solution {
+private:
+    void cal(vector<int>& idxs, vector<ll>& ans) {
+        int n = idxs.size();
+        ll total = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            total += idxs[i];
+        }
+        ll prefix = 0;
+        for (int i = 0; i < n; i++) {
+            ans[idxs[i]] += (total - prefix - idxs[i]) - (ll)(n - i - 1) * idxs[i];
+            ans[idxs[i]] += (ll)i * idxs[i] - prefix;
+            prefix += idxs[i];
+        }
+    }
+public:
+    vector<ll> distance(vector<int>& nums) {
+        unordered_map<int, vector<int>> ma;
+        for (int i = 0; i < nums.size(); i++) {
+            ma[nums[i]].push_back(i);
+        }
+
+        vector<ll> ans(nums.size());
+        for (auto&& [_, idxs] : ma) {
+            cal(idxs, ans);
+        }
+        return ans;
+    }
+};
