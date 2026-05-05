44bb70941b356dfbeda38e022b1d687f8798b51c
LetMeFly666 <Tisfy@qq.com>
2026-05-05 11:49:13 +0800

    0061: RE.cpp (#1568)
    Line 26: Char 19: runtime error: member access within null pointer of type 'ListNode' (solution.cpp)
    SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior prog_joined.cpp:35:19

---
+++ Codes/0061-rotate-list_20260505.cpp
@@ -0,0 +1,42 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-05-05 11:42:10
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-05-05 11:48:08
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        int n = 1;
+        ListNode* p = head;
+        while (p->next) {
+            n++;
+            p = p->next;
+        }
+        p->next = head;
+        k %= n;
+        k = n - k;
+
+        p = head;
+        for (int i = 1; i < k; i++) {
+            p = p->next;
+        }
+        ListNode* ans = p->next;
+        p->next = nullptr;
+        return ans;
+    }
+};
