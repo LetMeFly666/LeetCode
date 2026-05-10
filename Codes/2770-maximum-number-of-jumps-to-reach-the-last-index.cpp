852f1e759a4b0aa99ab2ddc34051490749834f19
LetMeFly666 <Tisfy@qq.com>
2026-05-10 19:51:44 +0800

    2700: WA.cpp (#1579) - should max not min ans

--- Codes/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ Codes/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-05-10 19:40:18
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-05-10 19:46:10
+ * @LastEditTime: 2026-05-10 19:51:26
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -15,30 +15,29 @@ private:
     vector<int> mem;
 
     int dfs(int loc) {
-        if (mem[loc] >= -1) {
-            return mem[loc];
-        }
         if (loc == 0) {
-            return mem[loc] = 0;
+            return 0;
+        }
+        int& ans = mem[loc];  // 引用
+        if (ans) {
+            return ans;
         }
 
-        int ans = n + 1;
+        ans = INT_MIN;
         for (int i = 0; i < loc; i++) {
             if (abs(nums[loc] - nums[i] <= target)) {
-                int val = dfs(i);
-                if (val != -1) {
-                    ans = min(ans, val + 1);
-                }
+                ans = max(ans, dfs(i) + 1);
             }
         }
-        return mem[loc] = ans == n + 1 ? -1 : ans;
+        return ans;
     }
 public:
     int maximumJumps(vector<int>& nums, int target) {
         n = nums.size();
         this->nums = move(nums);
         this->target = target;
-        mem = move(vector<int>(n, -2));
-        return dfs(n - 1);
+        mem = move(vector<int>(n));
+        int ans = dfs(n - 1);
+        return ans < 0 ? -1 : ans;
     }
 };
