b724a8360501d13f1ab3081f4aefe84dfc021740
LetMeFly666 <Tisfy@qq.com>
2026-05-12 16:38:08 +0800

    1665: AC.cpp (#1583) - AC,37.24%,81.38%

--- Codes/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ Codes/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-05-12 16:30:14
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-05-12 16:33:29
+ * @LastEditTime: 2026-05-12 16:36:57
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -12,7 +12,7 @@ class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(), tasks.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] - a[0] < b[1] - b[0];
+            return a[1] - a[0] > b[1] - b[0];
         });
         int ans = 0, now = 0;
         for (vector<int>& task : tasks) {
@@ -26,3 +26,20 @@ public:
         return ans;
     }
 };
+
+#ifdef _DEBUG
+/*
+[[1,2],[2,4],[4,8]]
+
+8
+*/
+int main() {
+    string s;
+    while (cin >> s) {
+        vector<vector<int>> v = stringToVectorVector(s);
+        Solution sol;
+        cout << sol.minimumEffort(v) << endl;
+    }
+    return 0;
+}
+#endif
