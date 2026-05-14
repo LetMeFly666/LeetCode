26022d7f3522d974b8d2d5928f6bfbedaf089ca3
LetMeFly666 <Tisfy@qq.com>
2026-05-14 17:37:58 +0800

    test: try 15.5M libstdc++.so.6

--- test.cpp
+++ test.cpp
@@ -2,13 +2,20 @@
  * @Author: LetMeFly
  * @Date: 2026-05-13 12:08:45
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-05-14 04:06:03
+ * @LastEditTime: 2026-05-14 17:23:22
  */
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    vector<string> v;
+    v.push_back("hello");
+    v.push_back("world");
+
+    cout << v[0] << endl;
+
     int n;
     cin >> n;
     cout << n << endl;
