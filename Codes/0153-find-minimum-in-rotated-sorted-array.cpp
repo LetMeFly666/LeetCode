87432907bf6b98e2dd71f274c2ea9aa281e6a489
LetMeFly666 <Tisfy@qq.com>
2026-05-15 21:25:16 +0800

    153: half.cpp (#1588) 天呐

--- Codes/0153-find-minimum-in-rotated-sorted-array.cpp
+++ Codes/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-05-15 21:04:02
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-05-15 21:11:35
+ * @LastEditTime: 2026-05-15 21:22:40
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -11,13 +11,13 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l = -1, r = nums.size() - 1;
-        while (l + 1 < r) {  // 左右开区间
+        int l = 0, r = nums.size();  // 左闭右开
+        while (l < r) {
             int m = (l + r) >> 1;
             if (nums[m] > nums.back()) {
-                l = m;
+                l = m + 1;
             } else {
-                r = m + 1;
+                r = m;
             }
         }
     }
