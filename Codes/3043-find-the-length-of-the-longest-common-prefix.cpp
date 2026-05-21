d8c7abc1fb867724dfa77786db43ff5ac5d20f4a
LetMeFly666 <Tisfy@qq.com>
2026-05-21 22:28:27 +0800

    feat: 先全关，后面写脚本Coding状态下自动关闭结束后自动打开好了 (#1600) + remove many cpp header files associations

--- Codes/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ Codes/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-05-21 22:00:11
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-05-21 22:06:43
+ * @LastEditTime: 2026-05-21 22:15:51
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
