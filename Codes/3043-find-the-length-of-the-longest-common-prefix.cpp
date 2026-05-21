1539e4bab73168f83a0cdac7042ab5601b2304c0
LetMeFly666 <Tisfy@qq.com>
2026-05-21 22:15:17 +0800

    3043: tmp (#1600) - 尝试禁用仅Codes目录下的AI补全 - 失败

---
+++ Codes/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-05-21 22:00:11
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-05-21 22:06:43
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<int> 
+    }
+};
