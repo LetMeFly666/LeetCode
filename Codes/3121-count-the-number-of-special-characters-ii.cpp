16e643cd9ccf076ecc3cdcff2445903a522c3359
LetMeFly666 <Tisfy@qq.com>
2026-05-27 22:28:32 +0800

    3121: refactor.cpp (#1608) - 刚刚错误是因为没有“每个lower都在upper前”

--- Codes/3121-count-the-number-of-special-characters-ii.cpp
+++ Codes/3121-count-the-number-of-special-characters-ii.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-05-27 22:18:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-05-27 22:26:08
+ * @LastEditTime: 2026-05-27 22:27:21
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -25,11 +25,7 @@ public:
             int idx = small ? c - 'a' : c - 'A';
             if (state[idx] == NONE) {
                 all++;
-                if (small) {
-                    state[idx] = SMALL;
-                } else {
-                    state[idx] = CANNOT;
-                }
+                state[idx] = small ? SMALL : CANNOT;
                 if (all == 26) {
                     break;
                 }
