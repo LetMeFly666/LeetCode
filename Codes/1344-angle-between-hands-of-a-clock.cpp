3b5eaaa8076a52bf227a6385dc1a7cea0438c52d
LetMeFly666 <Tisfy@qq.com>
2026-06-18 10:03:47 +0800

    1344: tmp.cpp (#1643) - 因为分针转动说明时针不是刚好指在整数位置

--- Codes/1344-angle-between-hands-of-a-clock.cpp
+++ Codes/1344-angle-between-hands-of-a-clock.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-06-18 09:58:59
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-06-18 09:59:33
+ * @LastEditTime: 2026-06-18 10:02:14
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -13,6 +13,6 @@ public:
     double angleClock(int hour, int minutes) {
         int diff = abs(hour * 5 - minutes);
         diff = min(diff, 60 - diff);
-        return 3 * diff;
+        return 6 * diff;  // 这次肯定还是WA，但是先fix一个bug再说，一个格子是6°不是3°
     }
 };
