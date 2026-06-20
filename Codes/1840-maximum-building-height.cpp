acd31067a8724c549fe1e1ef94485647ed361416
LetMeFly666 <Tisfy@qq.com>
2026-06-20 11:08:15 +0800

    1840: WA.cpp (#1647) - 38 / 50 - 给n忘了
    input: 6 []
    output: 0
    should: 5

--- Codes/1840-maximum-building-height.cpp
+++ Codes/1840-maximum-building-height.cpp
@@ -2,30 +2,38 @@
  * @Author: LetMeFly
  * @Date: 2026-06-20 10:09:03
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-06-20 10:57:16
+ * @LastEditTime: 2026-06-20 11:07:07
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
 #endif
 
+/*
+从左到右限制一遍再从右到左限制一遍是不是就可以了呢？是不是不需要由较低的高度往两边限制呢？
+*/
 class Solution {
 private:
-    pair<int, int> cal(int a, int ha, int b, int hb) {
+    int cal(int a, int ha, int b, int hb) {
         if (ha > hb) {
-            int range_height = b - a <= ha - hb ? 
-        } else {
-
+            swap(ha, hb);
+            swap(a, b);
         }
+        return hb + (b - a - (hb - ha)) / 2;
     }
 public:
     int maxBuilding(int n, vector<vector<int>>& restrictions) {
         restrictions.push_back({0, 0});
         sort(restrictions.begin(), restrictions.end());
+        for (int i = 1; i < restrictions.size(); i++) {
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i - 1][1] + restrictions[i][0] - restrictions[i - 1][0]);
+        }
+        for (int i = restrictions.size() - 2; i >= 0; i--) {
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i + 1][1] + restrictions[i + 1][0] - restrictions[i][0]);
+        }
+        
         int ans = 0;
         for (int i = 1; i < restrictions.size(); i++) {
-            auto [range_height, last_height] = cal(restrictions[i - 1][0], restrictions[i - 1][1], restrictions[i][0], restrictions[i][1]);
-            ans = max(ans, range_height);
-            restrictions[i][1] = min(restrictions[i][1], last_height);
+            ans = max(ans, cal(restrictions[i - 1][0], restrictions[i - 1][1], restrictions[i][0], restrictions[i][1]));
         }
         return ans;
     }
