dd3dfae282633460bb8ac9f8a6f68a6e8a1611c5
LetMeFly666 <Tisfy@qq.com>
2026-06-20 10:18:27 +0800

    1840: tmp.cpp (#1647) - 忽然想起来a和b的高度不仅仅受限于a和b，还有可能受限于其左右

--- Codes/1840-maximum-building-height.cpp
+++ Codes/1840-maximum-building-height.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-06-20 10:09:03
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-06-20 10:12:06
+ * @LastEditTime: 2026-06-20 10:17:47
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -10,8 +10,15 @@
 
 class Solution {
 private:
-    int cal(vector<vector<int>>& restrictions, int a, int b) {
-        
+    int cal(int a, int ha, int b, int hb) {
+        if (ha > hb) {
+            swap(ha, hb);
+            swap(a, b);
+        }
+        if (hb - ha <= b - a) {
+            return ha + b - a;
+        }
+        return hb + (b - a - (hb - ha) - 1) / 2;
     }
 public:
     int maxBuilding(int n, vector<vector<int>>& restrictions) {
@@ -19,7 +26,7 @@ public:
         sort(restrictions.begin(), restrictions.end());
         int ans = 0;
         for (int i = 1; i < restrictions.size(); i++) {
-            ans = max(ans, cal(restrictions, i - 1, i));
+            ans = max(ans, cal(restrictions[i - 1][0], restrictions[i - 1][1], restrictions[i][0], restrictions[i][1]));
         }
         return ans;
     }
