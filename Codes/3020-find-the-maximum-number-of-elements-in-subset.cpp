0a2686db960e1ada61f970c755bffaedae3c3d5e
LetMeFly666 <Tisfy@qq.com>
2026-06-27 13:04:12 +0800

    3020: TLE.cpp (#1663)

---
+++ Codes/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,29 @@
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+private:    
+    int cal(unordered_map<int, int>& cnt, int a) {
+        int ans = 0;
+        while (cnt.contains(a) && cnt[a] >= 2) {
+            ans += 2;
+            a *= a;
+        }
+        ans += cnt.contains(a) ? 1 : -1;
+        return ans;
+    }
+public:
+    int maximumLength(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        for (int t : nums) {
+            cnt[t]++;
+        }
+
+        int ans = 0;
+        for (auto[a, _] : cnt) {
+            ans = max(ans, cal(cnt, a));
+        }
+        return ans;
+    }
+};
