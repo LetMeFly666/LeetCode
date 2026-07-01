f9125c80d2bb96549d34c7f797e298ecd2a23522
LetMeFly666 <Tisfy@qq.com>
2026-07-01 15:36:24 +0800

    1356: AC.cpp (#1669) - AC,55.76%,78.18%
    之前反例：aaacb

--- Codes/1358-number-of-substrings-containing-all-three-characters.cpp
+++ Codes/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-06-30 12:22:55
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-07-01 15:27:46
+ * @LastEditTime: 2026-07-01 15:34:43
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -13,11 +13,12 @@ public:
     int numberOfSubstrings(string s) {
         int ans = 0;
         int cnt[3] = {0};
-        for (int l = 0, r = 0, n = s.size(); r < n; l++) {
+        for (int l = 0, r = 0, n = s.size(); l < n; l++) {
             while (r < n && !(cnt[0] && cnt[1] && cnt[2])) {
                 cnt[s[r++] - 'a']++;
             }
-            ans += n - r;
+            // printf("l = %d, r = %d, cnt = {%d, %d, %d}, ans += %d\n", l, r, cnt[0], cnt[1], cnt[2], n - r + 1);
+            ans += (cnt[0] && cnt[1] && cnt[2]) ? n - r + 1 : 0;
             cnt[s[l] - 'a']--;
         }
         return ans;
