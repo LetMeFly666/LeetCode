933e97d08bce6ad35c83e23958c61e0cfc165b9c
LetMeFly <Tisfy@qq.com>
2026-07-03 11:31:07 +0800

    update: 添加问题“2812.找出最安全路径”的代码和题解 (#1674)
    * 2812: RE.cpp (#1670)
    
    * 2812: TLE.cpp (#1670)
    
    * 2812: WA.cpp (#1670) - 327 / 1036
    
    * 2812: dbg.cpp (#1670)
    
    input: [[1,0,0],[0,0,0],[0,0,1]]
    
    stdout:
    dis:
    [[0, 1, 2],
     [1, 2, 1],
     [2, 1, 0]]
    start:
    [[0, 0, 1],
     [0, 1, 1],
     [1, 1, 0]]
    end:
    [[0, 1, 1],
     [1, 1, 0],
     [1, 0, 0]]
    
    * tmp(fix bugs): 2812(#1670)
    
    1. res[nx][ny] = min(dis[nx][ny], res[x][y])，不应该是min(dis, dis)
    2. init起点的时候忘记visited=true了
    
    这样写还不对的原因：不能通过上下左右来bfs，要不然先走到小的地方可能会给大的地方直接赋值了
    
    * 2812: WA.cpp (#1670) - 212 / 1036
    
    * 2812: WA.cpp (#1670) - 1029 / 1036
    
    * 2812: half.cpp (#1670)
    
    * 2812: CE.cpp (#1670)
    
    * 2812: WA.cpp (#1670) - 479 / 1036
    
    * 2812: AC.cpp (#1670) - AC,74.11%,94.64%
    
    * 2812: tmp.py (#1670) - 2026-07-02 16:48:04 - sleeeeep
    
    * 2812: CE.py (#1670)
    
    * 2812: WA.py (#1670) - 497 / 1036
    
    * 2812: dbg.py (#1670) - 出优先队列的时候才应该更新ans
    
    * 2812: AC.py (#1670) - AC,91.67%,64.96%
    
    * update: 添加问题“2812.找出最安全路径”的代码和题解 (#1674)
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>

---
+++ Codes/2812-find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,111 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-01 17:16:47
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-01 22:58:28
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+#define debug_(x) {cout << #x << ":" << endl; debug(x);}
+
+typedef pair<int, int> pii;
+class Solution {
+private:
+    vector<vector<int>> dis;
+    static constexpr int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    void genDis(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        queue<pii> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j]) {
+                    q.push({i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        dis = vector<vector<int>>(n, vector<int>(m));
+        while (q.size()) {
+            auto [x, y] = q.front();
+            q.pop();
+            for (int d = 0; d < 4; d++) {
+                int nx = x + directions[d][0];
+                int ny = y + directions[d][1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny <m && !visited[nx][ny]) {
+                    q.push({nx, ny});
+                    visited[nx][ny] = true;
+                    dis[nx][ny] = dis[x][y] + 1;
+                }
+            }
+        }
+    }
+
+    int dfs() {
+        int n = dis.size(), m = dis[0].size();
+        auto cmp = [this](const pii& a, const pii& b) {
+            return dis[a.first][a.second] < dis[b.first][b.second];
+        };
+        priority_queue<pii, vector<pii>, decltype(cmp)> q(cmp);
+        vector<vector<bool>> visited(n, vector<bool>(m));
+        int ans = min(dis[0][0], dis[n - 1][m - 1]);
+        q.push({0, 0});
+        visited[0][0] = true;
+        if (n == 1 && m == 1) {
+            return ans;
+        }
+        
+        while (q.size()) {
+            auto [x, y] = q.top();
+            q.pop();
+            ans = min(ans, dis[x][y]);
+            for (int d = 0; d < 4; d++) {
+                int nx = x + directions[d][0];
+                int ny = y + directions[d][1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.push({nx, ny});
+                    if (nx == n - 1 && ny == m - 1) {
+                        return ans;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    
+    void debug(vector<vector<int>>& v) {
+        int n = v.size(), m = v[0].size();
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                cout << "[[";
+            } else {
+                cout << " [";
+            }
+
+            for (int j = 0; j < m; j++) {
+                if (j) {
+                    cout << ", ";
+                }
+                cout << v[i][j];
+            }
+
+            if (i == n - 1) {
+                cout << "]]";
+            } else {
+                cout << "],";
+            }
+            puts("");
+        }
+    }
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        genDis(grid);
+        return dfs();
+    }
+};
+
+// 忽然发现其实m=n
---
+++ Codes/2812-find-the-safest-path-in-a-grid.py
@@ -0,0 +1,55 @@
+'''
+Author: LetMeFly
+Date: 2026-07-01 17:16:47
+LastEditors: LetMeFly.xyz
+LastEditTime: 2026-07-03 10:59:07
+'''
+from typing import List
+from collections import deque
+import heapq
+
+class Solution:
+    def __init__(self) -> None:
+        self.directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    
+    def genDis(self, grid: List[List[int]]):
+        n, m = len(grid), len(grid[0])
+        dis = [[-1] * m for _ in range(n)]
+        q = deque()
+        for i, row in enumerate(grid):
+            for j, v in enumerate(row):
+                if v:
+                    q.append((i, j))
+                    dis[i][j] = 0
+        
+        while q:
+            x, y = q.popleft()
+            for dx, dy in self.directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and dis[nx][ny] == -1:
+                    q.append((nx, ny))
+                    dis[nx][ny] = dis[x][y] + 1
+        self.dis = dis
+
+    def dfs(self) -> int:
+        n, m = len(self.dis), len(self.dis[0])
+        visited = [[False] * m for _ in range(n)]
+        pq = [(-self.dis[0][0], 0, 0)]
+        visited[0][0] = True
+        ans = self.dis[0][0]
+        
+        while pq:
+            dis, x, y = heapq.heappop(pq)
+            dis = -dis
+            ans = min(ans, dis)
+            if x == n - 1 and y == m - 1:
+                return ans
+            for dx, dy in self.directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    heapq.heappush(pq, (-self.dis[nx][ny], nx, ny))
+
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        self.genDis(grid)
+        return self.dfs()
