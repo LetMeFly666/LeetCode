5b1b3d05cbbf1e82a0ec737e5cb1c20c715d9361
LetMeFly666 <Tisfy@qq.com>
2026-07-07 22:33:09 +0800

    1288: AC.java (#1680) - AC,6.47%,87.06% - 刚刚把这些未完成的内容也给commit上了

--- Codes/1288-remove-covered-intervals.java
+++ Codes/1288-remove-covered-intervals.java
@@ -1,5 +1,25 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-07 22:09:22
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-07 22:31:39
+ */
+import java.util.Arrays;
+
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        
+        Arrays.sort(intervals, (a, b) -> {
+            return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
+        });
+        int ans = intervals.length;
+        int r = 0;
+        for (int[] v : intervals) {
+            if (v[1] <= r) {
+                ans--;
+            } else {
+                r = v[1];
+            }
+        }
+        return ans;
     }
 }
