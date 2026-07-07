bcec57252a4f6ccb8d241b8c297fc4720cd3ce72
LetMeFly666 <Tisfy@qq.com>
2026-07-07 22:17:57 +0800

    1288: AC.cpp (#1680) - AC,46.18%,27.94%

--- Codes/1288-remove-covered-intervals.cpp
+++ Codes/1288-remove-covered-intervals.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-07-07 22:09:22
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-07-07 22:11:33
+ * @LastEditTime: 2026-07-07 22:16:53
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -11,7 +11,9 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+        });
         int ans = intervals.size();
         for (int i = 0, n = intervals.size(), r = 0; i < n; i++) {
             ans -= intervals[i][1] <= r;
