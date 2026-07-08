b27dc2e207fbfa4798e39a61eb6ca44374e1301d
LetMeFly666 <Tisfy@qq.com>
2026-07-08 14:38:06 +0800

    3756: RE.py (#1684)
    IndexError: list index out of range
                 ~~~^^^
        pow[i] = pow[i] * 10 % MOD
    Line 13 in <module> (Solution.py)

---
+++ Codes/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -0,0 +1,37 @@
+'''
+Author: LetMeFly
+Date: 2026-07-08 14:31:47
+LastEditors: LetMeFly.xyz
+LastEditTime: 2026-07-08 14:37:33
+'''
+from typing import List
+
+N = 100000
+MOD = 1000000007
+pow = [1] * N
+for i in range(1, N + 1):
+    pow[i] = pow[i] * 10 % MOD
+
+class Solution:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(queries)
+        sum = [0] * (n + 1)
+        con = [0] * (n + 1)
+        cnt1 = [0] * (n + 1)
+        for i in range(n):
+            if s[i] == '0':
+                sum[i + 1] = sum[i]
+                con[i + 1] = con[i]
+                cnt1[i + 1] = cnt1[i]
+            else:
+                v = ord(s[i]) - ord('0')
+                sum[i + 1] = sum[i] + v
+                con[i + 1] = (con[i] * 10 + v) % MOD
+                cnt1[i + 1] = cnt1[i] + 1
+        
+        ans = [0] * len(queries)
+        for i, (l, r) in enumerate(queries):
+            su = sum[r + 1] - sum[l]
+            cn = (con[r + 1] - con[l] * pow[cnt1[r + 1] - cnt1[l]]) % MOD + MOD
+        ans[i] = su * cn % MOD
+        return ans
