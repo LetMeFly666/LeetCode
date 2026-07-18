bf9d599930120dcb045fef45bad8a663c0dcedb7
LetMeFly666 <Tisfy@qq.com>
2026-07-18 08:01:30 +0800

    1288.resort: CE.rust (#1680)
    let mut ans这里需要xx.len() as i32

--- Codes/1288-remove-covered-intervals_20260718.rs
+++ Codes/1288-remove-covered-intervals_20260718.rs
@@ -2,11 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2026-07-18 07:44:07
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-07-18 07:57:42
+ * @LastEditTime: 2026-07-18 08:00:57
  */
 impl Solution {
     pub fn remove_covered_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
-        intervals.sort_by_key((x) |x[0], -x[1]|);
+        intervals.sort_by_key(|x| (x[0], -x[1]));
         let mut ans: i32 = intervals.len();
         let mut maxr = 0;
         for v in intervals.iter() {
