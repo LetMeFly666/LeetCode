c1841d4d307cd901ad18278e374105930ac9fbc3
LetMeFly666 <Tisfy@qq.com>
2026-07-23 15:02:15 +0800

    3513: AC.cpp (#1714) - AC,100.00%,77.27%

--- Codes/3513-number-of-unique-xor-triplets-i_bitLength.cpp
+++ Codes/3513-number-of-unique-xor-triplets-i_bitLength.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-07-23 14:50:11
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-07-23 14:56:23
+ * @LastEditTime: 2026-07-23 15:00:59
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -11,11 +11,11 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n = nums.size();
+        unsigned n = nums.size();
         if (n < 3) {
             return n;
         }
         
-        return (n & (-n)) << 1;
+        return 1 << bit_width(n);
     }
 };
