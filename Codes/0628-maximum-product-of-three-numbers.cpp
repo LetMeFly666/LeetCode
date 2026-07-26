1ff2d6fd560d21dd2c16737e5a8b305422963e4c
LetMeFly666 <Tisfy@qq.com>
2026-07-26 22:00:13 +0800

    628: AC.cpp (#1721) - AC,31.52%,55.01%

--- Codes/0628-maximum-product-of-three-numbers.cpp
+++ Codes/0628-maximum-product-of-three-numbers.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-07-26 18:46:00
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-07-26 19:04:58
+ * @LastEditTime: 2026-07-26 21:59:32
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -27,16 +27,21 @@
 
 ---
 
+选法：不是迫不得已不选三个负数 / 尽量不选0
 
+假设正负数都很充足： 三个最大 / 两个最小负数+一最大正数
+没有负数： 三个最大
+必须选且只选一个负数：只有一个负数和另外两个数，就三个数没得选
+有至少两个负数并且能选非负数：两个最小负数+一最大数
+只能全选负数：三个最大负数
+
+要么三个最大，要么两个最小+一最大
 */
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        vector<int> pos, neg;
-        for (int t : nums) {
-            if (t < 0) {
-
-            }
-        }
+        ranges::sort(nums);
+        int n = nums.size();
+        return max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
     }
 };
