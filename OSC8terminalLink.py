LetMeFly666/LeetCode

commit 2392b92a4320ed9101a7cd816079513c78fd70f6

update: 添加问题“3536.两个数字的最大乘积”的代码和题解 (#1720) 3536: AC.cpp (#1719) - AC,100.00%,86.27%
---

3658: re gcd (#1699)

01.return n - AC,100.00%,5.17%
02.recures  - AC,递归,100.00%,38.94%
03.traverse - AC,traverse,100.00%,37.26%

---

docs: OSC 8（终端超链接）
Signed-off-by: LetMeFly666 <Tisfy@qq.com>

.commitmsg
@@ -1,1 +1,13 @@
-3499: AC.cpp (#1710) - AC,80.00%,100.00%
+3536: AC.cpp (#1719) - AC,100.00%,86.27%
+
+---
+
+3658: re gcd (#1699)
+
+01.return n - AC,100.00%,5.17%
+02.recures  - AC,递归,100.00%,38.94%
+03.traverse - AC,traverse,100.00%,37.26%
+
+---
+
+docs: OSC 8（终端超链接）

OSC8terminalLink.py
@@ -1,0 +1,15 @@
+'''
+Author: LetMeFly
+Date: 2026-07-24 22:05:18
+LastEditors: LetMeFly.xyz
+LastEditTime: 2026-07-24 22:26:03
+Description: 在支持OSC8协议的终端输出自定义显示文字的超链接
+Description: https://gist.github.com/LetMeFly666/a15181a9edfcd25b2d60b3decb9f2dfd
+'''
+print("see TODO: for more")
+
+print(
+    "\033]8;;https://letmefly.xyz?from=terminalOSC8_20260724\033\\"
+    "宝藏小网站"
+    "\033]8;;\033\\"
+)

customePhrase_MacOSPlist2Rime.py
@@ -2,9 +2,10 @@
 Author: LetMeFly
 Date: 2026-07-22 20:56:24
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-07-22 21:13:57
+LastEditTime: 2026-07-24 22:23:57
 Description: 把MacOS的自定义短语导出为Rime可使用版本（词Tab拼写）
 Description: 首先需要在 设置-键盘-自定义短句-全部选中-导出为plist
+Description: See https://gist.github.com/LetMeFly666/f1cc13df935e99908d67a8b2bbff99eb for more
 '''
 import plistlib
 

Codes/3536-maximum-product-of-two-digits.cpp
@@ -1,0 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-25 22:11:12
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-25 22:11:39
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        int mx1 = 0, mx2 = 0;
+        while (n) {
+            int t = n % 10;
+            n /= 10;
+            if (t > mx2) {
+                mx1 = mx2;
+                mx2 = t;
+            } else if (t > mx1) {
+                mx1 = t;
+            }
+        }
+        return mx1 * mx2;
+    }
+};

Codes/3658-gcd-of-odd-and-even-sums_20260724-reGCD-01return.cpp
@@ -1,0 +1,16 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-24 10:22:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-24 10:22:37
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};

Codes/3658-gcd-of-odd-and-even-sums_20260724-reGCD-02recurse.cpp
@@ -1,0 +1,20 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-24 10:22:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-24 10:25:36
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+private:
+    int gcd(int x, int y) {
+        return y ? gcd(y, x % y) : x;
+    }
+public:
+    int gcdOfOddEvenSums(int n) {
+        return gcd(n * (n + 1), n * n);
+    }
+};

Codes/3658-gcd-of-odd-and-even-sums_20260724-reGCD-03traverse.cpp
@@ -1,0 +1,25 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-24 10:22:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-24 10:26:57
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+private:
+    int gcd(int x, int y) {
+        while (y) {
+            int tmp = y;
+            y = x % y;
+            x = tmp;
+        }
+        return x;
+    }
+public:
+    int gcdOfOddEvenSums(int n) {
+        return gcd(n * (n + 1), n * n);
+    }
+};

commit 5dbb1bc90fdd4f37548fde6fae7907122363acce

clean: caused by #1720

.commitmsg
@@ -1,13 +1,0 @@
-3536: AC.cpp (#1719) - AC,100.00%,86.27%
-
----
-
-3658: re gcd (#1699)
-
-01.return n - AC,100.00%,5.17%
-02.recures  - AC,递归,100.00%,38.94%
-03.traverse - AC,traverse,100.00%,37.26%
-
----
-
-docs: OSC 8（终端超链接）

OSC8terminalLink.py
@@ -2,14 +2,12 @@
 Author: LetMeFly
 Date: 2026-07-24 22:05:18
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-07-24 22:26:03
+LastEditTime: 2026-07-25 22:39:38
 Description: 在支持OSC8协议的终端输出自定义显示文字的超链接
 Description: https://gist.github.com/LetMeFly666/a15181a9edfcd25b2d60b3decb9f2dfd
 '''
-print("see TODO: for more")
+def osc8(link: str, text: str) -> str:
+    return f"\033]8;;{link}\033\\{text}\033]8;;\033\\"
 
-print(
-    "\033]8;;https://letmefly.xyz?from=terminalOSC8_20260724\033\\"
-    "宝藏小网站"
-    "\033]8;;\033\\"
-)
+print(osc8('https://letmefly.xyz?from=terminalOSC8_20260726', '宝藏小网站'))
+print("see", osc8('https://github.com/LetMeFly666/LeetCode/blob/2392b92a4320ed9101a7cd816079513c78fd70f6/Solutions/Other-Accumulation-SomeTips.md?plain=1#L1932-L1955', 'Github@LeetCode'), "for more")

customePhrase_MacOSPlist2Rime.py
@@ -1,54 +1,0 @@
-'''
-Author: LetMeFly
-Date: 2026-07-22 20:56:24
-LastEditors: LetMeFly.xyz
-LastEditTime: 2026-07-24 22:23:57
-Description: 把MacOS的自定义短语导出为Rime可使用版本（词Tab拼写）
-Description: 首先需要在 设置-键盘-自定义短句-全部选中-导出为plist
-Description: See https://gist.github.com/LetMeFly666/f1cc13df935e99908d67a8b2bbff99eb for more
-'''
-import plistlib
-
-"""
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<array>
-	<dict>
-		<key>phrase</key>
-		<string>北京邮电大学</string>
-		<key>shortcut</key>
-		<string>bjyd</string>
-	</dict>
-	<dict>
-		<key>phrase</key>
-		<string>≠</string>
-		<key>shortcut</key>
-		<string>budengyu</string>
-	</dict>
-    ...
-</array>
-</plist>
-"""
-
-INPUT = "/Users/tisfy/Downloads/自定义短语.plist"
-OUTPUT = "output.txt"
-
-
-def main():
-    with open(INPUT, "rb") as f:
-        data = plistlib.load(f)
-
-    with open(OUTPUT, "w", encoding="utf-8") as f:
-        for item in data:
-            phrase = item.get("phrase")
-            shortcut = item.get("shortcut")
-
-            if phrase and shortcut:
-                f.write(f"{phrase}\t{shortcut}\n")
-            else:
-                print(f"'{phrase}'\t'{shortcut}'")
-
-
-if __name__ == "__main__":
-    main()