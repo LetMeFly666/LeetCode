LetMeFly666/LeetCode

commit 2392b92a4320ed9101a7cd816079513c78fd70f6

update: 添加问题“3536.两个数字的最大乘积”的代码和题解 (#1720) 3536: AC.cpp (#1719) - AC,100.00%,86.27%
---

3658: re gcd (#1699)

01.return n - AC,100.00%,5.17%
02.recures  - AC,递归,100.00%,38.94%
03.traverse - AC,traverse,100.00%,37.26%

---

docs: OSC 8（终端超链接）
Signed-off-by: LetMeFly666 <Tisfy@qq.com>

.commitmsg
@@ -1,1 +1,13 @@
-3499: AC.cpp (#1710) - AC,80.00%,100.00%
+3536: AC.cpp (#1719) - AC,100.00%,86.27%
+
+---
+
+3658: re gcd (#1699)
+
+01.return n - AC,100.00%,5.17%
+02.recures  - AC,递归,100.00%,38.94%
+03.traverse - AC,traverse,100.00%,37.26%
+
+---
+
+docs: OSC 8（终端超链接）

OSC8terminalLink.py
@@ -1,0 +1,15 @@
+'''
+Author: LetMeFly
+Date: 2026-07-24 22:05:18
+LastEditors: LetMeFly.xyz
+LastEditTime: 2026-07-24 22:26:03
+Description: 在支持OSC8协议的终端输出自定义显示文字的超链接
+Description: https://gist.github.com/LetMeFly666/a15181a9edfcd25b2d60b3decb9f2dfd
+'''
+print("see TODO: for more")
+
+print(
+    "\033]8;;https://letmefly.xyz?from=terminalOSC8_20260724\033\\"
+    "宝藏小网站"
+    "\033]8;;\033\\"
+)

customePhrase_MacOSPlist2Rime.py
@@ -2,9 +2,10 @@
 Author: LetMeFly
 Date: 2026-07-22 20:56:24
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-07-22 21:13:57
+LastEditTime: 2026-07-24 22:23:57
 Description: 把MacOS的自定义短语导出为Rime可使用版本（词Tab拼写）
 Description: 首先需要在 设置-键盘-自定义短句-全部选中-导出为plist
+Description: See https://gist.github.com/LetMeFly666/f1cc13df935e99908d67a8b2bbff99eb for more
 '''
 import plistlib
 

Codes/3536-maximum-product-of-two-digits.cpp
@@ -1,0 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-25 22:11:12
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-25 22:11:39
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        int mx1 = 0, mx2 = 0;
+        while (n) {
+            int t = n % 10;
+            n /= 10;
+            if (t > mx2) {
+                mx1 = mx2;
+                mx2 = t;
+            } else if (t > mx1) {
+                mx1 = t;
+            }
+        }
+        return mx1 * mx2;
+    }
+};

Codes/3658-gcd-of-odd-and-even-sums_20260724-reGCD-01return.cpp
@@ -1,0 +1,16 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-24 10:22:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-24 10:22:37
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};

Codes/3658-gcd-of-odd-and-even-sums_20260724-reGCD-02recurse.cpp
@@ -1,0 +1,20 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-24 10:22:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-24 10:25:36
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+private:
+    int gcd(int x, int y) {
+        return y ? gcd(y, x % y) : x;
+    }
+public:
+    int gcdOfOddEvenSums(int n) {
+        return gcd(n * (n + 1), n * n);
+    }
+};

Codes/3658-gcd-of-odd-and-even-sums_20260724-reGCD-03traverse.cpp
@@ -1,0 +1,25 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-24 10:22:13
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-24 10:26:57
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+private:
+    int gcd(int x, int y) {
+        while (y) {
+            int tmp = y;
+            y = x % y;
+            x = tmp;
+        }
+        return x;
+    }
+public:
+    int gcdOfOddEvenSums(int n) {
+        return gcd(n * (n + 1), n * n);
+    }
+};

commit 111b6506e93d298d4aebe6438c06f491ac8924ff

update(+docs): 添加问题“628.三个数的最大乘积”的代码和题解 + docs: 在VsCode markdown的公式中快速插入⌊分数⌋等代码段 + fix:  tag修复 (#1723)
* docs: 在VsCode markdown的公式中快速插入⌊分数⌋等代码段 (#1722)

* clean: caused by #1720

* 628: tmp.cpp (#1721)

* 628: tmp.cpp (#1721)

* 628: AC.cpp (#1721) - AC,31.52%,55.01%

* docs: tmp (#1721)

* update: 添加问题“628.三个数的最大乘积”的代码和题解 (#1723)

Signed-off-by: LetMeFly666 <Tisfy@qq.com>

* review: by ChatGPT (#1721)

* fix: typo + URL + better表述 (#1723)

* fix: typo (#1723)

* docs: 时空复杂度 (#1721)

---------

Signed-off-by: LetMeFly666 <Tisfy@qq.com>

.commitmsg
@@ -1,13 +1,0 @@
-3536: AC.cpp (#1719) - AC,100.00%,86.27%
-
----
-
-3658: re gcd (#1699)
-
-01.return n - AC,100.00%,5.17%
-02.recures  - AC,递归,100.00%,38.94%
-03.traverse - AC,traverse,100.00%,37.26%
-
----
-
-docs: OSC 8（终端超链接）

Codes/0628-maximum-product-of-three-numbers.cpp
@@ -1,0 +1,47 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-26 18:46:00
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-26 21:59:32
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+/*
+全正 / 1负 / 2负 / 全负
+
++ 全正: 最大3+
++ 1负： 最大1-、最小2+
++ 2负： 最小2-、最大1+
++ 全负: 最大3-
+
+不对，没考虑还有0的情况
+
+---
+
+假设全是负数： 只能选三个最大的
+没正数但有0：  选0
+
+算了，这样想有点麻烦
+
+---
+
+选法：不是迫不得已不选三个负数 / 尽量不选0
+
+假设正负数都很充足： 三个最大 / 两个最小负数+一最大正数
+没有负数： 三个最大
+必须选且只选一个负数：只有一个负数和另外两个数，就三个数没得选
+有至少两个负数并且能选非负数：两个最小负数+一最大数
+只能全选负数：三个最大负数
+
+要么三个最大，要么两个最小+一最大
+*/
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        ranges::sort(nums);
+        int n = nums.size();
+        return max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
+    }
+};

OSC8terminalLink.py
@@ -2,14 +2,12 @@
 Author: LetMeFly
 Date: 2026-07-24 22:05:18
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-07-24 22:26:03
+LastEditTime: 2026-07-25 22:39:38
 Description: 在支持OSC8协议的终端输出自定义显示文字的超链接
 Description: https://gist.github.com/LetMeFly666/a15181a9edfcd25b2d60b3decb9f2dfd
 '''
-print("see TODO: for more")
+def osc8(link: str, text: str) -> str:
+    return f"\033]8;;{link}\033\\{text}\033]8;;\033\\"
 
-print(
-    "\033]8;;https://letmefly.xyz?from=terminalOSC8_20260724\033\\"
-    "宝藏小网站"
-    "\033]8;;\033\\"
-)
+print(osc8('https://letmefly.xyz?from=terminalOSC8_20260726', '宝藏小网站'))
+print("see", osc8('https://github.com/LetMeFly666/LeetCode/blob/2392b92a4320ed9101a7cd816079513c78fd70f6/Solutions/Other-Accumulation-SomeTips.md?plain=1#L1932-L1955', 'Github@LeetCode'), "for more")

customePhrase_MacOSPlist2Rime.py
@@ -1,54 +1,0 @@
-'''
-Author: LetMeFly
-Date: 2026-07-22 20:56:24
-LastEditors: LetMeFly.xyz
-LastEditTime: 2026-07-24 22:23:57
-Description: 把MacOS的自定义短语导出为Rime可使用版本（词Tab拼写）
-Description: 首先需要在 设置-键盘-自定义短句-全部选中-导出为plist
-Description: See https://gist.github.com/LetMeFly666/f1cc13df935e99908d67a8b2bbff99eb for more
-'''
-import plistlib
-
-"""
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<array>
-	<dict>
-		<key>phrase</key>
-		<string>北京邮电大学</string>
-		<key>shortcut</key>
-		<string>bjyd</string>
-	</dict>
-	<dict>
-		<key>phrase</key>
-		<string>≠</string>
-		<key>shortcut</key>
-		<string>budengyu</string>
-	</dict>
-    ...
-</array>
-</plist>
-"""
-
-INPUT = "/Users/tisfy/Downloads/自定义短语.plist"
-OUTPUT = "output.txt"
-
-
-def main():
-    with open(INPUT, "rb") as f:
-        data = plistlib.load(f)
-
-    with open(OUTPUT, "w", encoding="utf-8") as f:
-        for item in data:
-            phrase = item.get("phrase")
-            shortcut = item.get("shortcut")
-
-            if phrase and shortcut:
-                f.write(f"{phrase}\t{shortcut}\n")
-            else:
-                print(f"'{phrase}'\t'{shortcut}'")
-
-
-if __name__ == "__main__":
-    main()

review-byChatGPT.sh
@@ -1,0 +1,100 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PR="${1:-}"
+
+if [[ -z "$PR" ]]; then
+    echo "Usage: $0 <PR_NUMBER>"
+    exit 1
+fi
+
+read -r -d '' REVIEW <<'EOF' || true
+## Review
+
+### 🔴 Must fix
+
+#### 1. Remove temporary VSCode snippet test files
+
+The following files look like local debugging artifacts rather than repository content:
+
+- `testForSnipppet.md`
+- `testForSnipppet.md.cpp`
+- `testForSnipppet.md.tex`
+
+Please remove them or move them into a dedicated documentation/test area.
+
+#### 2. Fix typo in filenames
+
+`testForSnipppet` appears to contain an extra `p`.
+
+Consider renaming it to:
+
+```
+testForSnippet
+```
+
+#### 3. Confirm `.commitmsg` deletion
+
+`.commitmsg` was deleted in this PR.
+
+Please verify that no git hooks, scripts, or automation still depend on it before merging.
+
+---
+
+### 🟡 Suggestions
+
+#### 4. Consider O(n) solution for LeetCode 628
+
+The current implementation sorts the array:
+
+```cpp
+ranges::sort(nums);
+```
+
+This is correct, but the problem can also be solved with O(n) time and O(1) extra space by maintaining:
+
+- three maximum values
+- two minimum values
+
+The current solution is acceptable if simplicity is preferred.
+
+#### 5. Add `__main__` guard in `OSC8terminalLink.py`
+
+The script currently executes `print(...)` at import time.
+
+Consider wrapping demo code:
+
+```python
+if __name__ == "__main__":
+    ...
+```
+
+to avoid import side effects.
+
+#### 6. Improve explanation wording in LeetCode 628 article
+
+The proof is correct, but the negative-number classification could be made clearer by first explaining:
+
+- If non-negative choices exist, selecting an odd number of negative numbers is generally not optimal.
+- The optimal candidates reduce to:
+  - three largest numbers
+  - two smallest numbers plus the largest number
+
+---
+
+### Summary
+
+Overall this PR is functional and the main changes look reasonable.
+
+Before merging, I recommend:
+- remove temporary snippet test files
+- verify `.commitmsg` removal
+- fix typo naming
+
+The remaining items are improvement suggestions.
+
+by ChatGPT
+EOF
+
+gh pr review "$PR" --request-changes --body "$REVIEW"

testForSnippet.md.cpp
@@ -1,0 +1,12 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-26 16:59:51
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-26 17:00:04
+ */
+fenshu
+
+for (int i = 0; i < n; i++) {
+    
+}
+

testForSnippet.md.tex
@@ -1,0 +1,3 @@
+\lfloor\frac{}{2}\rfloor
+
+$ \lfloor\frac{}{2}\rfloor $