03936fd01164a8ed5de9e1c30cd5c731ece6926e
LetMeFly666 <Tisfy@qq.com>
2026-07-28 21:42:09 +0800

    3517: RE.cpp (#1726)

--- Codes/3517-smallest-palindromic-rearrangement-i.cpp
+++ Codes/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-07-28 21:34:54
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-07-28 21:36:51
+ * @LastEditTime: 2026-07-28 21:41:30
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -15,12 +15,20 @@ public:
         for (char c : s) {
             cnt[c - 'a']++;
         }
+        
+        char middle = 0;
+        if (s.size() % 2) {
+            middle = s[s.size() / 2 - 1];
+        }
 
         int idx = 0;
         for (int i = 0; i < 26; i++) {
             for (int j = 0; j < cnt[i] / 2; j++) {
                 s[idx++] = i + 'a';
             }
+        }
+        if (middle) {
+            s[idx++] = middle;
         }
         for (int i = 25; i >= 0; i--) {
             for (int j = 0; j < cnt[i] / 2; j++) {
