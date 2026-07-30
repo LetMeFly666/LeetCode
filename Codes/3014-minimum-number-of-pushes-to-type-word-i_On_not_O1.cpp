044b6ba8dae293cf8fc4cc349900877ebdf1561a
Tisfy <Tisfy@foxmail.com>
2026-07-30 19:01:29 +0800

    3014: WA.On.cpp (#1731) - 106 / 500

---
+++ Codes/3014-minimum-number-of-pushes-to-type-word-i_On_not_O1.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-07-30 18:53:16
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-07-30 19:00:50
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int minimumPushes(string& word) {
+        int ans = 0;
+        for (int i = 1, n = word.size(); i <= n; i++) {
+            ans += i / 8 + 1;
+        }
+        return ans;
+    }
+};
