48e38f5b6667e08e933573453d4ed4db122553ad
LetMeFly666 <Tisfy@qq.com>
2026-08-03 18:59:08 +0800

    1406: WA.cpp.DP.O1 (#1740) - 80 / 185

---
+++ Codes/1406-stone-game-iii_DP-O1Space.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-08-03 18:28:31
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-08-03 18:58:38
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int dp3[3] = {0};
+        int suffix = 0;
+        for (int i = stoneValue.size() - 1; i >= 0; i--) {
+            int dp = INT_MIN;
+            suffix += stoneValue[i];
+            for (int j = 0; j < 3; j++) {
+                dp = max(dp, suffix - dp3[j]);
+            }
+            dp = dp3[0], dp3[0] = dp3[1], dp3[1] = dp3[2];
+        }
+        
+        int diff = dp3[0] - (suffix - dp3[0]);
+        return diff > 0 ? "Alice" : diff ? "Bob" : "Tie";
+    }
+};
