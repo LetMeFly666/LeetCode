4c37363047663be211afe03b1c0b14d579baf222
LetMeFly <Tisfy@qq.com>
2026-08-03 21:48:29 +0800

    update: 添加问题“1406.石子游戏III”的代码和题解 (#1741)
    * 1406: WA.cpp (#1740) -
    87 / 185
    
    忽然想起来忘记忆化了，但是竟然先WA了
    
    * 1406: AC.cpp (#1740) - AC,18.92%,35.13%
    
    * 1406: WA.cpp.DP (#1740) - 170 / 185
    
    * 1406: AC.cpp.DP (#1740) - AC,56.76%,70.27%
    
    好吧，一个未采用的初始化计划所致
    
    * 1406: WA.cpp.DP.O1 (#1740) - 80 / 185
    
    * 1406: AC.cpp.DP.O1 (#1740) - AC,94.59%,83.78%
    
    * update: 添加问题“1406.石子游戏III”的代码和题解 (#1741)
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>

---
+++ Codes/1406-stone-game-iii_DP.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-08-03 18:28:31
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-08-03 18:55:21
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        vector<int> dp(stoneValue.size() + 3);
+        int suffix = 0;
+        for (int i = stoneValue.size() - 1; i >= 0; i--) {
+            dp[i] = INT_MIN;
+            suffix += stoneValue[i];
+            for (int j = 1; j <= 3; j++) {
+                dp[i] = max(dp[i], suffix - dp[i + j]);
+            }
+        }
+        
+        int diff = dp[0] - (suffix - dp[0]);
+        return diff > 0 ? "Alice" : diff ? "Bob" : "Tie";
+    }
+};
