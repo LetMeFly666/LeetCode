892f35a8a59f7cfcd5dde0eac7e3fb0833936625
LetMeFly666 <Tisfy@qq.com>
2026-08-03 19:02:32 +0800

    1406: AC.cpp.DP.O1 (#1740) - AC,94.59%,83.78%

--- Codes/1406-stone-game-iii_DP-O1Space.cpp
+++ Codes/1406-stone-game-iii_DP-O1Space.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-08-03 18:28:31
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-08-03 18:58:38
+ * @LastEditTime: 2026-08-03 19:01:21
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -19,7 +19,7 @@ public:
             for (int j = 0; j < 3; j++) {
                 dp = max(dp, suffix - dp3[j]);
             }
-            dp = dp3[0], dp3[0] = dp3[1], dp3[1] = dp3[2];
+            dp3[2] = dp3[1], dp3[1] = dp3[0], dp3[0] = dp;
         }
         
         int diff = dp3[0] - (suffix - dp3[0]);
