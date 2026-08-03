aab8aa7579a3bb766addc708daa17a6cf6c8a293
LetMeFly666 <Tisfy@qq.com>
2026-08-03 18:37:29 +0800

    1406: AC.cpp (#1740) - AC,18.92%,35.13%

--- Codes/1406-stone-game-iii.cpp
+++ Codes/1406-stone-game-iii.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-08-03 18:28:31
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-08-03 18:33:32
+ * @LastEditTime: 2026-08-03 18:36:37
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -11,10 +11,14 @@
 class Solution {
 private:
     int n;
+    vector<int> mem;
 
     int play(vector<int>& v, int idx=0) {
+        if (mem[idx] != INT_MIN) {
+            return mem[idx];
+        }
         if (idx == n) {
-            return 0;
+            return mem[idx] = 0;
         }
         int ans = INT_MIN;
         for (int i = 0, cnt = 0; i < 3; i++) {
@@ -22,13 +26,14 @@ private:
                 break;
             }
             cnt += v[idx + i];
-            ans = max(ans, cnt + play(v, idx + i + 1));
+            ans = max(ans, cnt - play(v, idx + i + 1));
         }
-        return ans;
+        return mem[idx] = ans;
     }
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         n = stoneValue.size();
+        mem.resize(n + 1, INT_MIN);
         int score = play(stoneValue);
         return score > 0 ? "Alice" : score ? "Bob" : "Tie";
     }
