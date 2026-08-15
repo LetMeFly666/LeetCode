2d2854ba4a02fd787420afd5701ea42876049107
LetMeFly666 <Tisfy@qq.com>
2026-08-15 10:30:27 +0800

    1288: AC.py+CE.rust (#1680)
    py - AC,34.96%,5.39%

--- Codes/1288-remove-covered-intervals_20260815.py
+++ Codes/1288-remove-covered-intervals_20260815.py
@@ -2,7 +2,7 @@
 Author: LetMeFly
 Date: 2026-08-15 10:09:19
 LastEditors: LetMeFly.xyz
-LastEditTime: 2026-08-15 10:25:54
+LastEditTime: 2026-08-15 10:27:14
 '''
 from typing import List
 
@@ -10,7 +10,7 @@ class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: (x[0], -x[1]))
         ans, maxr = len(intervals), 0
-        for _, e in range(intervals):
+        for _, e in intervals:  # 看来语言写多了就是容易弄混
             if maxr >= e:
                 ans -= 1
             else:
---
+++ Codes/1288-remove-covered-intervals_20260815.rs
@@ -0,0 +1,27 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-08-15 09:52:55
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-08-15 10:30:02
+ */
+impl Solution {
+    pub fn remove_covered_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
+        intervals.sort_by_key(|a, b| {
+            if a[0] == b[0] {
+                a[1] > b[1]
+            } else {
+                a[0] < b[0]
+            }
+        })
+        let mut ans = intervals.len() as i32;
+        let mut maxr = 0;
+        for p in intervals.iter() {
+            if p[1] <= maxr {
+                ans--;
+            } else {
+                maxr = p[1];
+            }
+        }
+        return ans
+    }
+}
