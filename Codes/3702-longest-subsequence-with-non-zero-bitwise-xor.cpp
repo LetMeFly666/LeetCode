bb2733ed27d63f9b714019462b6809e14ce3f0fd
LetMeFly666 <Tisfy@qq.com>
2026-08-15 09:37:53 +0800

    3702: AC.cpp (#1760)
    AC,100.00%,29.41%

--- Codes/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ Codes/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-08-15 09:33:14
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-08-15 09:34:42
+ * @LastEditTime: 2026-08-15 09:36:22
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -14,7 +14,7 @@ public:
         bool hasVal = false;
         int res = 0;
         for (int t : nums) {
-            res ^= res;
+            res ^= t;
             hasVal |= (t != 0);
         }
         return hasVal ? res ? nums.size() : nums.size() - 1 : 0;
