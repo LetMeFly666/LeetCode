ca7677d32ae2f4e8cd1946cdf729cc6148de45b9
LetMeFly666 <Tisfy@qq.com>
2026-08-18 16:43:23 +0800

    3471: WA.cpp (#1764) - 756 / 900

---
+++ Codes/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,39 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-08-18 16:23:09
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-08-18 16:41:58
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n == k || n == 1) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        int first = nums[0], last = nums.back();
+        if (first == last) {
+            return -1;
+        }
+        bool another1 = false, another2 = false;
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == first) {
+                another1 = true;
+            }
+            if (nums[i] == last) {
+                another2 = true;
+            }
+        }
+        if (another1 && another2) {
+            return -1;
+        }
+        if (!another1 && !another2) {
+            return max(first, last);
+        }
+        return another1 ? last : first;
+    }
+};
