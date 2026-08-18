c9df42be3c0e6aa12337fc447e4241357eb357a1
LetMeFly666 <Tisfy@qq.com>
2026-08-18 17:26:30 +0800

    3090: WA.cpp (#1757)

---
+++ Codes/3090-maximum-length-substring-with-two-occurrences_20260818.cpp
@@ -0,0 +1,26 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-08-18 17:24:17
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-08-18 17:26:09
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int maximumLengthSubstring(string& s) {
+        int ans = 0;
+        int cnt[26] = {0};
+        for (int l = 0, r = 0, n = s.size(); r < n; r++) {
+            int th = s[r] - 'a';
+            cnt[th]++;
+            while (cnt[th] > 1) {
+                cnt[s[l++] - 'a']--;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
