267d97b825c3ecddf3eaa713dd333f5ac9a0abd0
LetMeFly666 <Tisfy@qq.com>
2026-08-26 14:13:16 +0800

    2904: WA.cpp (#1777) - 522 / 674

--- Codes/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ Codes/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-08-26 07:55:52
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-08-26 14:07:19
+ * @LastEditTime: 2026-08-26 14:10:54
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -11,9 +11,10 @@
 class Solution {
 private:
     void update(string& ans, string& s, int l, int r) {
-        std::string_view cur(s.data() + l, r - l + 1);
-        if (ans.empty() || r - l + 1 < ans.size() || ans < cur) {
-            ans = s.substr(l, r - l + 1);
+        int len = r - l + 1;
+        std::string_view cur(s.data() + l, len);
+        if (ans.empty() || len < ans.size() || len == ans.size() && ans < cur) {
+            ans = cur;
         }
     }
 public:
