d2b3063557b78a972acdcfd551c130df73210582
LetMeFly666 <Tisfy@qq.com>
2026-08-26 14:02:06 +0800

    2904: WA.cpp (#1777) - 403 / 674

--- Codes/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ Codes/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-08-26 07:55:52
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-08-26 13:59:14
+ * @LastEditTime: 2026-08-26 14:01:39
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -21,7 +21,7 @@ public:
         for (int l = 0, r = 0, cnt = 0, n = s.size(); r < n; r++) {
             cnt += s[r] == '1';
             while (cnt > k) {
-                cnt -= s[l++] - '1';
+                cnt -= s[l++] == '1';
             }
             if (cnt == k) {
                 update(ans, s, l, r);
