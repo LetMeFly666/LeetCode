95aea217abe4d0395ba93d6c3b573762c6ed58fd
LetMeFly666 <Tisfy@qq.com>
2026-08-29 11:28:45 +0800

    2948: AC.cpp (#1783) - AC,50.00%,81.82%

--- Codes/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ Codes/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -2,22 +2,37 @@
  * @Author: LetMeFly
  * @Date: 2026-08-29 11:00:55
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-08-29 11:05:44
+ * @LastEditTime: 2026-08-29 11:27:22
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
 #endif
 
 class Solution {
+private:
+    void full(vector<int>& ans, vector<int>& nums, vector<int>& idxs, int l, int r) {
+        vector<int> pos(r - l);
+        for (int i = l, th = 0; i < r; i++, th++) {
+            pos[th] = idxs[i];
+        }
+        ranges::sort(pos);
+        for (int i = l, th = 0; i < r; i++, th++) {
+            ans[pos[th]] = nums[idxs[i]];
+        }
+    }
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        for (int i = 1, n = nums.size(), m = nums[0], M = nums[0], from = 0; i <= n; i++) {
-            if (i == n || abs(nums[i] - m) > limit && abs(nums[i] - M) > limit) {
-                sort(nums.begin() + from, nums.begin() + i);
-                m = nums[i], M = nums[i], from = i;
-            } else {
-                m = min()
+        vector<int> idxs(nums.size());
+        ranges::iota(idxs, 0);
+        sort(idxs.begin(), idxs.end(), [&nums](const int& a, const int& b) { return nums[a] < nums[b]; });
+
+        vector<int> ans(nums.size());
+        for (int i = 1, n = nums.size(), last = 0; i <= n; i++) {
+            if (i == n || nums[idxs[i]] - nums[idxs[i - 1]] > limit) {
+                full(ans, nums, idxs, last, i);
+                last = i;
             }
         }
+        return ans;
     }
 };
