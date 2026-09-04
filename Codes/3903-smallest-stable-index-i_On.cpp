a8aa544e87862d594ab0a8664337d20caa1e00d2
LetMeFly666 <Tisfy@qq.com>
2026-09-04 18:39:34 +0800

    3093: AC.cpp (#1794) - 刚刚是低级笔误，也许以后刷力扣应该先检查下再提交了
    AC,100.00%,41.94%

--- Codes/3903-smallest-stable-index-i_On.cpp
+++ Codes/3903-smallest-stable-index-i_On.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-09-04 18:29:59
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-04 18:37:18
+ * @LastEditTime: 2026-09-04 18:38:35
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -15,7 +15,7 @@ public:
         vector<int> mini(n);
         mini.back() = nums.back();
         for (int i = n - 2; i >= 0; i--) {
-            mini[i] = min(mini[i], mini[i + 1]);
+            mini[i] = min(nums[i], mini[i + 1]);
         }
         for (int i = 0, M = 0; i < n; i++) {
             M = max(M, nums[i]);
