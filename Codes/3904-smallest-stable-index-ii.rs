a7a989376a1d2a49a417d4dd60818c1c1d314a59
LetMeFly666 <Tisfy@qq.com>
2026-09-05 08:54:09 +0800

    3904: CE.rust (#1796)

---
+++ Codes/3904-smallest-stable-index-ii.rs
@@ -0,0 +1,24 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-09-05 08:24:28
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-09-05 08:53:38
+ */
+impl Solution {
+    pub fn first_stable_index(nums: Vec<i32>, k: i32) -> i32 {
+        let n = nums.len();
+        let mut mini = Vec![n];
+        mini[n-1] = nums[n-1];
+        for i in (0..n-1).rev() {
+            mini[i] = nums[i].min(mini[i+1]);
+        }
+        let mut M = 0;
+        for i in 0..n {
+            M = M.max(nums[i]);
+            if M - mini[i] <= k {
+                return i;
+            }
+        }
+        -1
+    }
+}
