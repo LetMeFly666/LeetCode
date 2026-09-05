b893d3ba721acbac955056a01024f7c120b2b362
LetMeFly666 <Tisfy@qq.com>
2026-09-05 08:56:20 +0800

    3904: AC.rust (#1796) - AC,60.00%,80.00%

--- Codes/3904-smallest-stable-index-ii.rs
+++ Codes/3904-smallest-stable-index-ii.rs
@@ -2,12 +2,12 @@
  * @Author: LetMeFly
  * @Date: 2026-09-05 08:24:28
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-05 08:53:38
+ * @LastEditTime: 2026-09-05 08:55:35
  */
 impl Solution {
     pub fn first_stable_index(nums: Vec<i32>, k: i32) -> i32 {
         let n = nums.len();
-        let mut mini = Vec![n];
+        let mut mini = vec![0; n];
         mini[n-1] = nums[n-1];
         for i in (0..n-1).rev() {
             mini[i] = nums[i].min(mini[i+1]);
@@ -16,7 +16,7 @@ impl Solution {
         for i in 0..n {
             M = M.max(nums[i]);
             if M - mini[i] <= k {
-                return i;
+                return i as i32;
             }
         }
         -1
