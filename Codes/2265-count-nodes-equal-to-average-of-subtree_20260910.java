87e08279f7de7d10e96ce064279f1c8f07ea7e1c
LetMeFly <Tisfy@qq.com>
2026-09-10 17:24:05 +0800

    update: 添加问题“2265.统计值等于子树平均值的节点数”的代码和题解 (#1806)
    * 2265: AC.cpp+go + CE.java (#1850)
    
    cpp - AC,23.35%,14.97%
    go - AC,100.00%,75.00%
    
    * 2265: AC.java+python (#1850)
    
    java - AC,98.55%,40.58%
    py - AC,53.43%,87.79%
    
    2026-09-10 14:53:13
    
    * update: 添加问题“2265.统计值等于子树平均值的节点数”的代码和题解 (#1806)
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>

---
+++ Codes/2265-count-nodes-equal-to-average-of-subtree_20260910.java
@@ -0,0 +1,46 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-09-10 14:38:19
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-09-10 14:49:28
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+record Result (int val, int n) {}
+
+class Solution {
+    private int ans;
+
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(0, 0);
+        }
+        Result left = dfs(root.left);
+        Result right = dfs(root.right);
+        int val = root.val + left.val() + right.val();
+        int n = 1 + left.n() + right.n();
+        if (val / n == root.val) {
+            ans++;
+        }
+        return new Result(val, n);
+    }
+
+    public int averageOfSubtree(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+}
---
+++ toSay.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: LetMeFly
+ * @Date: 2026-09-10 17:13:34
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-09-10 17:13:34
+-->
+最近CSDN发文效果不好捏。
+
+算法变了可能。
