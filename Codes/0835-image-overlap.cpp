693d169eaff5b9034fbbd4bd9bc77900949a7157
LetMeFly666 <Tisfy@qq.com>
2026-09-13 11:32:32 +0800

    835: AC.cpp (#1810) - AC,74.19%,64.52%

--- Codes/0835-image-overlap.cpp
+++ Codes/0835-image-overlap.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-09-13 11:21:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-13 11:31:07
+ * @LastEditTime: 2026-09-13 11:32:09
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -13,8 +13,8 @@ public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int ans = 0;
         int n = img1.size();
-        for (int dx = n - 1; dx < n; dx++) {
-            for (int dy = n - 1; dy < n; dy++) {
+        for (int dx = 1 - n; dx < n; dx++) {  // [-(n-1), n-1]
+            for (int dy = 1 - n; dy < n; dy++) {
                 int this_ans = 0;
                 // img1[i][j] -> img2[i+dx][j+dy]
                 // 0≤i<n 且 0≤i+dx<n 得出 0≤i -dx≤i i<n i<n-dx
