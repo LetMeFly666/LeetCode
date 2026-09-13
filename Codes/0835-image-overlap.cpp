2216e8052c5f261e2bc01b88512a796d5e77d7d5
LetMeFly666 <Tisfy@qq.com>
2026-09-13 11:31:14 +0800

    835: WA.cpp (#1810)
    刚刚其实是RE

--- Codes/0835-image-overlap.cpp
+++ Codes/0835-image-overlap.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-09-13 11:21:15
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-13 11:29:44
+ * @LastEditTime: 2026-09-13 11:31:07
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -17,9 +17,9 @@ public:
             for (int dy = n - 1; dy < n; dy++) {
                 int this_ans = 0;
                 // img1[i][j] -> img2[i+dx][j+dy]
-                // 0≤i≤n 且 0≤i+dx≤n 得出 0≤i -dx≤i i≤n i≤n-dx
-                for (int i = max(0, -dx); i <= min(n, n-dx); i++) {
-                    for (int j = max(0, -dy); j <= min(n, n-dy); j++) {
+                // 0≤i<n 且 0≤i+dx<n 得出 0≤i -dx≤i i<n i<n-dx
+                for (int i = max(0, -dx); i < min(n, n-dx); i++) {
+                    for (int j = max(0, -dy); j < min(n, n-dy); j++) {
                         this_ans += img1[i][j] * img2[i+dx][j+dy];
                     }
                 }
