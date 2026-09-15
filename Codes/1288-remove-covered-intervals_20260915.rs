ed9c6548653e8515d4199d828e72ae7b9642b1a7
LetMeFly666 <Tisfy@qq.com>
2026-09-15 17:32:08 +0800

    1288: AC.rust (#1680)
    rust - AC,100.00%,46.67%

--- Codes/1288-remove-covered-intervals_20260915.rs
+++ Codes/1288-remove-covered-intervals_20260915.rs
@@ -2,17 +2,11 @@
  * @Author: LetMeFly
  * @Date: 2026-09-15 17:12:02
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-15 17:29:56
+ * @LastEditTime: 2026-09-15 17:31:44
  */
 impl Solution {
     pub fn remove_covered_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
-        intervals.sort(|i, j| {
-            if intervals[i][0] == intervals[j][0] {
-                intervals[i][1] > intervals[j][1]
-            } else {
-                intervals[i][0] < intervals[j][0]
-            }
-        });
+        intervals.sort_by_key(|x| (x[0], -x[1]));
         let mut ans = intervals.len() as i32;
         let mut maxr = -1;
         for p in intervals.iter() {
