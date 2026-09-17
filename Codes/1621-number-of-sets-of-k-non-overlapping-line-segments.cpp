488126521a4a0c83ba9a60fa2eb23698d8545792
LetMeFly666 <Tisfy@qq.com>
2026-09-17 08:33:41 +0800

    1621: AC.cpp - AC,100.00%,86.72%

--- Codes/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ Codes/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-09-16 16:56:43
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-16 17:48:02
+ * @LastEditTime: 2026-09-17 08:33:08
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -10,11 +10,42 @@
 
 typedef long long ll;
 const ll MOD = 1e9 + 7;
-ll rev[2001];
-int _ = []() {}();
+ll Jie[2001];
+ll Rev[2001];
+
+ll myPow(ll a, ll b) {
+    ll ans = 1;
+    while (b) {
+        if (b & 1) {
+            ans = ans * a % MOD;
+        }
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+// J[i] = J[i-1] * i
+// R[i] = (J[i-1] * i)^-1 = J[i-1]^-1 * i^-1 = R[i-1] * i^-1
+// R[i-1] = R[i] * i
+int _ = []{
+    Jie[0] = 1;
+    for (int i = 1; i <= 2000; i++) {
+        Jie[i] = Jie[i - 1] * i % MOD;
+    }
+    Rev[2000] = myPow(Jie[2000], MOD - 2);
+    for (int i = 2000; i; i--) {
+        Rev[i - 1] = Rev[i] * i % MOD;
+    }
+    return 0;
+} ();
 
 
 class Solution {
+private:
+    int C(ll a, ll b) {
+        return (Jie[a] * Rev[a - b] % MOD) * Rev[b] % MOD;
+    }
 public:
     int numberOfSets(int n, int k) {
         // 长n-1，k条线，还有n-1-k长度可供使用
@@ -28,5 +59,8 @@ public:
         // C_a^b = a(a-1)(a-2)...(a-b+1) / b(b-1)(b-2)...1
         //       = a/1 * (a-1)/2 * (a-2)/3 * ... * (a-b+1)/b
         // a/t相当于a*rev(t)相当于a*t^{MOD-2}
+        //
+        // 换种思路，C_a^b= a! / (a-b)!b!
+        return C(n + k - 1, 2 * k);
     }
 };
