42f11a88d8cc1848f88d3925f243f14e7fce663c
LetMeFly666 <Tisfy@qq.com>
2026-09-19 10:10:55 +0800

    3090: half.cpp (#1757)
    是每个字符最多出现两次，不是最多出现两种字符

--- Codes/3090-maximum-length-substring-with-two-occurrences_20260919.cpp
+++ Codes/3090-maximum-length-substring-with-two-occurrences_20260919.cpp
@@ -2,26 +2,30 @@
  * @Author: LetMeFly
  * @Date: 2026-09-19 10:04:00
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-19 10:06:38
+ * @LastEditTime: 2026-09-19 10:10:52
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
 #endif
 
 class Solution {
+private:
+    bool ok(int cnt[]) {
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > 2) {
+                return false;
+            }
+        }
+    }
 public:
     int maximumLengthSubstring(string& s) {
         int ans = 0;
         int cnt[26] = {0};
-        for (int l = 0, r = 0, n = s.size(), type_cnt = 0; r < n; r++) {
+        for (int l = 0, r = 0, n = s.size(); r < n; r++) {
             int idx = s[r] - 'a';
-            if (!cnt[idx]++) {
-                type_cnt++;
-            }
-            while (type_cnt == 3) {
-                if (!--cnt[s[l++] - 'a']) {
-                    type_cnt--;
-                }
+            cnt[idx]++;
+            while (cnt[idx] == 3) {
+                
             }
             ans = max(ans, r - l + 1);
         }
