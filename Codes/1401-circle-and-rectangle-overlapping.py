943945ee623c265d08a26e69190a18dff3e9e3bb
LetMeFly666 <Tisfy@qq.com>
2026-09-19 08:42:43 +0800

    1401: AC.cpp+go+java + WA.py (#1821)
    cpp - AC,100.00%,43.59%
    go - AC,100.00%,100.00%
    java - AC,100.00%,19.23%

---
+++ Codes/1401-circle-and-rectangle-overlapping.py
@@ -0,0 +1,11 @@
+'''
+Author: LetMeFly
+Date: 2026-09-19 08:23:49
+LastEditors: LetMeFly.xyz
+LastEditTime: 2026-09-19 08:42:11
+'''
+class Solution:
+    def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
+        x = max(x1, min(xCenter, x2))
+        y = max(y1, min(yCenter, y2))
+        return (x - radius) ** 2 + (y - yCenter) ** 2 <= radius ** 2
