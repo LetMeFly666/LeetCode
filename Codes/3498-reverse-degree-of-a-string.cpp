b65fc7e3dca37cb928ea8bfc068dff6667daf72f
LetMeFly666 <Tisfy@qq.com>
2026-09-20 10:29:25 +0800

    3498: AC.cpp (#1823)
    cpp - AC,100.00%,98.63%

--- Codes/3498-reverse-degree-of-a-string.cpp
+++ Codes/3498-reverse-degree-of-a-string.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-09-20 10:25:05
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-20 10:26:17
+ * @LastEditTime: 2026-09-20 10:27:58
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -13,7 +13,8 @@ public:
     int reverseDegree(const string& s) {
         int ans = 0;
         for (int i = 0, n = s.size(); i < n; i++) {
-            ans += (i + 1) * (27 - s[i] + 'a');
+            // cout << (27 - s[i] + 'a') << " * " << i + 1 << endl;
+            ans += (i + 1) * (26 - s[i] + 'a');
         }
         return ans;
     }
