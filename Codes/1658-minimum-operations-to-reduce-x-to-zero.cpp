1b4612c8a4fb7a57bda2bd707856d8859b907efa
LetMeFly666 <Tisfy@qq.com>
2026-09-23 18:39:07 +0800

    1658: WA.cpp (#1828)

--- Codes/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ Codes/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2026-09-23 18:34:56
  * @LastEditors: LetMeFly.xyz
- * @LastEditTime: 2026-09-23 18:37:23
+ * @LastEditTime: 2026-09-23 18:38:54
  */
 #ifdef _DEBUG
 #include "_[1,2]toVector.h"
@@ -20,7 +20,7 @@ public:
         for (int n = nums.size(), i = n - 1, cnt = 0; i >= 0 && cnt < x; i--) {
             cnt += nums[i];
             if (prefix.count(x - cnt)) {
-                ans = min(ans, prefix[x - cnt] + n - i);
+                ans = min(ans, prefix[x - cnt] + n - i + 1);
             }
         }
         return ans > nums.size() ? -1 : ans;
