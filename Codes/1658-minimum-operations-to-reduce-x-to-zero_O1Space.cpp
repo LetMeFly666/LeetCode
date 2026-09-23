16b169a986d6cb4a26e01af515ebfba0ece8dd3b
LetMeFly <Tisfy@qq.com>
2026-09-23 19:00:29 +0800

    update: 添加问题“1658.将x减到0的最小操作数”的代码和题解 (#1829)
    * 1658: WA.cpp (#1828)
    
    * 1658: WA.cpp (#1828)
    
    * 1658: AC.cpp (#1828) - AC,5.27%,5.01%
    
    * 1658: RE.cpp (#1828)
    
    * 1658: AC.cpp (#1828) - 滑动窗口 AC,5.27%,66.49%
    
    * update: 添加问题“1658.将x减到0的最小操作数”的代码和题解 (#1829)
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>
    
    ---------
    
    Signed-off-by: LetMeFly666 <Tisfy@qq.com>

---
+++ Codes/1658-minimum-operations-to-reduce-x-to-zero_O1Space.cpp
@@ -0,0 +1,31 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2026-09-23 18:44:45
+ * @LastEditors: LetMeFly.xyz
+ * @LastEditTime: 2026-09-23 18:48:44
+ */
+#ifdef _DEBUG
+#include "_[1,2]toVector.h"
+#endif
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int allVal = accumulate(nums.begin(), nums.end(), 0);
+        x = allVal - x;
+        if (x < 0) {  // 不然while会下标越界
+            return -1;
+        }
+        int ans = 10000000;
+        for (int l = 0, r = 0, n = nums.size(), cnt = 0; r < n; r++) {
+            cnt += nums[r];
+            while (cnt > x) {
+                cnt -= nums[l++];
+            }
+            if (cnt == x) {
+                ans = min(ans, n - (r - l + 1));
+            }
+        }
+        return ans == 10000000 ? -1 : ans;
+    }
+};
