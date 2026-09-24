cee81d16abb3adf8939b54e1196fdee83fd13074
LetMeFly666 <Tisfy@qq.com>
2026-09-24 12:27:53 +0800

    1288: AC.java+py+rust (#1680)
    java - AC,82.29%,20.94%
    py - AC,44.80%,5.37%
    rust - AC,100.00%,74.26%

--- Codes/1288-remove-covered-intervals_20260924.java
+++ Codes/1288-remove-covered-intervals_20260924.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (i, j) -> {
-            return intervals[i][0] == intervals[j][0] ? intervals[j][1] - intervals[i][1] : intervals[i][0] - intervals[j][0];
+        Arrays.sort(intervals, (a, b) -> {
+            return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
         });
         int ans = intervals.length;
         int maxr = -1;
